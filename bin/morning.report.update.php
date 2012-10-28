01337f9cd7c36fd12a62c2c02619096ff83cb40a
Carl Schelin <cschelin@west.com>
2012-10-28 19:37:19 +0000

    Updated the script to copy the timestamp field to maintain the created/modified date from day to day.

--- bin/morning.report.update.php
+++ bin/morning.report.update.php
@@ -9,6 +9,7 @@
 
   $db = dbconn('localhost','status','root','this4now!!');
 
+# set the date so the report knows which data to retrieve
   $today = date('Y-m-d');
 
 # if today is Monday, get the updates from Friday and not from Sunday.
@@ -22,18 +23,19 @@
   $q_groups = mysql_query($q_string) or die($q_string . ": " . mysql_error());
   while ($a_groups = mysql_fetch_array($q_groups)) {
 
-    $q_string = "select rep_id,rep_user,rep_group,rep_status,rep_task,rep_date from report where rep_group = " . $a_groups['grp_id'] . " and rep_date = '" . $previous . "'";
+    $q_string = "select rep_id,rep_user,rep_group,rep_status,rep_task,rep_timestamp from report where rep_group = " . $a_groups['grp_id'] . " and rep_date = '" . $previous . "'";
     $q_report = mysql_query($q_string) or die($q_string . ": " . mysql_error());
     while ($a_report = mysql_fetch_array($q_report)) {
 
       if ($a_report['rep_status'] > 1) {
         $q_string = "insert into report set " .
-          "rep_id     =   " . "NULL"                  . "," . 
-          "rep_user   =   " . $a_report['rep_user']   . "," . 
-          "rep_group  =   " . $a_report['rep_group']  . "," . 
-          "rep_date   = \"" . $a_report['rep_date']   . "\"," . 
-          "rep_status =   " . $a_report['rep_status'] . "," . 
-          "rep_task   = \"" . $a_report['rep_task']   . "\"";
+          "rep_id        =   " . "NULL"                      . "," . 
+          "rep_user      =   " . $a_report['rep_user']       . "," . 
+          "rep_timestamp = \"" . $a_report['rep_timestamp']  . "\"," . 
+          "rep_group     =   " . $a_report['rep_group']      . "," . 
+          "rep_date      = \"" . $today                      . "\"," . 
+          "rep_status    =   " . $a_report['rep_status']     . "," . 
+          "rep_task      = \"" . $a_report['rep_task']       . "\"";
 
         mysql_query($q_string) or die($q_string . ": " . mysql_error());
 
