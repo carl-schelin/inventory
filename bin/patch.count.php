3d64e49afc7abd62d93787c6ae84f0f6c691d379
Carl Schelin <cschelin@west.com>
2016-03-01 02:35:28 +0000

    Initial revision

---
+++ bin/patch.count.php
@@ -0,0 +1,50 @@
+#!/usr/local/bin/php
+<?php
+# Script: patch.count.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: 
+
+  include('settings.php');
+  include($Sitepath . '/function.php');
+
+  function dbconn($server,$database,$user,$pass){
+    $db = mysql_connect($server,$user,$pass);
+    $db_select = mysql_select_db($database,$db);
+    return $db;
+  }
+
+  $db = dbconn($DBserver, $DBname, $DBuser, $DBpassword);
+
+  print "Server Name,OS,Target OS,Date Reached\n";
+
+  $q_string  = "select inv_id,inv_name,inv_kernel ";
+  $q_string .= "from inventory ";
+  $q_string .= "where inv_status = 0 and inv_manager = 1 and inv_ssh = 1 ";
+  $q_string .= "order by inv_name ";
+  $q_inventory = mysql_query($q_string) or die($q_string . ": " . mysql_error() . "\n");
+  while ($a_inventory = mysql_fetch_array($q_inventory)) {
+
+    $target = 'Unknown';
+
+    $q_string  = "select sw_software ";
+    $q_string .= "from software ";
+    $q_string .= "where sw_companyid = " . $a_inventory['inv_id'] . " and sw_type = 'OS' ";
+    $q_software = mysql_query($q_string) or die($q_string . ": " . mysql_error() . "\n");
+    $a_software = mysql_fetch_array($q_software);
+
+    if (strpos($a_software['sw_software'], 'Red Hat') !== false) {
+      $target = 'RHEL 6.7';
+    }
+    if (strpos($a_software['sw_software'], 'CentOS') !== false) {
+      $target = 'CentOS 6.7';
+    }
+    if (strpos($a_software['sw_software'], 'HP-UX') !== false) {
+      $target = 'HP-UX B.11.31';
+    }
+
+    print "\"" . $a_inventory['inv_name'] . "\",\"" . $a_software['sw_software'] . "\",\"" . $target . "\",\"" . $a_inventory['inv_kernel'] . "\"\n";
+  }
+
+?>
