7af09acc013ac272c0d16224a9235db43697256b
Carl Schelin <cschelin@west.com>
2016-04-06 17:18:43 +0000

    Modified the parsing and errors to work better with Remedy

--- bin/changelog.submit.php
+++ bin/changelog.submit.php
@@ -57,15 +57,21 @@
 ### validate parameter list
 #########################
 
-  $q_string  = "select usr_email,usr_clientid,usr_group ";
+  $q_string  = "select usr_email,usr_first,usr_last,usr_name,usr_clientid,usr_group,usr_manager ";
   $q_string .= "from users ";
-  $q_string .= "where (usr_email = '" . $email . "' or usr_altemail like '%" . $email . "%') and usr_id != 1 and usr_disabled = 0";
+  $q_string .= "where (usr_email = '" . $email . "' or usr_altemail like '%" . $email . "%') and usr_id != 1 and usr_disabled = 0 ";
   $q_users = mysql_query($q_string) or die($q_string . ": " . mysql_error());
   $a_users = mysql_fetch_array($q_users);
 
 # reset the email if using an alternate email address (like incojs01).
 #  $email = $a_users['usr_email'];
   $clientid = $a_users['usr_clientid'];
+
+  $q_string  = "select usr_first,usr_last,usr_name,usr_clientid ";
+  $q_string .= "from users ";
+  $q_string .= "where usr_id = " . $a_users['usr_manager'] . " and usr_disabled = 0 ";
+  $q_manager = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $a_manager = mysql_fetch_array($q_manager);
 
   $q_string  = "select grp_changelog,grp_magic ";
   $q_string .= "from groups ";
@@ -94,25 +100,81 @@
     exit(1);
   }
 
-# bail if clientid not set. Can't submit to magic without it.
+# bail if clientid not set. Can't submit to remedy without it.
 # this also fails if email is incorrect including alternate emails.
-  if ($clientid == '') {
-    print "ERROR: ClientID not set\n";
+  if ($a_users['usr_clientid'] == '') {
+    print "ERROR: RemedyID not set\n";
     $headers  = "From: Changelog <changelog@incojs01.scc911.com>\r\n";
 
-    $body  = "Your ClientID has not been set in the Inventory application. In order for changelog messages ";
-    $body .= "to be submitted successfully to Magic, the ClientID must be set. Please go into your ";
-    $body .= "Account Details and add your Magic ClientID to your account and resend the change to ";
-    $body .= "the system.\n\n";
+    $body  = "Your RemedyID has not been set in the Inventory application. In order for changelog messages ";
+    $body .= "to be submitted successfully to Remedy, the RemedyID must be set. This value is the same ";
+    $body .= "as your Windows Login ID. Please go into your Account Details and add your Remedy/Windows ";
+    $body .= "RemedyID to your account and resend the change to the system.\n\n";
 
     $body .= "This message can also be received if your sending email address doesn't match your ";
     $body .= "Inventory email address.\n\n";
 
-    $body .= "https://incojs01/inventory/login/user_admin.php\n";
+    $body .= "https://incojs01/inventory/accounts/profile.php\n";
 
     $email .= ",carl.schelin@intrado.com";
 
-    mail($email, "Error: ClientID Missing", $body, $headers);
+    mail($email, "Error: RemedyID Missing", $body, $headers);
+
+    unlink($changelog);
+    exit(1);
+  }
+
+# bail if manager is not selected. Can't submit to remedy without it.
+  if ($a_users['usr_manager'] == 0) {
+    print "ERROR: Manager not selected\n";
+    $headers  = "From: Changelog <changelog@incojs01.scc911.com>\r\n";
+
+    $body  = "Remedy requires your manager's login information in order to store your changelog entry. You have not ";
+    $body .= "identified your manager in your profile. Log in to your account and select your manager from the list.";
+
+    $body .= "https://incojs01/inventory/accounts/profile.php\n";
+
+    $email .= ",carl.schelin@intrado.com";
+
+    mail($email, "Error: Manager not selected", $body, $headers);
+
+    unlink($changelog);
+    exit(1);
+  }
+
+
+# bail if manager or manager clientid is not set. Can't submit to remedy without it.
+  if ($a_manager['usr_clientid'] == '') {
+    print "ERROR: Manager's RemedyID not set\n";
+    $headers  = "From: Changelog <changelog@incojs01.scc911.com>\r\n";
+
+    $body  = "Your Manager's RemedyID has not been set in the Inventory application. In order for changelog messages ";
+    $body .= "to be submitted successfully to Remedy, your Manager's RemedyID must be set. This value is the same ";
+    $body .= "as their Windows Login ID. Please have your manager go into their Account Details page and add their Remedy/Windows ";
+    $body .= "RemedyID to their account and then you can resend the change to the system.\n\n";
+
+    $body .= "An email was also sent to your manager.\n\n";
+
+    $body .= "https://incojs01/inventory/accounts/profile.php\n";
+
+    $email .= ",carl.schelin@intrado.com";
+
+    mail($email, "Error: Manager's RemedyID Missing", $body, $headers);
+
+# send an email to the manager as well.
+    $headers  = "From: Changelog <changelog@incojs01.scc911.com>\r\n";
+
+    $body  = "Your RemedyID has not been set in the Inventory application. In order for members of your ";
+    $body .= "group to send changelog messages to Remedy, your RemedyID must be set. This value is the same ";
+    $body .= "as your Windows Login ID. Please go into your Account Details and add your Remedy/Windows ";
+    $body .= "RemedyID to your account and notify " . $a_users['usr_first'] . " " . $a_users['usr_last'] . " ";
+    $body .= "so they can resend the change to the system.\n\n";
+
+    $body .= "https://incojs01/inventory/accounts/profile.php\n";
+
+    $email .= ",carl.schelin@intrado.com";
+
+    mail($email, "Error: RemedyID Missing", $body, $headers);
 
     unlink($changelog);
     exit(1);
@@ -203,92 +265,92 @@
 
     exit(1);
   } else {
-  while(!feof($file)) {
-    $process = trim(fgets($file));
+    while(!feof($file)) {
+      $process = trim(fgets($file));
 
-    if (preg_match("/boundary/", $process) && $leave == 0) {
-      $value = split("\"", $process);
-      $boundary = $value[1];
-    }
+      if (preg_match("/boundary/", $process) && $leave == 0) {
+        $value = split("\"", $process);
+        $boundary = $value[1];
+      }
 
 # this is added by the procmail script. lets us skip all the header information
-    if (preg_match("/^--------------/", $process) && $leave == 0) {
-      print "Loop: Past the header lines, reached the separator between header and body\n";
-      while (!feof($file)) {
-        $process = trim(fgets($file));
+      if (preg_match("/^--------------/", $process) && $leave == 0) {
+        print "Loop: Past the header lines, reached the separator between header and body\n";
+        while (!feof($file)) {
+          $process = trim(fgets($file));
 
 # again, if a blackberry (bb uses '__' as signature sep), we're done
-        if (preg_match("/Intrado Wireless Information Network/", $process) || preg_match("/__/", $process) && $leave == 0) {
-          print "Loop-Wireless: Found Blackberry signature line. Saving text, exiting loop.\n";
-          $report .= $savedlines . "\n";
-          $leave = 1;
-          break;
-        }
+          if (preg_match("/Intrado Wireless Information Network/", $process) || preg_match("/__/", $process) && $leave == 0) {
+            print "Loop-Wireless: Found Blackberry signature line. Saving text, exiting loop.\n";
+            $report .= $savedlines . "\n";
+            $leave = 1;
+            break;
+          }
 # save the lines in case it's a plain text message from the blackberry; save after the exit due to the "Wireless" message.
-        if ((preg_match("/^--/", $process) == 0 && 
-             preg_match("/Content-Type: multipart\/alternative/", $process) == 0 && 
-             preg_match("/boundary=\"_/", $process) == 0 && 
-             preg_match("/Content-Type: text\/plain/", $process) == 0 && 
-             preg_match("/Content-Transfer-Encoding:/", $process) == 0) && 
-             $leave == 0
-           ) {
-          print "Loop-Blank: Non-blank line; saving.\n";
-          $savedlines .= $process . "\n";
-        }
+          if ((preg_match("/^--/", $process) == 0 && 
+               preg_match("/Content-Type: multipart\/alternative/", $process) == 0 && 
+               preg_match("/boundary=\"_/", $process) == 0 && 
+               preg_match("/Content-Type: text\/plain/", $process) == 0 && 
+               preg_match("/Content-Transfer-Encoding:/", $process) == 0) && 
+               $leave == 0
+             ) {
+            print "Loop-Blank: Non-blank line; saving.\n";
+            $savedlines .= $process . "\n";
+          }
 
 # on the other hand, if it's an outlook message, parse out the mime encoding.
 # assume the text/plain is the first block of data with the text/html as the second block
 # each block starts with '^--' so after the '-----' line added by procmail, the next line to look for is
 # the Content-Transfer line (the last line of the separator block). Once that's reached, begin parsing each line 
 # until the next '^--' is reached which is either the signature block or the text/html block.
-        if (preg_match("/Content-Transfer-Encoding: quoted-printable/", $process) && $leave == 0) {
-          print "Loop-Content: quoted-printable found.\n";
-          while (!feof($file)) {
-            $process = trim(fgets($file));
-            if (preg_match("/^--/", $process)) {
-              print "Loop-Content: '^--' found. Exiting loop.\n";
-              $leave = 1;
-              break;
-            }
-            if ($process != '') {
-              print "Loop-Content: Non-blank line; removing equals sign and saving.\n";
-              $report .= preg_replace("/=$/", '', $process);
-            }
-          }
-        }
-# need to read it in, then convert it, _then_ loop through the resultent output looking for the *this message has been sent... or -- /r/n lines to save any encoded information
-        if (preg_match("/Content-Transfer-Encoding: base64/", $process) && $leave == 0) {
-          print "Loop-Content: base64 found.\n";
-          while (!feof($file)) {
-            $process = trim(fgets($file));
-            if (preg_match("/^--/", $process)) {
-              print "Loop-Content: '^--' found. Exiting loop.\n";
-              $parse = explode("\n", base64_decode($report));
-              $report = '';
-              for ($i = 0; $i < count($parse); $i++) {
-                if (preg_match("/_______________________/", $parse[$i]) || preg_match("/^--/", $parse[$i])) {
-                  $leave = 1;
-                  break;
-                } else {
-                  $report .= $parse[$i] . "\n";
-                }
+          if (preg_match("/Content-Transfer-Encoding: quoted-printable/", $process) && $leave == 0) {
+            print "Loop-Content: quoted-printable found.\n";
+            while (!feof($file)) {
+              $process = trim(fgets($file));
+              if (preg_match("/^--/", $process)) {
+                print "Loop-Content: '^--' found. Exiting loop.\n";
+                $leave = 1;
+                break;
               }
-              $leave = 1;
-              break;
-            }
-            if ($process != '') {
-              $report .= $process . "\n";
+              if ($process != '') {
+                print "Loop-Content: Non-blank line; removing equals sign and saving.\n";
+                $report .= preg_replace("/=$/", '', $process);
+              }
             }
           }
-        }
+# need to read it in, then convert it, _then_ loop through the resultent output looking for the *this message has been sent... or -- /r/n lines to save any encoded information
+          if (preg_match("/Content-Transfer-Encoding: base64/", $process) && $leave == 0) {
+            print "Loop-Content: base64 found.\n";
+            while (!feof($file)) {
+              $process = trim(fgets($file));
+              if (preg_match("/^--/", $process)) {
+                print "Loop-Content: '^--' found. Exiting loop.\n";
+                $parse = explode("\n", base64_decode($report));
+                $report = '';
+                for ($i = 0; $i < count($parse); $i++) {
+                  if (preg_match("/_______________________/", $parse[$i]) || preg_match("/^--/", $parse[$i])) {
+                    $leave = 1;
+                    break;
+                  } else {
+                    $report .= $parse[$i] . "\n";
+                  }
+                }
+                $leave = 1;
+                break;
+              }
+              if ($process != '') {
+                $report .= $process . "\n";
+              }
+            }
+          }
 # just end when we get to a standard signature on a plain text message.
-        if (preg_match("/^--/", $process) && $leave == 0) {
-          $leave = 1;
+          if (preg_match("/^--/", $process) && $leave == 0) {
+            $leave = 1;
+          }
         }
       }
     }
-  }
-  fclose($file);
+    fclose($file);
   }
   unlink($changelog);
 
@@ -394,17 +456,16 @@
       $remedyserver = "LMV08-REMAR01.corp.intrado.pri";
     }
 
-    $headers = "From: Carl Schelin <carl.schelin@intrado.com>\r\n";
-    $headers .= "CC: carl.schelin@intrado.com\r\n";
-
-
 # get the user information for the person in the inventory and will be the one opening the ticket plus group information
-    $q_string  = "select usr_first,usr_last,usr_name,usr_manager,grp_name ";
+    $q_string  = "select usr_first,usr_last,usr_name,usr_email,usr_manager,grp_name ";
     $q_string .= "from users ";
     $q_string .= "left join groups on groups.grp_id = users.usr_group ";
     $q_string .= "where (usr_email = '" . $email . "' or usr_altemail like '%" . $email . "%') and usr_id != 1 and usr_disabled = 0 ";
     $q_users = mysql_query($q_string) or die($q_string . ": " . mysql_error());
     $a_users = mysql_fetch_array($q_users);
+
+    $headers = "From: " . $a_users['usr_first'] . " " . $a_users['usr_last'] . "<" . $a_users['usr_email'] . ">\r\n";
+    $headers .= "CC: carl.schelin@intrado.com\r\n";
 
 #
 # begin the email message
@@ -424,22 +485,14 @@
     $body .= "Support Organization !1000003227!: Technical Operations\n";
     $body .= "Support Group Name+ !1000003229!: " . $a_users['grp_name'] . "\n";
     $body .= "Change Coordinator+ !1000003230!: " . $a_users['usr_first'] . " " . $a_users['usr_last'] . "\n";
-    $body .= "Change Coordinator Login !1000003231!: " . $a_users['usr_name'] . "\n";
+    $body .= "Change Coordinator Login !1000003231!: " . $clientid . "\n";
 
-    $q_string  = "select usr_first,usr_last,usr_name ";
-    $q_string .= "from users ";
-    $q_string .= "where usr_id = " . $a_users['usr_manager'] . " ";
-    $q_manager = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-    if (mysql_num_rows($q_manager) > 0) {
-      $a_manager = mysql_fetch_array($q_manager);
-
-      $body .= "#Change Manager Details\n";
-      $body .= "Support Company !1000000251!: Intrado, Inc.\n";
-      $body .= "Support Organization !1000000014!: Technical Operations\n";
-      $body .= "Support Group Name !1000000015!: " . $a_users['grp_name'] . "\n";
-      $body .= "Change Manager !1000000403!: " . $a_manager['usr_first'] . " " . $a_manager['usr_last'] . "\n";
-      $body .= "Change Manager Login !1000000408!: " . $a_manager['usr_name'] . "\n";
-    }
+    $body .= "#Change Manager Details\n";
+    $body .= "Support Company !1000000251!: Intrado, Inc.\n";
+    $body .= "Support Organization !1000000014!: Technical Operations\n";
+    $body .= "Support Group Name !1000000015!: " . $a_users['grp_name'] . "\n";
+    $body .= "Change Manager !1000000403!: " . $a_manager['usr_first'] . " " . $a_manager['usr_last'] . "\n";
+    $body .= "Change Manager Login !1000000408!: " . $a_manager['usr_clientid'] . "\n";
 
     $body .= "# Change Dates in the following format 3/8/2016 1:00:00 AM MST\n";
     $body .= "Actual Start Date+ !1000000348!: " . date('n/j/Y g:i:s A e', strtotime("Yesterday")) . "\n";
