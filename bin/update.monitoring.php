f44d27763704b567033589f4333a1b7be1462f0d
Carl Schelin <cschelin@west.com>
2016-09-16 19:06:38 +0000

    updated cleanup and flags

--- bin/update.monitoring.php
+++ bin/update.monitoring.php
@@ -23,6 +23,19 @@
 
   $db = dbconn($DBserver, $DBname, $DBuser, $DBpassword);
 
+  print "Updating all interfaces setting openview, nagios, ping, and ssh off\n";
+
+# turn off openview, nagios, ping, and ssh monitoring for all systems for now.
+  $q_string  = "update interface ";
+  $q_string .= "left join inventory on inventory.inv_id = interface.int_companyid ";
+  $q_string .= "set ";
+  $q_string .= "int_openview = 0,int_nagios = 0,int_ping = 0,int_ssh = 0 ";
+  $q_string .= "where inv_manager = 1 ";
+
+  $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+
+  print "Identify every system and update if HP Openview is installed.\n";
+
 # get all software owned by monitoring and with a vendor of HP
 # it's an assuption but not a bad one.
   $q_string  = "select sw_companyid ";
@@ -33,35 +46,38 @@
 
     $q_string  = "update interface set ";
     $q_string .= "int_openview = 1,int_nagios = 0,int_ping = 1 ";
-    $q_string .= "where int_companyid = " . $a_software['sw_companyid'] . " ";
+    $q_string .= "where int_companyid = " . $a_software['sw_companyid'] . " and int_type = 1 ";
 
     $insert = mysql_query($q_string);
   }
 
-# turn off ssh monitoring for all systems for now.
+  print "Update every console to enable nagios and ping since openview isn't generally enabled to ping\n";
+# okay, all the systems managed by openview are identified. now ping loms where possible
   $q_string  = "update interface ";
   $q_string .= "left join inventory on inventory.inv_id = interface.int_companyid ";
   $q_string .= "set ";
-  $q_string .= "int_ssh = 0 ";
+  $q_string .= "int_nagios = 1,int_ping = 1 ";
+  $q_string .= "where (int_type = 4 or int_type = 6) and inv_manager = 1 ";
+
+  $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+
+  print "Update every management interface to enable nagios and ping but not ssh\n";
+# okay, all the systems managed by openview are identified. now update nagios but all loms as well.
+  $q_string  = "update interface ";
+  $q_string .= "left join inventory on inventory.inv_id = interface.int_companyid ";
+  $q_string .= "set ";
+  $q_string .= "int_nagios = 1,int_ping = 1 ";
   $q_string .= "where int_openview = 0 and (int_type = 1 or int_type = 4 or int_type = 6) and inv_manager = 1 ";
 
   $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
 
-# okay, all the systems managed by openview are identified.
-  $q_string  = "update interface ";
-  $q_string .= "left join inventory on inventory.inv_id = interface.int_companyid ";
-  $q_string .= "set ";
-  $q_string .= "int_nagios = 1,int_ping = 1,int_ssh = 0 ";
-  $q_string .= "where int_openview = 0 and (int_type = 1 or int_type = 4 or int_type = 6) and inv_manager = 1 ";
-
-  $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-
-# okay, enable ssh checks for all systems with ssh
+  print "Update every management interface that's accessible via ssh (inv_ssh = 1) to enable sshing to the system.\n";
+# okay, enable ssh checks for all systems mgt interfaces with ssh
   $q_string  = "update interface ";
   $q_string .= "left join inventory on inventory.inv_id = interface.int_companyid ";
   $q_string .= "set ";
   $q_string .= "int_nagios = 1,int_ping = 1,int_ssh = 1 ";
-  $q_string .= "where int_openview = 0 and (int_type = 1 or int_type = 4 or int_type = 6) and inv_manager = 1 and inv_ssh = 1 ";
+  $q_string .= "where int_openview = 0 and int_type = 1 and inv_manager = 1 and inv_ssh = 1 ";
 
   $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
 
