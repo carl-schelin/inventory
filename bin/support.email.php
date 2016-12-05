c01756645033f62244e596fc8a184d64990e8a2d
Carl Schelin <cschelin@west.com>
2016-12-05 21:42:05 +0000

    Added a line on how to locate the support csv file

--- bin/support.email.php
+++ bin/support.email.php
@@ -169,6 +169,8 @@
 
   $output .= "</table>\n\n";
 
+  $output .= "<p>Note that you can find the csv to validate your listing by going to " . $Siteroot . "/uploads and look for the support-YYYYmmdd.csv file.</p>\n\n";
+
   $output .= "<p>This mail box is not monitored, please do not reply.</p>\n\n";
 
   $output .= "</body>\n";
