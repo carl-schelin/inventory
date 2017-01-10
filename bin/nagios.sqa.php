0f34c2bc31333251c922862e99a4918e8875f320
Carl Schelin <cschelin@west.com>
2017-01-10 17:21:42 +0000

    updated to filter out inactive systems

--- bin/nagios.sqa.php
+++ bin/nagios.sqa.php
@@ -67,9 +67,10 @@
   $q_string .= "grp_name ";
   $q_string .= "from inventory ";
   $q_string .= "left join software on software.sw_companyid = inventory.inv_id ";
+  $q_string .= "left join hardware on hardware.hw_companyid = inventory.inv_id ";
   $q_string .= "left join interface on interface.int_companyid = inventory.inv_id ";
   $q_string .= "left join groups on groups.grp_id = inventory.inv_manager ";
-  $q_string .= "where int_nagios = 1 and inv_status = 0 and sw_type = 'OS' and int_ip6 = 0 and int_type = 1 and inv_location = 39 and inv_manager != 12 ";
+  $q_string .= "where int_nagios = 1 and inv_status = 0 and sw_type = 'OS' and hw_active != '0000-00-00' and int_ip6 = 0 and int_type = 1 and inv_location = 39 and inv_manager != 12 ";
   $q_inventory = mysql_query($q_string) or die($q_string . ": " . mysql_error());
   while ($a_inventory = mysql_fetch_array($q_inventory)) {
 
@@ -189,8 +190,8 @@
         $contactonecomma = ",";
       }
 
-# ssh to servers
-      if ($a_inventory['int_ssh'] == 1) {
+# ssh to servers but only if ssh flag is enabled
+      if ($a_inventory['int_ssh'] == 1 && $a_inventory['inv_ssh'] == 1) {
         $sshservers .= $sshcomma . $a_inventory['inv_name'];
         $sshcomma = ",";
       }
