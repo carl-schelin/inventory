dbd75952573821865fe6dbb941409be5bc6bec73
Carl Schelin <cschelin@west.com>
2017-07-12 21:52:35 +0000

    Updated to add options for opnet, datapalette, centrify, and for disabling netbackup if encryption is used.

--- bin/login.report.php
+++ bin/login.report.php
@@ -31,7 +31,8 @@
 
   $configuration = "";
 
-  $q_string  = "select rsdp_id,rsdp_function,rsdp_processors,rsdp_memory,rsdp_ossize,os_sysname,rsdp_osmonitor,rsdp_application ";
+  $q_string  = "select rsdp_id,rsdp_function,rsdp_processors,rsdp_memory,rsdp_ossize,os_sysname,rsdp_osmonitor,";
+  $q_string .= "rsdp_application,rsdp_opnet,rsdp_datapalette,rsdp_centrify,rsdp_backup ";
   $q_string .= "from rsdp_server ";
   $q_string .= "left join rsdp_osteam on rsdp_osteam.os_rsdp = rsdp_server.rsdp_id ";
   $q_string .= "where rsdp_platform = 1 ";
@@ -106,16 +107,27 @@
     if ($a_rsdp_server['rsdp_osmonitor']) {
       $configuration .= $servername . ":Openview\n";
     }
+    if ($a_rsdp_server['rsdp_opnet']) {
+      $configuration .= $servername . ":OpNet\n";
+    }
+    if ($a_rsdp_server['rsdp_datapalette']) {
+      $configuration .= $servername . ":Datapalette\n";
+    }
+    if ($a_rsdp_server['rsdp_centrify']) {
+      $configuration .= $servername . ":Centrify\n";
+    }
+# if backup checked, then encrypted backups are in place so don't plug netbackup in
+    if ($a_rsdp_server['rsdp_backup'] == 0) {
+      $q_string  = "select bu_retention ";
+      $q_string .= "from rsdp_backups ";
+      $q_string .= "where bu_rsdp = " . $a_rsdp_server['rsdp_id'] . " ";
+      $q_rsdp_backups = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      if (mysql_num_rows($q_rsdp_backups) > 0) {
+        $a_rsdp_backups = mysql_fetch_array($q_rsdp_backups);
 
-    $q_string  = "select bu_retention ";
-    $q_string .= "from rsdp_backups ";
-    $q_string .= "where bu_rsdp = " . $a_rsdp_server['rsdp_id'] . " ";
-    $q_rsdp_backups = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-    if (mysql_num_rows($q_rsdp_backups) > 0) {
-      $a_rsdp_backups = mysql_fetch_array($q_rsdp_backups);
-
-      if ($a_rsdp_backups['bu_retention']) {
-        $configuration .= $servername . ":Netbackup\n";
+        if ($a_rsdp_backups['bu_retention']) {
+          $configuration .= $servername . ":Netbackup\n";
+        }
       }
     }
   }
