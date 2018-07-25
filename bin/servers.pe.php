b37313e3cf9f428a465672539d51d3b7e60bc9ae
Carl Schelin <cschelin@west.com>
2018-07-25 18:47:30 +0000

    Initial revision

---
+++ bin/servers.pe.php
@@ -0,0 +1,88 @@
+#!/usr/local/bin/php
+<?php
+# Script: servers.pe.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description:
+
+  include('settings.php');
+  include($Sitepath . '/function.php');
+
+  function dbconn($server,$database,$user,$pass){
+    $db = mysql_connect($server,$user,$pass);
+    $db_select = mysql_select_db($database,$db);
+    return $db;
+  }
+
+  $db = dbconn($DBserver, $DBname, $DBuser, $DBpassword);
+
+  print "#Server Name(1):FQDN(2):Operating System(3):Time Zone(4):,Tag,(5):,Interface Name,(6):Inventory ID(7):Product Name(8)\n";
+
+  $q_string  = "select inv_id,inv_name,inv_fqdn,inv_ssh,zone_name,prod_name,prj_name ";
+  $q_string .= "from inventory ";
+  $q_string .= "left join zones on zones.zone_id = inventory.inv_zone ";
+  $q_string .= "left join products on products.prod_id = inventory.inv_product ";
+  $q_string .= "left join projects on projects.prj_id = inventory.inv_project ";
+  $q_string .= "where inv_manager = " . $GRP_OpsEng . " and inv_status = 0 ";
+  $q_string .= "order by inv_name";
+  $q_inventory = mysql_query($q_string) or die(mysql_error());
+  while ($a_inventory = mysql_fetch_array($q_inventory)) {
+
+    $os = "";
+    $pre = "";
+    $tags = "";
+
+# add a comment character to the server list for live servers but not ssh'able.
+# scripts use the "^#" part to make sure commented servers are able to use the changelog process
+    if ($a_inventory['inv_ssh'] == 0) {
+      $pre = '#';
+    }
+
+    $os = return_System($a_inventory['inv_id']);
+
+    $tags = '';
+    $q_string  = "select tag_name ";
+    $q_string .= "from tags ";
+    $q_string .= "where tag_inv_id = " . $a_inventory['inv_id'];
+    $q_tags = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+    while ($a_tags = mysql_fetch_array($q_tags)) {
+      $tags .= "," . $a_tags['tag_name'] . ",";
+    }
+
+    $interfaces = '';
+    $q_string  = "select int_server ";
+    $q_string .= "from interface ";
+    $q_string .= "where int_companyid = " . $a_inventory['inv_id'] . " and int_ip6 = 0 and (int_type = 1 || int_type = 2 || int_type = 6)";
+    $q_interface = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+    while ($a_interface = mysql_fetch_array($q_interface)) {
+      $interfaces .= "," . $a_interface['int_server'] . ",";
+    }
+
+    $product = str_replace(" ", "_", $a_inventory['prod_name']);
+    if ($product == '') {
+      $product = "Unassigned";
+    }
+
+    $project = str_replace(" ", "_", $a_inventory['prj_name']);
+    if ($project == '') {
+      $project = "Unassigned";
+    }
+
+    print "$pre" . $a_inventory['inv_name'] . ":" . $a_inventory['inv_fqdn'] . ":$os:" . $a_inventory['zone_name'] . ":$tags:$interfaces:" . $a_inventory['inv_id'] . ":" . $product . ":" . $project . "\n";
+
+  }
+
+# add the centrify application for changelog work
+  $q_string  = "select cl_name ";
+  $q_string .= "from changelog ";
+  $q_string .= "where cl_group = " . $GRP_OpsEng . " and cl_delete = 0 ";
+  $q_string .= "order by cl_name";
+  $q_changelog = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  while ($a_changelog = mysql_fetch_array($q_changelog)) {
+
+    print "#" . $a_changelog['cl_name'] . ":::::," . $a_changelog['cl_name'] . ",:0:" . $a_changelog['cl_name'] . "\n";
+
+  }
+
+?>
