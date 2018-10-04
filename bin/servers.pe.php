8e7a5ab5e2c54283e2a31f8248a64e915d2f882c
Carl Schelin <cschelin@west.com>
2018-10-04 18:44:28 +0000

    Updated to fix group info updated to add group header

--- bin/servers.pe.php
+++ bin/servers.pe.php
@@ -17,14 +17,39 @@
 
   $db = dbconn($DBserver, $DBname, $DBuser, $DBpassword);
 
+  $package     = "servers.pe.php";
+  $mygroup     = $GRP_OpsEng;
+
   print "#Server Name(1):FQDN(2):Operating System(3):Time Zone(4):,Tag,(5):,Interface Name,(6):Inventory ID(7):Product Name(8)\n";
+
+# add a header with settings and email target
+  $q_string  = "select grp_email,grp_status,grp_server,grp_import ";
+  $q_string .= "from groups ";
+  $q_string .= "where grp_id = " . $mygroup . " ";
+  $q_groups = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $a_groups = mysql_fetch_array($q_groups);
+
+  $chkstatus = 'No';
+  if ($a_groups['grp_status']) {
+    $chkstatus = 'Yes';
+  }
+  $chkserver = 'No';
+  if ($a_groups['grp_server']) {
+    $chkserver = 'Yes';
+  }
+  $import = 'No';
+  if ($a_groups['grp_import']) {
+    $import = 'Yes';
+  }
+
+  print "#email: " . $a_groups['grp_email'] . " chkstatus: " . $chkstatus . " chkserver: " . $chkserver . " import: " . $import . "\n";
 
   $q_string  = "select inv_id,inv_name,inv_fqdn,inv_ssh,zone_name,prod_name,prj_name ";
   $q_string .= "from inventory ";
   $q_string .= "left join zones on zones.zone_id = inventory.inv_zone ";
   $q_string .= "left join products on products.prod_id = inventory.inv_product ";
   $q_string .= "left join projects on projects.prj_id = inventory.inv_project ";
-  $q_string .= "where inv_manager = " . $GRP_OpsEng . " and inv_status = 0 ";
+  $q_string .= "where inv_manager = " . $mygroup . " and inv_status = 0 ";
   $q_string .= "order by inv_name";
   $q_inventory = mysql_query($q_string) or die(mysql_error());
   while ($a_inventory = mysql_fetch_array($q_inventory)) {
@@ -76,7 +101,7 @@
 # add the centrify application for changelog work
   $q_string  = "select cl_name ";
   $q_string .= "from changelog ";
-  $q_string .= "where cl_group = " . $GRP_OpsEng . " and cl_delete = 0 ";
+  $q_string .= "where cl_group = " . $mygroup . " and cl_delete = 0 ";
   $q_string .= "order by cl_name";
   $q_changelog = mysql_query($q_string) or die($q_string . ": " . mysql_error());
   while ($a_changelog = mysql_fetch_array($q_changelog)) {
