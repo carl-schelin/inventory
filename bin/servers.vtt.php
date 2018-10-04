27ed781afd6af755814d5dfa8332cef398a3dee2
Carl Schelin <cschelin@west.com>
2018-10-04 18:49:40 +0000

    Updated to fix variables updated to add group header

--- bin/servers.vtt.php
+++ bin/servers.vtt.php
@@ -17,11 +17,36 @@
 
   $db = dbconn($DBserver, $DBname, $DBuser, $DBpassword);
 
+  $package     = "servers.vtt.php";
+  $mygroup     = $GRP_Virtualization;
+
+# add a header with settings and email target
+  $q_string  = "select grp_email,grp_status,grp_server,grp_import ";
+  $q_string .= "from groups ";
+  $q_string .= "where grp_id = " . $mygroup . " ";
+  $q_groups = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $a_groups = mysql_fetch_array($q_groups);
+
+  $chkstatus = 'No';
+  if ($a_groups['grp_status']) {
+    $chkstatus = 'Yes';
+  }
+  $chkserver = 'No';
+  if ($a_groups['grp_server']) {
+    $chkserver = 'Yes';
+  }
+  $import = 'No';
+  if ($a_groups['grp_import']) {
+    $import = 'Yes';
+  }
+
+  print "#email: " . $a_groups['grp_email'] . " chkstatus: " . $chkstatus . " chkserver: " . $chkserver . " import: " . $import . "\n";
+
   $q_string  = "select inv_id,inv_name,inv_fqdn,zone_name,hw_serial,inv_notes ";
   $q_string .= "from inventory ";
   $q_string .= "left join zones on zones.zone_id = inventory.inv_zone ";
   $q_string .= "left join hardware on hardware.hw_companyid = inventory.inv_id ";
-  $q_string .= "where inv_status = 0 and inv_manager = " . $GRP_Virtualization . " and hw_primary = 1 ";
+  $q_string .= "where inv_status = 0 and inv_manager = " . $mygroup . " and hw_primary = 1 ";
   $q_string .= "order by inv_name";
   $q_inventory = mysql_query($q_string) or die(mysql_error());
   while ($a_inventory = mysql_fetch_array($q_inventory)) {
@@ -51,7 +76,7 @@
 # add the centrify application for changelog work
   $q_string  = "select cl_name ";
   $q_string .= "from changelog ";
-  $q_string .= "where cl_group = " . $GRP_Virtualization . " and cl_delete = 0 ";
+  $q_string .= "where cl_group = " . $mygroup . " and cl_delete = 0 ";
   $q_string .= "order by cl_name";
   $q_changelog = mysql_query($q_string) or die($q_string . ": " . mysql_error());
   while ($a_changelog = mysql_fetch_array($q_changelog)) {
