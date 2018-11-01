cb1a623cb06eb5491f448888d34af78a5ac1a77b
Carl Schelin <cschelin@west.com>
2018-11-01 20:43:04 +0000

    Initial revision

---
+++ bin/lcampbell.spreadsheet.php
@@ -0,0 +1,52 @@
+#!/usr/local/bin/php
+<?php
+# Script: lcampbell.spreadsheet.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: 
+# 
+
+# root.cron: # Lennie Campbell requesting spreadsheet output
+# root.cron: 30 8 * * * /usr/local/bin/php /usr/local/httpd/bin/lcampbell.spreadsheet.php > /usr/local/httpd/htsecure/reports/lcampbell.spreadsheet.csv 2>/dev/null
+
+  include('settings.php');
+  include($Sitepath . '/function.php');
+
+  function dbconn($server,$database,$user,$pass){
+    $db = mysql_connect($server,$user,$pass);
+    $db_select = mysql_select_db($database,$db);
+    return $db;
+  }
+
+  $db = dbconn($DBserver, $DBname, $DBuser, $DBpassword);
+
+  print "\"Server Name\",\"Operating System\",\"Location Name\",\"Address\",\"Address\",\"Suite\",\"City\",\"State\",\"Zipcode\",\",\"Make\",\"Model\",\"Function\"\n";
+
+  $q_string  = "select inv_id,inv_name,inv_function,sw_software,mod_vendor,mod_name,loc_name,loc_addr1,loc_addr2,loc_suite,loc_city,loc_state,loc_zipcode ";
+  $q_string .= "from inventory ";
+  $q_string .= "left join software on software.sw_companyid = inventory.inv_id ";
+  $q_string .= "left join locations on locations.loc_id = inventory.inv_location ";
+  $q_string .= "left join hardware on hardware.hw_companyid = inventory.inv_id ";
+  $q_string .= "left join models on models.mod_id = hardware.hw_vendorid ";
+  $q_string .= "where inv_status = 0 and sw_type = 'OS' and inv_manager = 1 and hw_primary = 1 and inv_project = 480 and loc_environment = 1 ";
+  $q_string .= "order by inv_name ";
+  $q_inventory = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  while ($a_inventory = mysql_fetch_array($q_inventory)) {
+
+    print "\"" . $a_inventory['inv_name'] . "\",";
+    print "\"" . $a_inventory['sw_software'] . "\",";
+    print "\"" . $a_inventory['loc_name'] . "\",";
+    print "\"" . $a_inventory['loc_addr1'] . "\",";
+    print "\"" . $a_inventory['loc_addr2'] . "\",";
+    print "\"" . $a_inventory['loc_suite'] . "\",";
+    print "\"" . $a_inventory['loc_city'] . "\",";
+    print "\"" . $a_inventory['loc_state'] . "\",";
+    print "\"" . $a_inventory['loc_zipcode'] . "\",";
+    print "\"" . $a_inventory['mod_vendor'] . "\",";
+    print "\"" . $a_inventory['mod_name'] . "\",";
+    print "\"" . $a_inventory['inv_function'] . "\"\n";
+
+  }
+
+?>
