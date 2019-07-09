39646a49fa0701c2b68cb5113c188d933bd6ff14
Carl Schelin <cschelin@west.com>
2019-07-09 09:59:41 -0600

    Removed all the code; this is a shell so I can get it working.

---
+++ edit/maintenance.fill.php
@@ -0,0 +1,28 @@
+<?php
+# Script: maintenance.fill.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: Fill in the table for editing.
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "maintenance.fill.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($AL_Edit)) {
+;
+    } else {
+      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+    }
+  }
+?>
