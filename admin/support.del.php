af254cc13438a163e65be2b8a93c9d6d151887e9
Carl Schelin <cschelin@west.com>
2019-08-08 11:07:11 -0600

    Updated to fix error output for mysql queries

--- admin/support.del.php
+++ admin/support.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from support ";
       $q_string .= "where sup_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
 
       print "alert('Support Contract deleted.');\n";
 
