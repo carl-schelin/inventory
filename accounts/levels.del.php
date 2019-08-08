695a87df31d6661740f37171ab87ab01822c95e7
Carl Schelin <cschelin@west.com>
2019-08-08 10:32:02 -0600

    Updated to fix gthe error output of mysql queries

--- accounts/levels.del.php
+++ accounts/levels.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from levels ";
       $q_string .= "where lvl_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
 
       print "alert('Level deleted.');\n";
 
