3a865743844fb12f4d412360eed434599386ff3e
Carl Schelin <cschelin@west.com>
2019-08-08 10:28:52 -0600

    Updated to fix the error output of mysql queries

--- accounts/business.del.php
+++ accounts/business.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from business_unit ";
       $q_string .= "where bus_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
 
       print "alert('Business Unit deleted');\n";
 
