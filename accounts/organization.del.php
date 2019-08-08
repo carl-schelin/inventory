6b96bbc4b322cde086757ea96ab4855e6f052d68
Carl Schelin <cschelin@west.com>
2019-08-08 10:37:01 -0600

    Updated to fix the error output of mysql queries

--- accounts/organization.del.php
+++ accounts/organization.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from organizations ";
       $q_string .= "where org_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
 
       print "alert('Organization deleted');\n";
 
