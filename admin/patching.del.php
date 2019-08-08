08479a67c37bb0ee975059c0b09b9e4d2d5314e8
Carl Schelin <cschelin@west.com>
2019-08-08 11:18:06 -0600

    Updated to fix the error output for mysql queries

--- admin/patching.del.php
+++ admin/patching.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from patching ";
       $q_string .= "where patch_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
 
       print "alert('Patch Description deleted.');\n";
 
