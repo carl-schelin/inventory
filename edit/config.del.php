0661da67e302590d2d818ad174682393cc088695
Carl Schelin <cschelin@west.com>
2019-11-11 12:10:57 -0700

    delete configuration script

---
+++ edit/config.del.php
@@ -0,0 +1,35 @@
+<?php
+# Script: association.del.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: Delete association entries
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "association.del.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($AL_Edit)) {
+      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from cluster");
+
+      $q_string  = "delete ";
+      $q_string .= "from cluster ";
+      $q_string .= "where clu_id= " . $formVars['id'];
+      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+
+      print "alert('Association deleted.');\n";
+    } else {
+      logaccess($_SESSION['uid'], $package, "Access denied");
+    }
+  }
+?>
