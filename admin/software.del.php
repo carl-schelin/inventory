0d667a675c52e165842ea1982a14923eac219d80
Carl Schelin <cschelin@west.com>
2019-11-15 14:20:48 -0700

    Delete from the software support table

---
+++ admin/software.del.php
@@ -0,0 +1,37 @@
+<?php
+# Script: software.del.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: 
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "software.del.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel(1)) {
+      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from sw_software");
+
+      $q_string  = "delete ";
+      $q_string .= "from sw_support ";
+      $q_string .= "where sw_id = " . $formVars['id'];
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+
+      print "alert('Software deleted.');\n";
+
+      print "clear_fields();\n";
+    } else {
+      logaccess($_SESSION['uid'], $package, "Access denied");
+    }
+  }
+?>
