58bff7e348c9bd5743b4f0dac7fbbdb94ebdd25f
Carl Schelin <cschelin@west.com>
2019-12-01 20:20:50 -0700

    Delete tags from the tags table

---
+++ admin/tags.del.php
@@ -0,0 +1,37 @@
+<?php
+# Script: tags.del.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: 
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "tags.del.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel(1)) {
+      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from tags");
+
+      $q_string  = "delete ";
+      $q_string .= "from tags ";
+      $q_string .= "where tag_id = " . $formVars['id'];
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+
+      print "alert('Tag deleted.');\n";
+
+      print "clear_fields();\n";
+    } else {
+      logaccess($_SESSION['uid'], $package, "Access denied");
+    }
+  }
+?>
