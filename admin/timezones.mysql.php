68ed5f35455e1e763414b09c1b1b9a64306d4b36
Carl Schelin <cschelin@west.com>
2019-12-05 15:49:21 -0700

    Updated to fix access levels to a variable

--- admin/timezones.mysql.php
+++ admin/timezones.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']               = clean($_GET['id'],               10);
         $formVars['zone_name']        = clean($_GET['zone_name'],        10);
@@ -85,7 +85,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel(1)) {
+      if (check_userlevel($AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -106,7 +106,7 @@
           $linkend   = "</a>";
 
           $output .= "<tr>";
-          if (check_userlevel(1)) {
+          if (check_userlevel($AL_Admin)) {
             $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel   . "</td>";
           }
           $output .= "  <td class=\"ui-widget-content delete\">"   . $linkstart . $a_zones['zone_id']          . $linkend . "</td>";
