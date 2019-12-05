8b6f4a7b8702d1f2e6761bda24c45bb792bb81a6
Carl Schelin <cschelin@west.com>
2019-12-05 15:09:17 -0700

    Updated to fix access levels to a variable

--- hardware/vlans.mysql.php
+++ hardware/vlans.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']               = clean($_GET['id'],                10);
         $formVars['vlan_vlan']        = clean($_GET['vlan_vlan'],         10);
@@ -100,7 +100,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel(1)) {
+      if (check_userlevel($AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -124,7 +124,7 @@
         $linkend   = "</a>";
 
         $output .= "<tr>";
-        if (check_userlevel(1)) {
+        if (check_userlevel($AL_Admin)) {
           $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel . "x"                          . $linkend . "</td>";
         }
         $output .= "  <td class=\"ui-widget-content delete\">" . $linkstart . $a_vlans['vlan_id']          . $linkend . "</td>";
