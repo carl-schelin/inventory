a36545df62763cb7bd89860044f507b937f7b0b4
Carl Schelin <cschelin@west.com>
2019-12-05 16:05:43 -0700

    Updated to fix access levels to a variable

--- rsdp/build/filesystem.del.php
+++ rsdp/build/filesystem.del.php
@@ -20,7 +20,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from rsdp_filesystem");
 
       $q_string  = "delete ";
