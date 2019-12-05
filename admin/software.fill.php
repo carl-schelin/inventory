c2216bb26a0d60089ba5f2d8b35d58dff86a5574
Carl Schelin <cschelin@west.com>
2019-12-05 15:49:21 -0700

    Updated to fix access levels to a variable

--- admin/software.fill.php
+++ admin/software.fill.php
@@ -19,7 +19,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from sw_support");
 
       $q_string  = "select sw_software,sw_eol,sw_eos ";
