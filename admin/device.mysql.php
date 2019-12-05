e5e572ae55db594bb196967260a8aa5e2d234d0d
Carl Schelin <cschelin@west.com>
2019-12-05 15:49:20 -0700

    Updated to fix access levels to a variable

--- admin/device.mysql.php
+++ admin/device.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']                 = clean($_GET['id'],                 10);
         $formVars['dev_type']           = clean($_GET['dev_type'],            5);
