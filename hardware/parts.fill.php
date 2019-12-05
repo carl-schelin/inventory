2555a9a2f789381152aab3aca40283c57c4ae7f2
Carl Schelin <cschelin@west.com>
2019-12-05 15:09:17 -0700

    Updated to fix access levels to a variable

--- hardware/parts.fill.php
+++ hardware/parts.fill.php
@@ -19,7 +19,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from parts");
 
       $q_string  = "select part_name,part_type,part_acronym ";
