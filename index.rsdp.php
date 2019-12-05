b3235e14f0bc8d2a937caca7338fa0719f6254fa
Carl Schelin <cschelin@west.com>
2019-12-05 13:13:44 -0700

    Updated to fix access level to a variable

--- index.rsdp.php
+++ index.rsdp.php
@@ -101,7 +101,7 @@ function attach_group( p_script_url ) {
   <li><a href="<?php print $RSDProot; ?>/rsdp/rsdp.php">Original MyRSDP Page</a> - This is the original listing of your RSDP servers that showed each server and the associated tasks.</li>
   <li><a href="<?php print $RSDProot; ?>/rsdp/rsdp.php?myrsdp=no">Original RSDP Page</a> - This is the original listing of all RSDP servers that showed each server and the associated tasks.</li>
 <?php
-  if (check_userlevel(1)) {
+  if (check_userlevel($AL_Admin)) {
 ?>
   <li><a href="<?php print $RSDProot; ?>/admin/status.php">Manage RSDP Status Entries</a> - Delete individual line items or an entire project from this script.</li>
 <?php
