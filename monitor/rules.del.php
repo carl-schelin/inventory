ba267af6fd32c0085945b558c308131d9a7abf75
Carl Schelin <cschelin@west.com>
2019-12-05 15:22:04 -0700

    Updated to fix access levels to a variable

--- monitor/rules.del.php
+++ monitor/rules.del.php
@@ -19,7 +19,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from rules");
 
       $q_string  = "update ";
