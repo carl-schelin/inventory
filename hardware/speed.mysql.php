229487970dcef784b06cf1b91332ee7d5fc62d5c
Carl Schelin <cschelin@west.com>
2019-12-05 15:09:17 -0700

    Updated to fix access levels to a variable

--- hardware/speed.mysql.php
+++ hardware/speed.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']        = clean($_GET['id'],         10);
         $formVars['spd_text']  = clean($_GET['spd_text'],   30);
@@ -88,7 +88,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel(1)) {
+      if (check_userlevel($AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -107,7 +107,7 @@
           $linkend   = "</a>";
 
           $output .= "<tr>";
-          if (check_userlevel(1)) {
+          if (check_userlevel($AL_Admin)) {
             $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel . 'x'                      . $linkend . "</td>";
           }
           $output .= "  <td class=\"ui-widget-content delete\">" . $linkstart . $a_int_speed['spd_id']   . $linkend . "</td>";
