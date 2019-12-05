5edcc8afa7ca4909978533b982d88c4da6c8cd33
Carl Schelin <cschelin@west.com>
2019-12-05 13:57:13 -0700

    Updated to fix access levels to a variable

--- exclude/exclude.mysql.php
+++ exclude/exclude.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']                 = clean($_GET['id'],             10);
         $formVars['ex_companyid']       = clean($_GET['ex_companyid'],   10);
