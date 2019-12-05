73a577fa1b9d27c436a6bb7589a64e26d38ea352
Carl Schelin <cschelin@west.com>
2019-12-05 15:22:04 -0700

    Updated to fix access levels to a variable

--- monitor/objects.fill.php
+++ monitor/objects.fill.php
@@ -19,7 +19,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from objects");
 
       $q_string  = "select obj_name,obj_deleted ";
