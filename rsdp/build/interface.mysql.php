0b5e0ad98416c88f28b2da0f7bf9acbfc4595ccf
Carl Schelin <cschelin@west.com>
2019-12-05 16:05:43 -0700

    Updated to fix access levels to a variable

--- rsdp/build/interface.mysql.php
+++ rsdp/build/interface.mysql.php
@@ -22,7 +22,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']             = clean($_GET['if_id'],           10);
         $formVars['if_name']        = clean($_GET['if_name'],         60);
