7248f63ffc8e7691ea222bcfea0369b290ecb55c
Carl Schelin <cschelin@west.com>
2019-12-05 16:52:25 -0700

    Updated to fix access levels to a variable

--- rsdp/reports/timelines.php
+++ rsdp/reports/timelines.php
@@ -83,7 +83,7 @@
 
 <script type="text/javascript">
 <?php
-  if (check_userlevel(1)) {
+  if (check_userlevel($AL_Admin)) {
 ?>
 function delete_line( p_script_url ) {
   var answer = confirm("Delete this Server?")
