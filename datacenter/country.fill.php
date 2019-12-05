d22c7f11f74952509f6de00601f521ff7c33e249
Carl Schelin <cschelin@west.com>
2019-12-05 13:48:31 -0700

    Updated to fix access levels to a variable

--- datacenter/country.fill.php
+++ datacenter/country.fill.php
@@ -19,7 +19,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from country");
 
       $q_string  = "select cn_acronym,cn_country ";
