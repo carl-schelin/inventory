4a442a035c4d843756b0213796d80727635cafe6
Carl Schelin <cschelin@west.com>
2019-12-05 15:49:21 -0700

    Updated to fix access levels to a variable

--- admin/service.fill.php
+++ admin/service.fill.php
@@ -19,7 +19,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from service");
 
       $q_string  = "select svc_name,svc_acronym,svc_availability,svc_downtime,svc_mtbf,svc_geographic,svc_mttr,svc_resource,svc_restore ";
