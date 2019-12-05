311975e4cabc9a6301fcb0a0ed104f5e8687530d
Carl Schelin <cschelin@west.com>
2019-12-05 15:09:17 -0700

    Updated to fix access levels to a variable

--- hardware/hardware.del.php
+++ hardware/hardware.del.php
@@ -19,7 +19,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(1)) {
+    if (check_userlevel($AL_Admin)) {
       logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from models");
 
       $q_string  = "delete ";
