eaeadf37379037991e50be135ba2072ce99c4ac8
Carl Schelin <cschelin@west.com>
2019-12-05 13:48:31 -0700

    Updated to fix access levels to a variable

--- datacenter/datacenter.fill.php
+++ datacenter/datacenter.fill.php
@@ -19,7 +19,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from locations");
 
       $q_string  = "select loc_name,loc_addr1,loc_addr2,loc_suite,loc_city,loc_default,loc_type,";
