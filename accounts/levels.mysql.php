c366a3293b4659c2e3869dd80d7a0b3edc33c60d
Carl Schelin <cschelin@west.com>
2019-12-05 13:29:11 -0700

    Updated to fix access levels to a variable

--- accounts/levels.mysql.php
+++ accounts/levels.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel(1)) {
+    if (check_userlevel($AL_Admin)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']            = clean($_GET['id'],            10);
         $formVars['lvl_name']      = clean($_GET['lvl_name'],     255);
@@ -93,7 +93,7 @@
 
       $output .= "<table class=\"ui-styled-table\">";
       $output .= "<tr>";
-      if (check_userlevel(1)) {
+      if (check_userlevel($AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>";
@@ -118,7 +118,7 @@
           }
 
           $output .= "<tr>";
-          if (check_userlevel(1)) {
+          if (check_userlevel($AL_Admin)) {
             $output .= "  <td class=\"" . $class . " delete\">" . $linkdel   . "</td>";
           }
           $output .= "  <td class=\"" . $class . "\">"        . $linkstart . $a_levels['lvl_id']        . $linkend . "</td>";
