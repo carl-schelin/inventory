a580260f4948acd109741ceaa337c41fdfd89000
Carl Schelin <cschelin@west.com>
2019-12-05 15:09:49 -0700

    Updated to fix access levels to a variable

--- hardware/parts.php
+++ hardware/parts.php
@@ -34,7 +34,7 @@
 <script type="text/javascript">
 
 <?php
-  if (check_userlevel(1)) {
+  if (check_userlevel($AL_Admin)) {
 ?>
 function delete_line( p_script_url ) {
   var answer = confirm("Deleting a part could orphan hardware parts in the \ninventory. Deleting a part should only be done when \nyou're sure there are no associated equipment.\n\nDelete this Part?")
