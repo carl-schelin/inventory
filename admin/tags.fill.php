5477216105075dc71f4981f10b1e1e2c8abaf5f3
Carl Schelin <cschelin@west.com>
2019-12-05 15:39:00 -0700

    Updated to fix access levels to a variable

--- admin/tags.fill.php
+++ admin/tags.fill.php
@@ -19,7 +19,7 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from tags");
 
       $q_string  = "select tag_companyid,tag_name,tag_view,tag_owner,tag_group ";
