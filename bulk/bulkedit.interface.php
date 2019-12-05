1345e85206b40a362a62ca76fb42ab333fb411ef
Carl Schelin <cschelin@west.com>
2019-12-05 15:24:07 -0700

    Updated to fix access levels to a variable

--- bulk/bulkedit.interface.php
+++ bulk/bulkedit.interface.php
@@ -22,7 +22,7 @@
 
     $formVars['interface'] = substr($formVars['id'], 3);
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
 
 # get the rsdp id for a quick test
       $q_string  = "select if_rsdp ";
