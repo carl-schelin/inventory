642b0d0aa3ff8c1acc67f9a72ac56cf19f400cb5
Carl Schelin <cschelin@west.com>
2019-12-05 13:48:31 -0700

    Updated to fix access levels to a variable

--- datacenter/country.mysql.php
+++ datacenter/country.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']               = clean($_GET['id'],              10);
         $formVars['cn_acronym']       = clean($_GET['cn_acronym'],      10);
@@ -83,7 +83,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel(1)) {
+      if (check_userlevel($AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -103,7 +103,7 @@
           $linkend   = "</a>";
 
           $output .= "<tr>";
-          if (check_userlevel(1)) {
+          if (check_userlevel($AL_Admin)) {
             $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel    . "</td>";
           }
           $output .= "  <td class=\"ui-widget-content delete\">"   . $linkstart . $a_country['cn_id']          . $linkend . "</td>";
