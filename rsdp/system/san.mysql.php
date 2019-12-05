eee70a91785754777126e6ea1ac8634e44fee278
Carl Schelin <cschelin@west.com>
2019-12-05 16:19:06 -0700

    Updated to fix access levels to a variable

--- rsdp/system/san.mysql.php
+++ rsdp/system/san.mysql.php
@@ -21,7 +21,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel(2)) {
+    if (check_userlevel($AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']           = clean($_GET['id'],           10);
         $formVars['san_wwnnzone'] = clean($_GET['san_wwnnzone'], 20);
