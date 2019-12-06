efd905a7b7d5a179385dbb2c548d6bfe602d5e0b
Carl Schelin <cschelin@west.com>
2019-12-06 14:47:07 -0700

    Updated to use better error checking for the mysql_query

--- reports/endoflife.php
+++ reports/endoflife.php
@@ -177,7 +177,7 @@
   $q_string .= "left join locations on locations.loc_id      = inventory.inv_location ";
   $q_string .= $where . " and sw_type = 'OS' ";
   $q_string .= $orderby;
-  $q_software = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $q_software = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   if (mysql_num_rows($q_software) > 0) {
     while ($a_software = mysql_fetch_array($q_software)) {
 
