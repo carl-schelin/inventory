9bd597e4ee4624a8391fd073696e8960ba08e45c
Carl Schelin <cschelin@west.com>
2019-12-06 14:47:07 -0700

    Updated to use better error checking for the mysql_query

--- reports/dell.php
+++ reports/dell.php
@@ -186,7 +186,7 @@
   $q_string .= "left join products on products.prod_id = inventory.inv_product ";
   $q_string .= $where;
   $q_string .= $orderby;
-  $q_inventory = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $q_inventory = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   while ($a_inventory = mysql_fetch_array($q_inventory)) {
 
     if ($a_inventory['hw_serial'] != "" && $a_inventory['hw_serial'] != "N/A" && $a_inventory['hw_serial'] != "None" && $a_inventory['hw_serial'] != "VM") {
