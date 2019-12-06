5b8f69bf440b30cde296825b933e8443dd7ec44e
Carl Schelin <cschelin@west.com>
2019-12-06 13:57:13 -0700

    Updated to use better error checking for the mysql_query

--- features/ticket.php
+++ features/ticket.php
@@ -25,7 +25,7 @@
   $q_string .= "from features ";
   $q_string .= "left join modules on modules.mod_id = features.feat_module ";
   $q_string .= "where feat_id = " . $formVars['id'];
-  $q_features = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $q_features = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   $a_features = mysql_fetch_array($q_features);
 
   $features = $a_features['mod_name'] . ' Feature: ' . $formVars['id'];
@@ -33,7 +33,7 @@
   $q_string  = "select usr_last,usr_first,usr_phone,usr_email ";
   $q_string .= "from users ";
   $q_string .= "where usr_id = " . $a_features['feat_openby'];
-  $q_users = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $q_users = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   $a_users = mysql_fetch_array($q_users);
 
 ?>
@@ -441,7 +441,7 @@ $(document).ready( function() {
   $q_string .= "from features ";
   $q_string .= "left join modules on modules.mod_id = features.feat_module ";
   $q_string .= "where feat_id = " . $formVars['id'];
-  $q_features = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $q_features = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   $a_features = mysql_fetch_array($q_features);
 
   if ($a_features['feat_closed'] == '0000-00-00') {
@@ -450,7 +450,7 @@ $(document).ready( function() {
     $q_string  = "select mod_id,mod_name ";
     $q_string .= "from modules ";
     $q_string .= "order by mod_name ";
-    $q_modules = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+    $q_modules = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
     while ($a_modules = mysql_fetch_array($q_modules)) {
       if ($a_features['feat_module'] == $a_modules['mod_id']) {
         print "<option selected value=\"" . $a_modules['mod_id'] . "\">" . $a_modules['mod_name'] . "</option>\n";
@@ -606,7 +606,7 @@ The <strong>character count</strong> field shows you the limit of the number of 
   $q_string  = "select usr_first,usr_last ";
   $q_string .= "from users ";
   $q_string .= "where usr_id = " . $_SESSION['uid'];
-  $q_users = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $q_users = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   $a_users = mysql_fetch_array($q_users);
 
   print "<option value=\"" . $_SESSION['uid'] . "\">" . $a_users['usr_first'] . " " . $a_users['usr_last'] . "</option>\n";
@@ -615,7 +615,7 @@ The <strong>character count</strong> field shows you the limit of the number of 
   $q_string .= "from users ";
   $q_string .= "where usr_disabled = 0 ";
   $q_string .= "order by usr_last,usr_first";
-  $q_users = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $q_users = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   while ($a_users = mysql_fetch_array($q_users)) {
     print "<option value=\"" . $a_users['usr_id'] . "\">" . $a_users['usr_first'] . " " . $a_users['usr_last'] . "</option>\n";
   }
