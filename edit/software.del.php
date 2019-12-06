75c589f28d8de3c1b00b4d23092bb234957d3db4
Carl Schelin <cschelin@west.com>
2019-12-06 15:13:45 -0700

    Updated to use better error checking for the mysql_query

--- edit/software.del.php
+++ edit/software.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from software ";
       $q_string .= "where sw_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
 
       print "alert('Software deleted.');\n";
     } else {
