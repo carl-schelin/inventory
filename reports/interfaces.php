1f2fac0bd23c2e27cca0daf624b5cb1ac2d992bb
Carl Schelin <cschelin@west.com>
2019-12-06 14:47:07 -0700

    Updated to use better error checking for the mysql_query

--- reports/interfaces.php
+++ reports/interfaces.php
@@ -69,7 +69,7 @@
 
   $q_string  = "select zone_id,zone_name ";
   $q_string .= "from ip_zones";
-  $q_ip_zones = mysql_query($q_string) or die($q_string . ': ' . mysql_error());
+  $q_ip_zones = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   while ($a_ip_zones = mysql_fetch_array($q_ip_zones)) {
     $zoneval[$a_ip_zones['zone_id']] = $a_ip_zones['zone_name'];
   }
@@ -273,7 +273,7 @@
   $q_string .= "left join inttype on inttype.itp_id = interface.int_type ";
   $q_string .= $where;
   $q_string .= $orderby;
-  $q_interface = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $q_interface = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   while ($a_interface = mysql_fetch_array($q_interface)) {
 
     if ($a_interface['int_primary']) {
