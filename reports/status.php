d6a76054fb1af5ba6111c875030478d389fbeaeb
Carl Schelin <cschelin@west.com>
2019-12-06 14:47:07 -0700

    Updated to use better error checking for the mysql_query

--- reports/status.php
+++ reports/status.php
@@ -211,7 +211,7 @@
   $q_string .= "left join products  on products.prod_id = hardware.hw_product ";
   $q_string .= $where;
   $q_string .= $orderby;
-  $q_interface = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $q_interface = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
   while ($a_interface = mysql_fetch_array($q_interface)) {
 
     $managed++;
