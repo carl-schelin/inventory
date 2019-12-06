a7ca9adb33086b21c1cfadee6d7bac9f9cbd618d
Carl Schelin <cschelin@west.com>
2019-12-06 15:13:44 -0700

    Updated to use better error checking for the mysql_query

--- edit/firewall.del.php
+++ edit/firewall.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from firewall ";
       $q_string .= "where fw_id= " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
 
       print "alert('Firewall Rule deleted.');\n";
     } else {
