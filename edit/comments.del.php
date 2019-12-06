4aba89af39cc10bb671c3791f63648972db6f573
Carl Schelin <cschelin@west.com>
2019-12-06 15:13:44 -0700

    Updated to use better error checking for the mysql_query

--- edit/comments.del.php
+++ edit/comments.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from comments ";
       $q_string .= "where com_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
 
       print "alert('Comment deleted.');\n";
 
