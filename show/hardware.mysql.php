2f6aedd20ee39f40b6f25aac192f596576b9dd71
Carl Schelin <cschelin@west.com>
2020-02-06 10:34:50 -0700

    Updated to fix gthe imgs path for the pencil.gif

--- show/hardware.mysql.php
+++ show/hardware.mysql.php
@@ -30,7 +30,7 @@
   $output .= "  <th class=\"ui-state-default\">";
   if (check_userlevel($AL_Edit)) {
     if (check_grouplevel($a_inventory['inv_manager'])) {
-      $output .= "<a href=\"" . $Editroot . "/inventory.php?server=" . $formVars['id'] . "#hardware\" target=\"_blank\"><img src=\"/inventory/imgs/pencil.gif\">";
+      $output .= "<a href=\"" . $Editroot . "/inventory.php?server=" . $formVars['id'] . "#hardware\" target=\"_blank\"><img src=\"" . $Imgsroot . "/pencil.gif\">";
     }
   }
   $output .= "Hardware Information";
