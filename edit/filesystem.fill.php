531105aa086c5931acf729d8929b41f5e0ddf87c
Carl Schelin <cschelin@west.com>
2020-03-30 20:19:50 +0000

    Updated to add group ownership of a file system on a server

--- edit/filesystem.fill.php
+++ edit/filesystem.fill.php
@@ -22,12 +22,14 @@
     if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from filesystem");
 
-      $q_string  = "select fs_backup,fs_device,fs_mount,fs_size,fs_wwid,fs_subsystem,fs_lun,fs_volume,fs_volid,fs_path ";
+      $q_string  = "select fs_backup,fs_device,fs_mount,fs_group,fs_size,fs_wwid,fs_subsystem,fs_lun,fs_volume,fs_volid,fs_path ";
       $q_string .= "from filesystem ";
       $q_string .= "where fs_id = " . $formVars['id'];
       $q_filesystem = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
       $a_filesystem = mysql_fetch_array($q_filesystem);
       mysql_free_result($q_filesystem);
+
+      $group = return_Index($a_filesystem['fs_group'], "select grp_id from groups where grp_disabled = 0 order by grp_name");
 
       print "document.edit.fs_device.value = '"    . mysql_real_escape_string($a_filesystem['fs_device'])    . "';\n";
       print "document.edit.fs_mount.value = '"     . mysql_real_escape_string($a_filesystem['fs_mount'])     . "';\n";
@@ -38,6 +40,8 @@
       print "document.edit.fs_lun.value = '"       . mysql_real_escape_string($a_filesystem['fs_lun'])       . "';\n";
       print "document.edit.fs_volid.value = '"     . mysql_real_escape_string($a_filesystem['fs_volid'])     . "';\n";
       print "document.edit.fs_path.value = '"      . mysql_real_escape_string($a_filesystem['fs_path'])      . "';\n";
+
+      print "document.edit.fs_group['" . $group . "'].selected = true;\n";
 
       if ($a_filesystem['fs_backup']) {
         print "document.edit.fs_backup.checked = true;\n";
