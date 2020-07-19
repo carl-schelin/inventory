50ca43227db30c2b1c59dcdb02758d274b039b79
Carl Schelin <cschelin@west.com>
2020-07-19 02:36:24 +0000

    Fill in the form with the list of servers getting that patch

---
+++ reports/patches.fill.php
@@ -0,0 +1,66 @@
+<?php
+# Script: patches.fill.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description:
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "patches.fill.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+    $formVars['scheduled'] = date('Y-m-d');
+    if (isset($_GET['scheduled'])) {
+      $formVars['scheduled'] = clean($_GET['scheduled'], 12);
+    }
+
+    if (check_userlevel($AL_Edit)) {
+      logaccess($_SESSION['uid'], $package, "Requesting all records for " . $formVars['id'] . " from bigfix");
+
+      $q_string  = "select big_fixlet,inv_manager,inv_appadmin ";
+      $q_string .= "from bigfix ";
+      $q_string .= "left join inventory on inventory.inv_id = bigfix.big_companyid ";
+      $q_string .= "where big_id = " . $formVars['id'] . " ";
+      $q_bigfix = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+      if (mysql_num_rows($q_bigfix) > 0) {
+        $a_bigfix = mysql_fetch_array($q_bigfix);
+
+        $big_fixlet = $a_bigfix['big_fixlet'];
+        $inv_manager = $a_bigfix['inv_manager'];
+        $inv_appadmin = $a_bigfix['inv_appadmin'];
+#        print "document.getElementById('big_servername').innerHTML = '" . mysql_real_escape_string($a_inventory['inv_name']) . "';";
+      }
+
+      $patches = '';
+      $flagged = '';
+      $q_string  = "select inv_name ";
+      $q_string .= "from bigfix ";
+      $q_string .= "left join inventory on inventory.inv_id = bigfix.big_companyid ";
+      $q_string .= "where big_fixlet = \"" . $big_fixlet . "\" and big_scheduled = \"" . $formVars['scheduled'] . "\" and (inv_manager = " . $inv_manager . " or inv_appadmin = " . $inv_appadmin . ") ";
+      $q_string .= "group by inv_name ";
+      $q_bigfix = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+      if (mysql_num_rows($q_bigfix) > 0) {
+
+        while ($a_bigfix = mysql_fetch_array($q_bigfix)) {
+
+          $patches .= $a_bigfix['inv_name'] . "\n";
+        }
+      }
+      mysql_free_result($q_bigfix);
+
+      print "document.bigfix.big_patches.value = '" . mysql_real_escape_string($patches) . "';\n";
+
+    } else {
+      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+    }
+  }
+?>
