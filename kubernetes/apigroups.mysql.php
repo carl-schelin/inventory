6af3d7dbbb93a50046c1fb24057292572f845e49
Carl Schelin <cschelin@west.com>
2020-08-19 17:30:51 +0000

    process apigroups entries and display the report

---
+++ kubernetes/apigroups.mysql.php
@@ -0,0 +1,128 @@
+<?php
+# Script: apigroups.mysql.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: Retrieve data and update the database with the new info. Prepare and display the table
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "apigroups.mysql.php";
+    $formVars['update'] = clean($_GET['update'], 10);
+
+    if ($formVars['update'] == '') {
+      $formVars['update'] = -1;
+    }
+
+    if (check_userlevel($AL_Edit)) {
+      if ($formVars['update'] == 0 || $formVars['update'] == 1) {
+        $formVars['id']               = clean($_GET['id'],               10);
+        $formVars['api_name']         = clean($_GET['api_name'],        100);
+
+        if ($formVars['id'] == '') {
+          $formVars['id'] = 0;
+        }
+
+        if (strlen($formVars['api_name']) > 0) {
+          logaccess($_SESSION['uid'], $package, "Building the query.");
+
+          $q_string =
+            "api_name        = \"" . $formVars['api_name'] . "\"";
+
+          if ($formVars['update'] == 0) {
+            $query = "insert into apigroups set api_id = NULL, " . $q_string;
+            $message = "apiGroups added.";
+          }
+          if ($formVars['update'] == 1) {
+            $query = "update apigroups set " . $q_string . " where api_id = " . $formVars['id'];
+            $message = "apiGroups updated.";
+          }
+
+          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['api_name']);
+
+          mysql_query($query) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $query . "&mysql=" . mysql_error()));
+
+          print "alert('" . $message . "');\n";
+        } else {
+          print "alert('You must input data before saving changes.');\n";
+        }
+      }
+
+
+      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+
+      $output  = "<p></p>\n";
+      $output .= "<table class=\"ui-styled-table\">\n";
+      $output .= "<tr>\n";
+      $output .= "  <th class=\"ui-state-default\">apiGroups Listing</th>\n";
+      $output .= "  <th class=\"ui-state-default\" width=\"20\"><a href=\"javascript:;\" onmousedown=\"toggleDiv('apigroups-listing-help');\">Help</a></th>\n";
+      $output .= "</tr>\n";
+      $output .= "</table>\n";
+
+      $output .= "<div id=\"apigroups-listing-help\" style=\"display: none\">\n";
+
+      $output .= "<div class=\"main-help ui-widget-content\">\n";
+      $output .= "<ul>\n";
+      $output .= "  <li><strong>apiGroups Listing</strong>\n";
+      $output .= "  <ul>\n";
+      $output .= "    <li><strong>Editing</strong> - Click on an apiGroups to edit it.</li>\n";
+      $output .= "  </ul></li>\n";
+      $output .= "</ul>\n";
+
+      $output .= "</div>\n";
+
+      $output .= "</div>\n";
+
+      $output .= "<table class=\"ui-styled-table\">\n";
+      $output .= "<tr>\n";
+      if (check_userlevel($AL_Admin)) {
+        $output .= "  <th class=\"ui-state-default\" width=\"10\">Del</th>\n";
+      }
+      $output .= "  <th class=\"ui-state-default\" width=\"30\">Id</th>\n";
+      $output .= "  <th class=\"ui-state-default\">apiGroup</th>\n";
+      $output .= "</tr>\n";
+
+      $q_string  = "select api_id,api_name ";
+      $q_string .= "from apigroups ";
+      $q_string .= "order by api_name ";
+      $q_apigroups = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+      if (mysql_num_rows($q_apigroups) > 0) {
+        while ($a_apigroups = mysql_fetch_array($q_apigroups)) {
+
+          $linkstart = "<a href=\"#\" onclick=\"show_file('apigroups.fill.php?id=" . $a_apigroups['api_id'] . "');jQuery('#dialogapiGroups').dialog('open');\">";
+          $linkdel   = "<input type=\"button\" value=\"Remove\" onclick=\"delete_line('apigroups.del.php?id="  . $a_apigroups['api_id'] . "');\">";
+          $linkend   = "</a>";
+
+          $output .= "<tr>";
+          if (check_userlevel($AL_Admin)) {
+            $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel   . "</td>";
+          }
+          $output .= "  <td class=\"ui-widget-content delete\">"   . $linkstart . $a_apigroups['api_id']          . $linkend . "</td>";
+          $output .= "  <td class=\"ui-widget-content\">"          . $linkstart . $a_apigroups['api_name']        . $linkend . "</td>";
+          $output .= "</tr>";
+        }
+      } else {
+        $output .= "<tr>";
+        $output .= "  <td class=\"ui-widget-content\" colspan=\"3\">No records found.</td>";
+        $output .= "</tr>";
+      }
+
+      $output .= "</table>";
+
+      mysql_free_result($q_apigroups);
+
+      print "document.getElementById('apigroups_mysql').innerHTML = '" . mysql_real_escape_string($output) . "';\n\n";
+
+      print "document.apigroups.api_name.value = '';\n";
+
+    } else {
+      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+    }
+  }
+?>
