d99cd03322ef530a366e0f2bea2412620a756c7f
Carl Schelin <cschelin@west.com>
2020-08-19 17:31:31 +0000

    fill in the form to manage resources'

---
+++ kubernetes/resources.fill.php
@@ -0,0 +1,40 @@
+<?php
+# Script: resources.fill.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description:
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "resources.fill.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($AL_Edit)) {
+      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from resources");
+
+      $q_string  = "select res_name ";
+      $q_string .= "from resources ";
+      $q_string .= "where res_id = " . $formVars['id'];
+      $q_resources = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+      $a_resources = mysql_fetch_array($q_resources);
+      mysql_free_result($q_resources);
+
+      print "document.resources.res_name.value = '" . mysql_real_escape_string($a_resources['res_name'])        . "';\n";
+
+      print "document.resources.id.value = " . $formVars['id'] . ";\n";
+
+    } else {
+      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+    }
+  }
+?>
