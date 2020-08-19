0c6665a3d564639f7136ab29160237079bd093fd
Carl Schelin <cschelin@west.com>
2020-08-19 17:31:15 +0000

    delete a resources entry

---
+++ kubernetes/resources.del.php
@@ -0,0 +1,37 @@
+<?php
+# Script: resources.del.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: 
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "resources.del.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($AL_Admin)) {
+      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from resources");
+
+      $q_string  = "delete ";
+      $q_string .= "from resources ";
+      $q_string .= "where res_id = " . $formVars['id'];
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+
+      print "alert('Resources deleted.');\n";
+
+      print "clear_fields();\n";
+    } else {
+      logaccess($_SESSION['uid'], $package, "Access denied");
+    }
+  }
+?>
