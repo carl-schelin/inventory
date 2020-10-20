d4eaac0b7162b68d92c21d99a1e850fcd7f51d1f
Carl Schelin <t.carl.schelin@gmail.com>
2020-10-20 11:15:55 -0600

    Updated to add route source and route static to be filled in.

--- edit/routing.fill.php
+++ edit/routing.fill.php
@@ -22,7 +22,7 @@
     if (check_userlevel($AL_Edit)) {
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from routing");
 
-      $q_string  = "select route_companyid,route_address,route_gateway,route_mask,route_interface,route_desc ";
+      $q_string  = "select route_companyid,route_address,route_gateway,route_mask,route_source,route_interface,route_desc,route_static ";
       $q_string .= "from routing ";
       $q_string .= "where route_id = " . $formVars['id'];
       $q_routing = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
@@ -38,9 +38,16 @@
       print "document.edit.route_address.value = '" . mysql_real_escape_string($a_routing['route_address']) . "';\n";
       print "document.edit.route_gateway.value = '" . mysql_real_escape_string($a_routing['route_gateway']) . "';\n";
       print "document.edit.route_desc.value = '"    . mysql_real_escape_string($a_routing['route_desc'])    . "';\n";
+      print "document.edit.route_source.value = '"  . mysql_real_escape_string($a_routing['route_source'])  . "';\n";
 
       print "document.edit.route_mask['"      . $a_routing['route_mask'] . "'].selected = true;\n";
       print "document.edit.route_interface['" . $interface               . "'].selected = true;\n";
+
+      if ($a_routing['route_static']) {
+        print "document.edit.route_static.checked = true;\n";
+      } else {
+        print "document.edit.route_static.checked = false;\n";
+      }
 
       print "document.edit.route_id.value = " . $formVars['id'] . ";\n";
 
