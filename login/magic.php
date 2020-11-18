8247f3984c92df67176669242d3e273da6514f08
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 19:25:30 -0700

    replace mysql with mysqli

--- login/magic.php
+++ login/magic.php
@@ -84,9 +84,9 @@ if (isset($_POST['mail_user'])) {
   $q_string  = "select usr_id,usr_name,usr_email ";
   $q_string .= "from users ";
   $q_string .= "where usr_id != 1 and usr_disabled = 0 and (usr_name = '" . $search_q . "' or usr_email = '" . $search_q . "')";
-  $q_users = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  if (mysql_num_rows($q_users) == '1') {
-    $a_users = mysql_fetch_array($q_users);
+  $q_users = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  if (mysqli_num_rows($q_users) == '1') {
+    $a_users = mysqli_fetch_array($q_users);
 
     $user_email = $a_users['usr_email'];
 
@@ -98,7 +98,7 @@ if (isset($_POST['mail_user'])) {
       $q_string .= "users ";
       $q_string .= "set usr_magickey = \"" . $magickey . "\",usr_linkexpire=" . (time() + 900) . " ";
       $q_string .= "where usr_id = " . $a_users['usr_id'];
-      $q_newpw = mysql_query($q_string) or die(mysql_error());
+      $q_newpw = mysqli_query($db, $q_string) or die(mysqli_error($db));
 
       $headers  = "From: Inventory Magic Link <root@" . $Sitehttp . ">\r\n";
       $headers .= "MIME-Version: 1.0\r\n";
