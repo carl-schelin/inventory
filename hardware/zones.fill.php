52be27fedae4f305bb8d8adba61f9c77ed987677
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:21:09 -0700

    Updated to replace mysql with mysqli

--- hardware/zones.fill.php
+++ hardware/zones.fill.php
@@ -25,12 +25,12 @@
       $q_string  = "select zone_name,zone_desc ";
       $q_string .= "from ip_zones ";
       $q_string .= "where zone_id = " . $formVars['id'];
-      $q_ip_zones = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      $a_ip_zones = mysql_fetch_array($q_ip_zones);
-      mysql_free_result($q_ip_zones);
+      $q_ip_zones = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      $a_ip_zones = mysqli_fetch_array($q_ip_zones);
+      mysqli_free_result($q_ip_zones);
 
-      print "document.zones.zone_name.value = '" . mysql_real_escape_string($a_ip_zones['zone_name']) . "';\n";
-      print "document.zones.zone_desc.value = '" . mysql_real_escape_string($a_ip_zones['zone_desc']) . "';\n";
+      print "document.zones.zone_name.value = '" . mysqli_real_escape_string($a_ip_zones['zone_name']) . "';\n";
+      print "document.zones.zone_desc.value = '" . mysqli_real_escape_string($a_ip_zones['zone_desc']) . "';\n";
 
       print "document.zones.id.value = " . $formVars['id'] . ";\n";
 
