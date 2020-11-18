c816354019cee8f0d4f2b1947b87138763e3b4eb
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:17:30 -0700

    Updated to replace mysql with mysqli

--- edit/filesystem.fill.php
+++ edit/filesystem.fill.php
@@ -25,21 +25,21 @@
       $q_string  = "select fs_backup,fs_device,fs_mount,fs_group,fs_size,fs_wwid,fs_subsystem,fs_lun,fs_volume,fs_volid,fs_path ";
       $q_string .= "from filesystem ";
       $q_string .= "where fs_id = " . $formVars['id'];
-      $q_filesystem = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_filesystem = mysql_fetch_array($q_filesystem);
-      mysql_free_result($q_filesystem);
+      $q_filesystem = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_filesystem = mysqli_fetch_array($q_filesystem);
+      mysqli_free_result($q_filesystem);
 
       $group = return_Index($a_filesystem['fs_group'], "select grp_id from groups where grp_disabled = 0 order by grp_name");
 
-      print "document.edit.fs_device.value = '"    . mysql_real_escape_string($a_filesystem['fs_device'])    . "';\n";
-      print "document.edit.fs_mount.value = '"     . mysql_real_escape_string($a_filesystem['fs_mount'])     . "';\n";
-      print "document.edit.fs_size.value = '"      . mysql_real_escape_string($a_filesystem['fs_size'])      . "';\n";
-      print "document.edit.fs_wwid.value = '"      . mysql_real_escape_string($a_filesystem['fs_wwid'])      . "';\n";
-      print "document.edit.fs_subsystem.value = '" . mysql_real_escape_string($a_filesystem['fs_subsystem']) . "';\n";
-      print "document.edit.fs_volume.value = '"    . mysql_real_escape_string($a_filesystem['fs_volume'])    . "';\n";
-      print "document.edit.fs_lun.value = '"       . mysql_real_escape_string($a_filesystem['fs_lun'])       . "';\n";
-      print "document.edit.fs_volid.value = '"     . mysql_real_escape_string($a_filesystem['fs_volid'])     . "';\n";
-      print "document.edit.fs_path.value = '"      . mysql_real_escape_string($a_filesystem['fs_path'])      . "';\n";
+      print "document.edit.fs_device.value = '"    . mysqli_real_escape_string($a_filesystem['fs_device'])    . "';\n";
+      print "document.edit.fs_mount.value = '"     . mysqli_real_escape_string($a_filesystem['fs_mount'])     . "';\n";
+      print "document.edit.fs_size.value = '"      . mysqli_real_escape_string($a_filesystem['fs_size'])      . "';\n";
+      print "document.edit.fs_wwid.value = '"      . mysqli_real_escape_string($a_filesystem['fs_wwid'])      . "';\n";
+      print "document.edit.fs_subsystem.value = '" . mysqli_real_escape_string($a_filesystem['fs_subsystem']) . "';\n";
+      print "document.edit.fs_volume.value = '"    . mysqli_real_escape_string($a_filesystem['fs_volume'])    . "';\n";
+      print "document.edit.fs_lun.value = '"       . mysqli_real_escape_string($a_filesystem['fs_lun'])       . "';\n";
+      print "document.edit.fs_volid.value = '"     . mysqli_real_escape_string($a_filesystem['fs_volid'])     . "';\n";
+      print "document.edit.fs_path.value = '"      . mysqli_real_escape_string($a_filesystem['fs_path'])      . "';\n";
 
       print "document.edit.fs_group['" . $group . "'].selected = true;\n";
 
@@ -49,7 +49,7 @@
         print "document.edit.fs_backup.checked = false;\n";
       }
 
-      print "document.edit.fs_id.value = "         . mysql_real_escape_string($formVars['id'])               . ";\n";
+      print "document.edit.fs_id.value = "         . mysqli_real_escape_string($formVars['id'])               . ";\n";
 
       print "document.edit.fs_update.disabled = false;\n";
 
