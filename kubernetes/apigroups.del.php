2e52a5c269029d9a1e0c9ae18b5155b844c9434e
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 18:23:17 -0700

    convert to mysqli

--- kubernetes/apigroups.del.php
+++ kubernetes/apigroups.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from apigroups ";
       $q_string .= "where api_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+      $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
 
       print "alert('apiGroups deleted.');\n";
 
