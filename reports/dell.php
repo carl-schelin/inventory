49e2729a5f89cef77f34f2e5bc3eb1aefc3ea88c
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:21:56 -0700

    Updated to replace mysql with mysqli

--- reports/dell.php
+++ reports/dell.php
@@ -186,8 +186,8 @@
   $q_string .= "left join products on products.prod_id = inventory.inv_product ";
   $q_string .= $where;
   $q_string .= $orderby;
-  $q_inventory = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-  while ($a_inventory = mysql_fetch_array($q_inventory)) {
+  $q_inventory = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+  while ($a_inventory = mysqli_fetch_array($q_inventory)) {
 
     if ($a_inventory['hw_serial'] != "" && $a_inventory['hw_serial'] != "N/A" && $a_inventory['hw_serial'] != "None" && $a_inventory['hw_serial'] != "VM") {
 
