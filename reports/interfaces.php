1f722f478584095e2fc39947664a7fc953fa9e61
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:22:21 -0700

    Updated to replace mysql with mysqli

--- reports/interfaces.php
+++ reports/interfaces.php
@@ -69,8 +69,8 @@
 
   $q_string  = "select zone_id,zone_name ";
   $q_string .= "from ip_zones";
-  $q_ip_zones = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-  while ($a_ip_zones = mysql_fetch_array($q_ip_zones)) {
+  $q_ip_zones = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+  while ($a_ip_zones = mysqli_fetch_array($q_ip_zones)) {
     $zoneval[$a_ip_zones['zone_id']] = $a_ip_zones['zone_name'];
   }
 
@@ -273,8 +273,8 @@
   $q_string .= "left join inttype on inttype.itp_id = interface.int_type ";
   $q_string .= $where;
   $q_string .= $orderby;
-  $q_interface = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-  while ($a_interface = mysql_fetch_array($q_interface)) {
+  $q_interface = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+  while ($a_interface = mysqli_fetch_array($q_interface)) {
 
     if ($a_interface['int_primary']) {
       $class = " class=\"ui-state-highlight\"";
