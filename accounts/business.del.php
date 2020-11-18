12c8eea47873ada75fa66594d9dca34242139a52
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:05:10 -0700

    replaced mysql with mysqli

--- accounts/business.del.php
+++ accounts/business.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from business_unit ";
       $q_string .= "where bus_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+      $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
 
       print "alert('Business Unit deleted');\n";
 
