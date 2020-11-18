c22020e423200e376ccafec334916fce64718b93
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:20:56 -0700

    Updated to replace mysql with mysqli

--- hardware/type.fill.php
+++ hardware/type.fill.php
@@ -25,13 +25,13 @@
       $q_string  = "select itp_name,itp_acronym,itp_description ";
       $q_string .= "from inttype ";
       $q_string .= "where itp_id = " . $formVars['id'];
-      $q_inttype = mysql_query($q_string) or die ($q_string . ": " . mysql_error());
-      $a_inttype = mysql_fetch_array($q_inttype);
-      mysql_free_result($q_inttype);
+      $q_inttype = mysqli_query($db, $q_string) or die ($q_string . ": " . mysqli_error($db));
+      $a_inttype = mysqli_fetch_array($q_inttype);
+      mysqli_free_result($q_inttype);
 
-      print "document.interfacetype.itp_name.value = '"        . mysql_real_escape_string($a_inttype['itp_name'])        . "';\n";
-      print "document.interfacetype.itp_acronym.value = '"     . mysql_real_escape_string($a_inttype['itp_acronym'])     . "';\n";
-      print "document.interfacetype.itp_description.value = '" . mysql_real_escape_string($a_inttype['itp_description']) . "';\n";
+      print "document.interfacetype.itp_name.value = '"        . mysqli_real_escape_string($a_inttype['itp_name'])        . "';\n";
+      print "document.interfacetype.itp_acronym.value = '"     . mysqli_real_escape_string($a_inttype['itp_acronym'])     . "';\n";
+      print "document.interfacetype.itp_description.value = '" . mysqli_real_escape_string($a_inttype['itp_description']) . "';\n";
 
       print "document.interfacetype.id.value = " . $formVars['id'] . ";\n";
 
