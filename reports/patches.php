ac0ba169c70b7755c81c78a6329dd0a12319f25e
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:23:08 -0700

    Updated to replace mysql with mysqli

--- reports/patches.php
+++ reports/patches.php
@@ -144,9 +144,9 @@ $(document).ready( function() {
   $q_string .= "left join inventory on inventory.inv_id       = bigfix.big_companyid ";
   $q_string .= "where (inv_manager = " . $formVars['group'] . " or inv_appadmin = " . $formVars['group'] . ") and big_scheduled = \"" . $formVars['scheduled'] . "\" ";
   $q_string .= "order by big_severity,big_fixlet ";
-  $q_bigfix = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-  if (mysql_num_rows($q_bigfix) > 0) {
-    while ($a_bigfix = mysql_fetch_array($q_bigfix)) {
+  $q_bigfix = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+  if (mysqli_num_rows($q_bigfix) > 0) {
+    while ($a_bigfix = mysqli_fetch_array($q_bigfix)) {
 
       $linkstart = "<a href=\"#\" onclick=\"show_file('patches.fill.php?id=" . $a_bigfix['big_id'] . "&scheduled=" . $formVars['scheduled'] . "');jQuery('#dialogBigFix').dialog('open');return false;\">";
       $linkend   = "</a>";
