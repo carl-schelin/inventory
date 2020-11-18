416b259a1a689ebd7ed6f59bcd747796670fa8ca
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:07:27 -0700

    replaced mysql with mysqli

--- accounts/levels.fill.php
+++ accounts/levels.fill.php
@@ -25,12 +25,12 @@
       $q_string  = "select lvl_name,lvl_level,lvl_disabled ";
       $q_string .= "from levels ";
       $q_string .= "where lvl_id = " . $formVars['id'];
-      $q_levels = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_levels = mysql_fetch_array($q_levels);
-      mysql_free_result($q_levels);
+      $q_levels = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_levels = mysqli_fetch_array($q_levels);
+      mysqli_free_result($q_levels);
 
-      print "document.levels.lvl_name.value = '"  . mysql_real_escape_string($a_levels['lvl_name'])  . "';\n";
-      print "document.levels.lvl_level.value = '" . mysql_real_escape_string($a_levels['lvl_level']) . "';\n";
+      print "document.levels.lvl_name.value = '"  . mysqli_real_escape_string($a_levels['lvl_name'])  . "';\n";
+      print "document.levels.lvl_level.value = '" . mysqli_real_escape_string($a_levels['lvl_level']) . "';\n";
 
       print "document.levels.lvl_disabled['" . $a_levels['lvl_disabled'] . "'].selected = 'true';\n";
 
