88faf996effbaba6c4769f3d0a9b26b9e8b40879
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:07:48 -0700

    replaced mysql with mysqli

--- accounts/organization.fill.php
+++ accounts/organization.fill.php
@@ -25,11 +25,11 @@
       $q_string  = "select org_name ";
       $q_string .= "from organizations ";
       $q_string .= "where org_id = " . $formVars['id'];
-      $q_organizations = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_organizations = mysql_fetch_array($q_organizations);
-      mysql_free_result($q_organizations);
+      $q_organizations = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_organizations = mysqli_fetch_array($q_organizations);
+      mysqli_free_result($q_organizations);
 
-      print "document.organization.org_name.value = '" . mysql_real_escape_string($a_organizations['org_name']) . "';\n";
+      print "document.organization.org_name.value = '" . mysqli_real_escape_string($a_organizations['org_name']) . "';\n";
 
       print "document.organization.id.value = " . $formVars['id'] . ";\n";
 
