3b106de2774693360dcde02886b96c5bd5c46aa3
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:17:04 -0700

    Updated to replace mysql with mysqli

--- edit/backups.fill.php
+++ edit/backups.fill.php
@@ -27,20 +27,20 @@
       $q_string .= "bu_suntime,bu_montime,bu_tuetime,bu_wedtime,bu_thutime,bu_fritime,bu_sattime,bu_notes ";
       $q_string .= "from backups ";
       $q_string .= "where bu_companyid = " . $formVars['id'];
-      $q_backups = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_backups = mysql_fetch_array($q_backups);
+      $q_backups = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_backups = mysqli_fetch_array($q_backups);
 
-      if (mysql_num_rows($q_backups) > 0) {
+      if (mysqli_num_rows($q_backups) > 0) {
 
-        print "document.edit.bu_start.value = '"   . mysql_real_escape_string($a_backups['bu_start'])   . "';\n";
-        print "document.edit.bu_suntime.value = '" . mysql_real_escape_string($a_backups['bu_suntime']) . "';\n";
-        print "document.edit.bu_montime.value = '" . mysql_real_escape_string($a_backups['bu_montime']) . "';\n";
-        print "document.edit.bu_tuetime.value = '" . mysql_real_escape_string($a_backups['bu_tuetime']) . "';\n";
-        print "document.edit.bu_wedtime.value = '" . mysql_real_escape_string($a_backups['bu_wedtime']) . "';\n";
-        print "document.edit.bu_thutime.value = '" . mysql_real_escape_string($a_backups['bu_thutime']) . "';\n";
-        print "document.edit.bu_fritime.value = '" . mysql_real_escape_string($a_backups['bu_fritime']) . "';\n";
-        print "document.edit.bu_sattime.value = '" . mysql_real_escape_string($a_backups['bu_sattime']) . "';\n";
-        print "document.edit.bu_notes.value = '"   . mysql_real_escape_string($a_backups['bu_notes'])   . "';\n";
+        print "document.edit.bu_start.value = '"   . mysqli_real_escape_string($a_backups['bu_start'])   . "';\n";
+        print "document.edit.bu_suntime.value = '" . mysqli_real_escape_string($a_backups['bu_suntime']) . "';\n";
+        print "document.edit.bu_montime.value = '" . mysqli_real_escape_string($a_backups['bu_montime']) . "';\n";
+        print "document.edit.bu_tuetime.value = '" . mysqli_real_escape_string($a_backups['bu_tuetime']) . "';\n";
+        print "document.edit.bu_wedtime.value = '" . mysqli_real_escape_string($a_backups['bu_wedtime']) . "';\n";
+        print "document.edit.bu_thutime.value = '" . mysqli_real_escape_string($a_backups['bu_thutime']) . "';\n";
+        print "document.edit.bu_fritime.value = '" . mysqli_real_escape_string($a_backups['bu_fritime']) . "';\n";
+        print "document.edit.bu_sattime.value = '" . mysqli_real_escape_string($a_backups['bu_sattime']) . "';\n";
+        print "document.edit.bu_notes.value = '"   . mysqli_real_escape_string($a_backups['bu_notes'])   . "';\n";
 
         print "document.edit.bu_retention['" . $a_backups['bu_retention'] . "'].selected = true;\n";
 
@@ -61,7 +61,7 @@
         print "document.edit.bu_id.value = '" . $a_backups['bu_id'] . "'\n";
       }
 
-      mysql_free_result($q_backups);
+      mysqli_free_result($q_backups);
 
     } else {
       logaccess($_SESSION['uid'], $package, "Unauthorized access.");
