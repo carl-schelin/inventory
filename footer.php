034694eb37c2f88daa2d6ca8116b8c1739f91e8a
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 19:36:50 -0700

    replace mysql with mysqli

--- footer.php
+++ footer.php
@@ -6,11 +6,11 @@
     $q_string .= "from grouplist ";
     $q_string .= "left join groups on groups.grp_id = grouplist.gpl_group ";
     $q_string .= "where gpl_user = " . $_SESSION['uid'];
-    $q_grouplist = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=footer.php&error=" . $q_string . "&mysql=" . mysql_error()));
-    if (mysql_num_rows($q_grouplist) > 0) {
+    $q_grouplist = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=footer.php&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+    if (mysqli_num_rows($q_grouplist) > 0) {
       print "<p style=\"text-align: center;\">You are currently a member of the following groups: ";
       $comma = "";
-      while ($a_grouplist = mysql_fetch_array($q_grouplist)) {
+      while ($a_grouplist = mysqli_fetch_array($q_grouplist)) {
         if ($a_grouplist['grp_id'] == $_SESSION['group']) {
           print $comma . "<strong><u>" . $a_grouplist['grp_name'] . "</u></strong>";
         } else {
