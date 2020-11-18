038d6ae5f3e0a8cd31c274ebd7f36dcbe3e4dc14
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:18:30 -0700

    Updated to replace mysql with mysqli

--- edit/tags.del.php
+++ edit/tags.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from tags ";
       $q_string .= "where tag_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+      $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
 
       print "alert('Tag deleted.');\n";
     } else {
