650292583e7fc08d4f2261b1d67cffc61f92c057
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 18:31:42 -0700

    replaced mysql with mysqli

--- kubernetes/apigroups.fill.php
+++ kubernetes/apigroups.fill.php
@@ -25,11 +25,11 @@
       $q_string  = "select api_name ";
       $q_string .= "from apigroups ";
       $q_string .= "where api_id = " . $formVars['id'];
-      $q_apigroups = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_apigroups = mysql_fetch_array($q_apigroups);
-      mysql_free_result($q_apigroups);
+      $q_apigroups = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_apigroups = mysqli_fetch_array($db, $q_apigroups);
+      mysqli_free_result($q_apigroups);
 
-      print "document.apigroups.api_name.value = '" . mysql_real_escape_string($a_apigroups['api_name'])        . "';\n";
+      print "document.apigroups.api_name.value = '" . mysqli_real_escape_string($db, $a_apigroups['api_name']) . "';\n";
 
       print "document.apigroups.id.value = " . $formVars['id'] . ";\n";
 
