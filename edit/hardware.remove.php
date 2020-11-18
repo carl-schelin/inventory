5ce4839e6b962be5b72cbcdf6898217d37d73834
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-17 21:17:52 -0700

    Updated to replace mysql with mysqli

--- edit/hardware.remove.php
+++ edit/hardware.remove.php
@@ -33,23 +33,23 @@
       $q_string  = "select hw_companyid ";
       $q_string .= "from hardware ";
       $q_string .= "where hw_id = " . $formVars['id'] . " ";
-      $q_hardware = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_hardware = mysql_fetch_array($q_hardware);
+      $q_hardware = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_hardware = mysqli_fetch_array($q_hardware);
 
       $q_string  = "select hw_id ";
       $q_string .= "from hardware ";
       $q_string .= "where hw_companyid = " . $a_hardware['hw_companyid'] . " and hw_id != " . $formVars['id'] . " and hw_primary = 1 ";
-      $q_hardware = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      if (mysql_num_rows($q_hardware) > 0) {
+      $q_hardware = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      if (mysqli_num_rows($q_hardware) > 0) {
         $q_string  = "update hardware ";
         $q_string .= "set hw_deleted = 1,hw_primary = 0,hw_hw_id = 0,hw_hd_id = 0,hw_retired = '" . $formVars['retired'] . "' ";
         $q_string .= "where hw_id = " . $formVars['id'] . " ";
-        $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+        $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
 
         $q_string  = "update hardware ";
         $q_string .= "set hw_hw_id = 0,hw_hd_id = 0 ";
         $q_string .= "where hw_hw_id = " . $formVars['id'] . " or hw_hd_id = " . $formVars['id'] . " ";
-        $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+        $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
 
         print "alert('Hardware updated.');\n";
       } else {
