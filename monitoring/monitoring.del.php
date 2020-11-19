566adb6e5b78f75368b947831d946af057b9965e
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-18 17:21:05 -0700

    Replaced mysql with mysqli

---
+++ monitoring/monitoring.del.php
@@ -0,0 +1,37 @@
+<?php
+# Script: monitoring.del.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: 
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "monitoring.del.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($AL_Admin)) {
+      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from monitoring");
+
+      $q_string  = "delete ";
+      $q_string .= "from monitoring ";
+      $q_string .= "where mon_id = " . $formVars['id'];
+      $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+
+      print "alert('Monitoring deleted');\n";
+
+      print "clear_fields();\n";
+    } else {
+      logaccess($_SESSION['uid'], $package, "Access denied");
+    }
+  }
+?>
