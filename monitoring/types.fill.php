f41b1bb54a787b05252b79043ba6f3a8028279e5
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-18 17:21:47 -0700

    Replaced mysql with mysqli

---
+++ monitoring/types.fill.php
@@ -0,0 +1,40 @@
+<?php
+# Script: types.fill.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: 
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "types.fill.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($AL_Edit)) {
+      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from mon_type");
+
+      $q_string  = "select mt_name ";
+      $q_string .= "from mon_type ";
+      $q_string .= "where mt_id = " . $formVars['id'];
+      $q_mon_type = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_mon_type = mysqli_fetch_array($q_mon_type);
+      mysqli_free_result($q_mon_type);
+
+      print "document.types.mt_name.value = '" . mysqli_real_escape_string($a_mon_type['mt_name']) . "';\n";
+
+      print "document.types.id.value = " . $formVars['id'] . ";\n";
+
+    } else {
+      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+    }
+  }
+?>
