6c5667c5895813926b3528bb6b03233c60225d97
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-18 17:21:23 -0700

    Replaced mysql with mysqli

---
+++ monitoring/monitoring.fill.php
@@ -0,0 +1,62 @@
+<?php
+# Script: monitoring.fill.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: 
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "monitoring.fill.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($AL_Edit)) {
+      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from organization");
+
+#+-----------------+---------+------+-----+---------+----------------+
+#| Field           | Type    | Null | Key | Default | Extra          |
+#+-----------------+---------+------+-----+---------+----------------+
+#| mon_id          | int(10) | NO   | PRI | NULL    | auto_increment |
+#| mon_interfaceid | int(10) | NO   |     | 0       |                |
+#| mon_openview    | int(10) | NO   |     | 0       |                |
+#| mon_nagios      | int(10) | NO   |     | 0       |                |
+#| mon_type        | int(10) | NO   |     | 0       |                |
+#| mon_active      | int(10) | NO   |     | 0       |                |
+#| mon_group       | int(10) | NO   |     | 0       |                |
+#| mon_user        | int(10) | NO   |     | 0       |                |
+#| mon_notify      | int(10) | NO   |     | 0       |                |
+#| mon_hours       | int(10) | NO   |     | 0       |                |
+#+-----------------+---------+------+-----+---------+----------------+
+
+      $q_string  = "select mon_openview,mon_nagios,mon_type,mon_active,mon_group,mon_user,mon_notify,mon_hours ";
+      $q_string .= "from monitoring ";
+      $q_string .= "where mon_id = " . $formVars['id'];
+      $q_monitoring = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_monitoring = mysqli_fetch_array($q_monitoring);
+      mysqli_free_result($q_monitoring);
+
+      print "document.monitoring.mon_openview.value = '" . mysqli_real_escape_string($a_monitoring['mon_openview']) . "';\n";
+      print "document.monitoring.mon_openview.value = '" . mysqli_real_escape_string($a_monitoring['mon_openview']) . "';\n";
+      print "document.monitoring.mon_openview.value = '" . mysqli_real_escape_string($a_monitoring['mon_openview']) . "';\n";
+      print "document.monitoring.mon_openview.value = '" . mysqli_real_escape_string($a_monitoring['mon_openview']) . "';\n";
+      print "document.monitoring.mon_openview.value = '" . mysqli_real_escape_string($a_monitoring['mon_openview']) . "';\n";
+      print "document.monitoring.mon_openview.value = '" . mysqli_real_escape_string($a_monitoring['mon_openview']) . "';\n";
+      print "document.monitoring.mon_openview.value = '" . mysqli_real_escape_string($a_monitoring['mon_openview']) . "';\n";
+      print "document.monitoring.mon_openview.value = '" . mysqli_real_escape_string($a_monitoring['mon_openview']) . "';\n";
+
+      print "document.monitoring.id.value = " . $formVars['id'] . ";\n";
+
+    } else {
+      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+    }
+  }
+?>
