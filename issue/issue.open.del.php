d55313cc5a7206830b4f6e63a46fb0438c0155d9
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:51:23 -0700

    Replaced mysql with mysqli

--- issue/issue.open.del.php
+++ issue/issue.open.del.php
@@ -23,28 +23,28 @@
       $q_string  = "select det_id ";
       $q_string .= "from issue_detail ";
       $q_string .= "where det_issue = " . $formVars['id'];
-      $q_issue_detail = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      while ($a_issue_detail = mysql_fetch_array($q_issue_detail)) {
+      $q_issue_detail = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      while ($a_issue_detail = mysqli_fetch_array($q_issue_detail)) {
 
         logaccess($_SESSION['uid'], $package, "Deleting " . $a_issue_detail['det_id'] . " from issue_detail");
 
         $q_string  = "delete ";
         $q_string .= "from issue_detail ";
         $q_string .= "where det_id = " . $a_issue_detail['det_id'];
-        $result = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+        $result = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
       }
 
       $q_string  = "select sup_id ";
       $q_string .= "from issue_support ";
       $q_string .= "where sup_issue = " . $formVars['id'];
-      $q_issue_support = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      while ($a_issue_support = mysql_fetch_array($q_issue_support)) {
+      $q_issue_support = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      while ($a_issue_support = mysqli_fetch_array($q_issue_support)) {
         logaccess($_SESSION['uid'], $package, "Deleting " . $a_issue_support['sup_id'] . " from sup_issue");
 
         $q_string  = "delete ";
         $q_string .= "from issue_support ";
         $q_string .= "where sup_id = " . $a_issue_support['sup_id'];
-        $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+        $insert = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
       }
 
       logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from issue");
@@ -52,7 +52,7 @@
       $q_string  = "delete ";
       $q_string .= "from issue ";
       $q_string .= "where iss_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
 
       print "alert('Issue deleted.');\n";
 
