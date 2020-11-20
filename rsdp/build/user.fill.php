a26707131e6632d97ddda21e4005a3dafb393485
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 21:11:59 -0700

    Replaced mysql with mysqli

--- rsdp/build/user.fill.php
+++ rsdp/build/user.fill.php
@@ -26,16 +26,16 @@
       $q_string  = "select usr_phone,usr_email,usr_deptname ";
       $q_string .= "from users ";
       $q_string .= "where usr_id = " . $formVars['rsdp_requestor'];
-      $q_users = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      $a_users = mysql_fetch_array($q_users);
-      mysql_free_result($q_users);
+      $q_users = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      $a_users = mysqli_fetch_array($q_users);
+      mysqli_free_result($q_users);
 
       $department = return_Index($a_users['usr_deptname'], "select dep_id from department left join business_unit on business_unit.bus_unit = department.dep_unit order by bus_name,dep_name");
 
       print "document.rsdp.usr_deptname['" . $department . "'].selected = true;\n";
 
-      print "document.rsdp.usr_phone.value = '" . mysql_real_escape_string($a_users['usr_phone']) . "';\n";
-      print "document.rsdp.usr_email.value = '" . mysql_real_escape_string($a_users['usr_email']) . "';\n";
+      print "document.rsdp.usr_phone.value = '" . mysqli_real_escape_string($a_users['usr_phone']) . "';\n";
+      print "document.rsdp.usr_email.value = '" . mysqli_real_escape_string($a_users['usr_email']) . "';\n";
 
     } else {
       logaccess($_SESSION['uid'], $package, "Unauthorized access. (" . $formVars['rsdp'] . ")");
