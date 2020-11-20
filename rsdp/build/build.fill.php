3308f41eb40e8d4477f534c5db7f32954c1e543c
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 21:11:12 -0700

    Replaced mysql with mysqli

--- rsdp/build/build.fill.php
+++ rsdp/build/build.fill.php
@@ -27,23 +27,23 @@
       $q_string .= "from rsdp_platform ";
       $q_string .= "where pf_rsdp = " . $formVars['rsdp'] . " ";
       $q_string .= "order by pf_id ";
-      $q_rsdp_platform = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      $numrows = mysql_num_rows($q_rsdp_platform);
+      $q_rsdp_platform = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      $numrows = mysqli_num_rows($q_rsdp_platform);
 
-      if (mysql_num_rows($q_rsdp_platform) > 0) {
-        $a_rsdp_platform = mysql_fetch_array($q_rsdp_platform);
+      if (mysqli_num_rows($q_rsdp_platform) > 0) {
+        $a_rsdp_platform = mysqli_fetch_array($q_rsdp_platform);
 
         $pf_model = return_Index($a_rsdp_platform['pf_model'], "select mod_id from models where mod_primary = 1 order by mod_vendor,mod_name");
 
         print "document.rsdp.pf_model['" . $pf_model . "'].selected = true;\n";
 
-        print "document.rsdp.pf_asset.value = '"   . mysql_real_escape_string($a_rsdp_platform['pf_asset'])   . "';\n";
-        print "document.rsdp.pf_serial.value = '"  . mysql_real_escape_string($a_rsdp_platform['pf_serial'])  . "';\n";
+        print "document.rsdp.pf_asset.value = '"   . mysqli_real_escape_string($a_rsdp_platform['pf_asset'])   . "';\n";
+        print "document.rsdp.pf_serial.value = '"  . mysqli_real_escape_string($a_rsdp_platform['pf_serial'])  . "';\n";
 
-        print "document.rsdp.pf_row.value = '"     . mysql_real_escape_string($a_rsdp_platform['pf_row'])     . "';\n";
-        print "document.rsdp.pf_rack.value = '"    . mysql_real_escape_string($a_rsdp_platform['pf_rack'])    . "';\n";
-        print "document.rsdp.pf_unit.value = "     . mysql_real_escape_string($a_rsdp_platform['pf_unit'])    . ";\n";
-        print "document.rsdp.pf_special.value = '" . mysql_real_escape_string($a_rsdp_platform['pf_special']) . "';\n";
+        print "document.rsdp.pf_row.value = '"     . mysqli_real_escape_string($a_rsdp_platform['pf_row'])     . "';\n";
+        print "document.rsdp.pf_rack.value = '"    . mysqli_real_escape_string($a_rsdp_platform['pf_rack'])    . "';\n";
+        print "document.rsdp.pf_unit.value = "     . mysqli_real_escape_string($a_rsdp_platform['pf_unit'])    . ";\n";
+        print "document.rsdp.pf_special.value = '" . mysqli_real_escape_string($a_rsdp_platform['pf_special']) . "';\n";
 
         if ($a_rsdp_platform['pf_redundant']) {
           print "document.rsdp.pf_redundant.checked = true;\n";
@@ -59,7 +59,7 @@
           print "alert(\"WARNING: There are duplicate Platform rows (" . $numrows . ")\");\n";
         }
       }
-      mysql_free_result($q_rsdp_platform);
+      mysqli_free_result($q_rsdp_platform);
 
       logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['rsdp'] . " from rsdp_osteam");
 
@@ -67,15 +67,15 @@
       $q_string .= "from rsdp_osteam ";
       $q_string .= "where os_rsdp = " . $formVars['rsdp'] . " ";
       $q_string .= "order by os_id ";
-      $q_rsdp_osteam = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      $a_rsdp_osteam = mysql_fetch_array($q_rsdp_osteam);
-      $numrows = mysql_num_rows($q_rsdp_osteam);
+      $q_rsdp_osteam = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      $a_rsdp_osteam = mysqli_fetch_array($q_rsdp_osteam);
+      $numrows = mysqli_num_rows($q_rsdp_osteam);
 
-      if (mysql_num_rows($q_rsdp_osteam) > 0) {
+      if (mysqli_num_rows($q_rsdp_osteam) > 0) {
         $os_software  = return_Index($a_rsdp_osteam['os_software'], "select os_id from operatingsystem where os_delete = 0 order by os_software");
 
-        print "document.rsdp.os_sysname.value = '" . mysql_real_escape_string($a_rsdp_osteam['os_sysname']) . "';\n";
-        print "document.rsdp.os_fqdn.value = '"    . mysql_real_escape_string($a_rsdp_osteam['os_fqdn'])    . "';\n";
+        print "document.rsdp.os_sysname.value = '" . mysqli_real_escape_string($a_rsdp_osteam['os_sysname']) . "';\n";
+        print "document.rsdp.os_fqdn.value = '"    . mysqli_real_escape_string($a_rsdp_osteam['os_fqdn'])    . "';\n";
 
         print "document.rsdp.os_software['" . $os_software . "'].selected = true;\n";
 
@@ -87,7 +87,7 @@
           print "alert(\"WARNING: There are duplicate OSTeam rows (" . $numrows . ")\");\n";
         }
       }
-      mysql_free_result($q_rsdp_osteam);
+      mysqli_free_result($q_rsdp_osteam);
 
       print "validate_Form();\n";
 
