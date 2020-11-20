d7406a14d66357e3cff1b50f11f8edcc664afb52
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:39:00 -0700

    Replaced mysql with mysqli

--- show/firewall.mysql.php
+++ show/firewall.mysql.php
@@ -20,12 +20,12 @@
   $q_string = "select inv_manager "
             . "from inventory "
             . "where inv_id = " . $formVars['id'] . " ";
-  $q_inventory = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  $a_inventory = mysql_fetch_array($q_inventory);
+  $q_inventory = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  $a_inventory = mysqli_fetch_array($q_inventory);
 
   $q_string = "select zone_id,zone_name from ip_zones";
-  $q_ip_zones = mysql_query($q_string) or die($q_string . ': ' . mysql_error());
-  while ($a_ip_zones = mysql_fetch_array($q_ip_zones)) {
+  $q_ip_zones = mysqli_query($db, $q_string) or die($q_string . ': ' . mysqli_error($db));
+  while ($a_ip_zones = mysqli_fetch_array($q_ip_zones)) {
     $zoneval[$a_ip_zones['zone_id']] = $a_ip_zones['zone_name'];
   }
 
@@ -92,8 +92,8 @@
   $q_string .= "from firewall ";
   $q_string .= "where fw_companyid = " . $formVars['id'] . " ";
   $q_string .= "order by fw_source,fw_destination,fw_port";
-  $q_firewall = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  while ($a_firewall = mysql_fetch_array($q_firewall)) {
+  $q_firewall = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  while ($a_firewall = mysqli_fetch_array($q_firewall)) {
 
     $output .= "<tr>";
     $output .= "  <td class=\"ui-widget-content\">" . $a_firewall['fw_source']                    . "</td>";
@@ -138,16 +138,16 @@
   $q_string .= "from interface ";
   $q_string .= "left join inventory on inventory.inv_id = interface.int_companyid ";
   $q_string .= "where int_companyid = " . $formVars['id'];
-  $q_interface = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  while ($a_interface = mysql_fetch_array($q_interface)) {
+  $q_interface = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  while ($a_interface = mysqli_fetch_array($q_interface)) {
     $q_string  = "select fw_id,fw_source,fw_sourcezone,fw_destination,fw_destinationzone,";
     $q_string .= "fw_port,fw_protocol,fw_timeout,fw_ticket,fw_description,fw_portdesc,inv_name ";
     $q_string .= "from firewall ";
     $q_string .= "left join inventory on inventory.inv_id = firewall.fw_companyid ";
     $q_string .= "where fw_source = '" . $a_interface['int_addr'] . "' or fw_destination = '" . $a_interface['int_addr'] . "' ";
     $q_string .= "order by fw_source,fw_destination,fw_port";
-    $q_firewall = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-    while ($a_firewall = mysql_fetch_array($q_firewall)) {
+    $q_firewall = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+    while ($a_firewall = mysqli_fetch_array($q_firewall)) {
 
       if ($a_interface['inv_name'] != $a_firewall['inv_name']) {
         $output .= "<tr>";
@@ -171,5 +171,5 @@
 
 ?>
 
-document.getElementById('firewall_mysql').innerHTML = '<?php print mysql_real_escape_string($output); ?>';
+document.getElementById('firewall_mysql').innerHTML = '<?php print mysqli_real_escape_string($output); ?>';
 
