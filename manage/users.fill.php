80c655391784ec737bb4fc23a6edc85f837a0b68
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 21:17:19 -0700

    Replaced mysql with mysqli

--- manage/users.fill.php
+++ manage/users.fill.php
@@ -25,15 +25,15 @@
       $q_string  = "select mu_username,mu_name,mu_email,mu_account,mu_comment,mu_locked,mu_ticket ";
       $q_string .= "from manageusers ";
       $q_string .= "where mu_id = " . $formVars['id'];
-      $q_manageusers = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_manageusers = mysql_fetch_array($q_manageusers);
-      mysql_free_result($q_manageusers);
+      $q_manageusers = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_manageusers = mysqli_fetch_array($q_manageusers);
+      mysqli_free_result($q_manageusers);
 
-      print "document.users.mu_username.value = '" . mysql_real_escape_string($a_manageusers['mu_username']) . "';\n";
-      print "document.users.mu_name.value = '"     . mysql_real_escape_string($a_manageusers['mu_name'])     . "';\n";
-      print "document.users.mu_email.value = '"    . mysql_real_escape_string($a_manageusers['mu_email'])    . "';\n";
-      print "document.users.mu_comment.value = '"  . mysql_real_escape_string($a_manageusers['mu_comment'])  . "';\n";
-      print "document.users.mu_ticket.value = '"   . mysql_real_escape_string($a_manageusers['mu_ticket'])   . "';\n";
+      print "document.users.mu_username.value = '" . mysqli_real_escape_string($a_manageusers['mu_username']) . "';\n";
+      print "document.users.mu_name.value = '"     . mysqli_real_escape_string($a_manageusers['mu_name'])     . "';\n";
+      print "document.users.mu_email.value = '"    . mysqli_real_escape_string($a_manageusers['mu_email'])    . "';\n";
+      print "document.users.mu_comment.value = '"  . mysqli_real_escape_string($a_manageusers['mu_comment'])  . "';\n";
+      print "document.users.mu_ticket.value = '"   . mysqli_real_escape_string($a_manageusers['mu_ticket'])   . "';\n";
 
       print "document.users.mu_account[" . $a_manageusers['mu_account'] . "].checked = true;\n";
 
