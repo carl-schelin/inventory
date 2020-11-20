3a3852ed7d2d4fece46750c1d140b52cb49ceca4
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:51:15 -0700

    Replaced mysql with mysqli

--- issue/comments.fill.php
+++ issue/comments.fill.php
@@ -26,14 +26,14 @@
       $q_string  = "select det_text,det_timestamp,det_user ";
       $q_string .= "from issue_detail ";
       $q_string .= "where det_id = " . $formVars['id'];
-      $q_issue_detail = mysql_query($q_string) or die (mysql_error());
-      $a_issue_detail = mysql_fetch_array($q_issue_detail);
-      mysql_free_result($q_issue_detail);
+      $q_issue_detail = mysqli_query($db, $q_string) or die (mysqli_error($db));
+      $a_issue_detail = mysqli_fetch_array($q_issue_detail);
+      mysqli_free_result($q_issue_detail);
 
       $selected = return_Index($a_issue_detail['det_user'],       "select usr_id from users where usr_disabled = 0 order by usr_last,usr_first");
 
-      print "document.start.det_text.value = '"      . mysql_real_escape_string($a_issue_detail['det_text'])      . "';\n";
-      print "document.start.det_timestamp.value = '" . mysql_real_escape_string($a_issue_detail['det_timestamp']) . "';\n";
+      print "document.start.det_text.value = '"      . mysqli_real_escape_string($a_issue_detail['det_text'])      . "';\n";
+      print "document.start.det_timestamp.value = '" . mysqli_real_escape_string($a_issue_detail['det_timestamp']) . "';\n";
 
       print "document.start.det_user['" . $selected . "'].selected = true;\n";
 
