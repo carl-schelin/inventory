4e89fa140dbf6e75759c15a5550a51f78a3e3435
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:51:48 -0700

    Replaced mysql with mysqli

--- issue/ticket.fill.php
+++ issue/ticket.fill.php
@@ -26,20 +26,20 @@
       $q_string  = "select iss_discovered,iss_closed,iss_subject ";
       $q_string .= "from issue ";
       $q_string .= "where iss_id = " . $formVars['id'];
-      $q_issue = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      $a_issue = mysql_fetch_array($q_issue);
+      $q_issue = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      $a_issue = mysqli_fetch_array($q_issue);
 
-      print "document.start.iss_discovered.value = '" . mysql_real_escape_string($a_issue['iss_discovered']) . "';\n";
-      print "document.start.iss_subject.value = '"    . mysql_real_escape_string($a_issue['iss_subject'])    . "';\n";
+      print "document.start.iss_discovered.value = '" . mysqli_real_escape_string($a_issue['iss_discovered']) . "';\n";
+      print "document.start.iss_subject.value = '"    . mysqli_real_escape_string($a_issue['iss_subject'])    . "';\n";
 
       if ($a_issue['iss_closed'] == '0000-00-00') {
         print "document.start.iss_closed.value = 'Current Date';\n";
 
-        print "document.getElementById('iss_discovered').innerHTML = '" . mysql_real_escape_string($a_issue['iss_discovered']) . "';\n";
-        print "document.getElementById('iss_closed').innerHTML = '"     . mysql_real_escape_string($a_issue['iss_closed'])     . "';\n";
-        print "document.getElementById('iss_subject').innerHTML = '"    . mysql_real_escape_string($a_issue['iss_subject'])    . "';\n";
+        print "document.getElementById('iss_discovered').innerHTML = '" . mysqli_real_escape_string($a_issue['iss_discovered']) . "';\n";
+        print "document.getElementById('iss_closed').innerHTML = '"     . mysqli_real_escape_string($a_issue['iss_closed'])     . "';\n";
+        print "document.getElementById('iss_subject').innerHTML = '"    . mysqli_real_escape_string($a_issue['iss_subject'])    . "';\n";
       } else {
-        print "document.start.iss_closed.value = '" . mysql_real_escape_string($a_issue['iss_closed']) . "';\n";
+        print "document.start.iss_closed.value = '" . mysqli_real_escape_string($a_issue['iss_closed']) . "';\n";
       }
 
       print "document.start.iss_id.value = " . $formVars['id'] . ";\n";
