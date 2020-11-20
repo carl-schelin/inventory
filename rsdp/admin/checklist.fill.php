0d871a42afe9d90b24b309720006a4abfc4e2c55
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 21:02:33 -0700

    Replaced mysql with mysqli

--- rsdp/admin/checklist.fill.php
+++ rsdp/admin/checklist.fill.php
@@ -26,18 +26,18 @@
       $q_string  = "select chk_group,chk_index,chk_text,chk_link,chk_task ";
       $q_string .= "from checklist ";
       $q_string .= "where chk_id = " . $formVars['id'];
-      $q_checklist = mysql_query($q_string) or die (mysql_error());
-      $a_checklist = mysql_fetch_array($q_checklist);
-      mysql_free_result($q_checklist);
+      $q_checklist = mysqli_query($db, $q_string) or die (mysqli_error($db));
+      $a_checklist = mysqli_fetch_array($q_checklist);
+      mysqli_free_result($q_checklist);
 
 #      $group = return_Index($a_checklist['chk_group'], "select grp_id from groups where grp_disabled = 0 order by grp_name");
 
 #      print "document.checklists.chk_group['" . $group . "'].selected = true;\n";
       print "document.checklists.chk_task['" . $a_checklist['chk_task'] . "'].selected = true;\n";
 
-      print "document.checklists.chk_index.value = '" . mysql_real_escape_string($a_checklist['chk_index']) . "';\n";
-      print "document.checklists.chk_text.value = '"  . mysql_real_escape_string($a_checklist['chk_text'])  . "';\n";
-      print "document.checklists.chk_link.value = '"  . mysql_real_escape_string($a_checklist['chk_link'])  . "';\n";
+      print "document.checklists.chk_index.value = '" . mysqli_real_escape_string($a_checklist['chk_index']) . "';\n";
+      print "document.checklists.chk_text.value = '"  . mysqli_real_escape_string($a_checklist['chk_text'])  . "';\n";
+      print "document.checklists.chk_link.value = '"  . mysqli_real_escape_string($a_checklist['chk_link'])  . "';\n";
 
       print "document.checklists.id.value = " . $formVars['id'] . ";\n";
 
