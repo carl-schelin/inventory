2963a4df69d5f013fa71c099eeb57a042de7537d
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:41:35 -0700

    Replaced mysql with mysqli

--- psaps/centurylink.fill.php
+++ psaps/centurylink.fill.php
@@ -26,23 +26,23 @@
       $q_string .= "psap_lport,psap_circuit_id,psap_pseudo_cid,psap_lec,psap_texas,psap_updated,psap_delete ";
       $q_string .= "from psaps ";
       $q_string .= "where psap_id = " . $formVars['id'];
-      $q_psaps = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      $a_psaps = mysql_fetch_array($q_psaps);
-      mysql_free_result($q_psaps);
+      $q_psaps = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      $a_psaps = mysqli_fetch_array($q_psaps);
+      mysqli_free_result($q_psaps);
 
-      print "document.psaps.psap_customerid.value = '"    . mysql_real_escape_string($a_psaps['psap_customerid'])    . "';\n";
-      print "document.psaps.psap_parentid.value = '"      . mysql_real_escape_string($a_psaps['psap_parentid'])      . "';\n";
-      print "document.psaps.psap_ali_id.value = '"        . mysql_real_escape_string($a_psaps['psap_ali_id'])        . "';\n";
-      print "document.psaps.psap_companyid.value = '"     . mysql_real_escape_string($a_psaps['psap_companyid'])     . "';\n";
-      print "document.psaps.psap_psap_id.value = '"       . mysql_real_escape_string($a_psaps['psap_psap_id'])       . "';\n";
-      print "document.psaps.psap_description.value = '"   . mysql_real_escape_string($a_psaps['psap_description'])   . "';\n";
-      print "document.psaps.psap_lport.value = '"         . mysql_real_escape_string($a_psaps['psap_lport'])         . "';\n";
-      print "document.psaps.psap_circuit_id.value = '"    . mysql_real_escape_string($a_psaps['psap_circuit_id'])    . "';\n";
-      print "document.psaps.psap_pseudo_cid.value = '"    . mysql_real_escape_string($a_psaps['psap_pseudo_cid'])    . "';\n";
-      print "document.psaps.psap_lec.value = '"           . mysql_real_escape_string($a_psaps['psap_lec'])           . "';\n";
-      print "document.psaps.psap_texas.value = '"         . mysql_real_escape_string($a_psaps['psap_texas'])         . "';\n";
-      print "document.psaps.psap_updated.value = '"       . mysql_real_escape_string($a_psaps['psap_updated'])       . "';\n";
-      print "document.psaps.psap_delete.value = '"        . mysql_real_escape_string($a_psaps['psap_delete'])        . "';\n";
+      print "document.psaps.psap_customerid.value = '"    . mysqli_real_escape_string($a_psaps['psap_customerid'])    . "';\n";
+      print "document.psaps.psap_parentid.value = '"      . mysqli_real_escape_string($a_psaps['psap_parentid'])      . "';\n";
+      print "document.psaps.psap_ali_id.value = '"        . mysqli_real_escape_string($a_psaps['psap_ali_id'])        . "';\n";
+      print "document.psaps.psap_companyid.value = '"     . mysqli_real_escape_string($a_psaps['psap_companyid'])     . "';\n";
+      print "document.psaps.psap_psap_id.value = '"       . mysqli_real_escape_string($a_psaps['psap_psap_id'])       . "';\n";
+      print "document.psaps.psap_description.value = '"   . mysqli_real_escape_string($a_psaps['psap_description'])   . "';\n";
+      print "document.psaps.psap_lport.value = '"         . mysqli_real_escape_string($a_psaps['psap_lport'])         . "';\n";
+      print "document.psaps.psap_circuit_id.value = '"    . mysqli_real_escape_string($a_psaps['psap_circuit_id'])    . "';\n";
+      print "document.psaps.psap_pseudo_cid.value = '"    . mysqli_real_escape_string($a_psaps['psap_pseudo_cid'])    . "';\n";
+      print "document.psaps.psap_lec.value = '"           . mysqli_real_escape_string($a_psaps['psap_lec'])           . "';\n";
+      print "document.psaps.psap_texas.value = '"         . mysqli_real_escape_string($a_psaps['psap_texas'])         . "';\n";
+      print "document.psaps.psap_updated.value = '"       . mysqli_real_escape_string($a_psaps['psap_updated'])       . "';\n";
+      print "document.psaps.psap_delete.value = '"        . mysqli_real_escape_string($a_psaps['psap_delete'])        . "';\n";
 
       print "document.psaps.id.value = " . $formVars['id'] . ";\n";
 
