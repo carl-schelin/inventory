1f9ca3189823353b3e21c36919a2bf3003037351
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:32:33 -0700

    Replaced mysql with mysqli

--- admin/product.fill.php
+++ admin/product.fill.php
@@ -25,29 +25,29 @@
       $q_string  = "select prod_name,prod_code,prod_oldcode,prod_desc,prod_group,prod_type,prod_citype,prod_tier1,prod_tier2,prod_tier3,prod_remedy,prod_unit,prod_service ";
       $q_string .= "from products ";
       $q_string .= "where prod_id = " . $formVars['id'];
-      $q_products = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_products = mysql_fetch_array($q_products);
-      mysql_free_result($q_products);
+      $q_products = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_products = mysqli_fetch_array($q_products);
+      mysqli_free_result($q_products);
 
       $group   = return_Index($a_products['prod_group'],   "select grp_id from groups where grp_disabled = 0 order by grp_name");
       $unit    = return_Index($a_products['prod_unit'],    "select bus_id from business_unit order by bus_name");
       $service = return_Index($a_products['prod_service'], "select svc_id from service order by svc_id");
 
-      print "document.products.prod_id.value = '"      . mysql_real_escape_string($formVars['id'])             . "';\n";
-      print "document.products.prod_name.value = '"    . mysql_real_escape_string($a_products['prod_name'])    . "';\n";
-      print "document.products.prod_code.value = '"    . mysql_real_escape_string($a_products['prod_code'])    . "';\n";
-      print "document.products.prod_oldcode.value = '" . mysql_real_escape_string($a_products['prod_oldcode']) . "';\n";
-      print "document.products.prod_desc.value = '"    . mysql_real_escape_string($a_products['prod_desc'])    . "';\n";
+      print "document.products.prod_id.value = '"      . mysqli_real_escape_string($formVars['id'])             . "';\n";
+      print "document.products.prod_name.value = '"    . mysqli_real_escape_string($a_products['prod_name'])    . "';\n";
+      print "document.products.prod_code.value = '"    . mysqli_real_escape_string($a_products['prod_code'])    . "';\n";
+      print "document.products.prod_oldcode.value = '" . mysqli_real_escape_string($a_products['prod_oldcode']) . "';\n";
+      print "document.products.prod_desc.value = '"    . mysqli_real_escape_string($a_products['prod_desc'])    . "';\n";
 
       print "document.products.prod_group['"   . $group   . "'].selected = true;\n";
       print "document.products.prod_unit['"    . $unit    . "'].selected = true;\n";
       print "document.products.prod_service['" . $service . "'].selected = true;\n";
 
-      print "document.products.prod_type.value = '"   . mysql_real_escape_string($a_products['prod_type'])   . "';\n";
-      print "document.products.prod_citype.value = '" . mysql_real_escape_string($a_products['prod_citype']) . "';\n";
-      print "document.products.prod_tier1.value = '"  . mysql_real_escape_string($a_products['prod_tier1'])  . "';\n";
-      print "document.products.prod_tier2.value = '"  . mysql_real_escape_string($a_products['prod_tier2'])  . "';\n";
-      print "document.products.prod_tier3.value = '"  . mysql_real_escape_string($a_products['prod_tier3'])  . "';\n";
+      print "document.products.prod_type.value = '"   . mysqli_real_escape_string($a_products['prod_type'])   . "';\n";
+      print "document.products.prod_citype.value = '" . mysqli_real_escape_string($a_products['prod_citype']) . "';\n";
+      print "document.products.prod_tier1.value = '"  . mysqli_real_escape_string($a_products['prod_tier1'])  . "';\n";
+      print "document.products.prod_tier2.value = '"  . mysqli_real_escape_string($a_products['prod_tier2'])  . "';\n";
+      print "document.products.prod_tier3.value = '"  . mysqli_real_escape_string($a_products['prod_tier3'])  . "';\n";
 
       if ($a_products['prod_remedy']) {
         print "document.products.prod_remedy.checked = true;\n";
