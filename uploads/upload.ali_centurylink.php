23808d36dc8c161d459ad1386a9cdbb05df1de09
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 21:20:17 -0700

    Replaced mysql with mysqli

--- uploads/upload.ali_centurylink.php
+++ uploads/upload.ali_centurylink.php
@@ -9,8 +9,8 @@
   include($Sitepath . '/function.php');
 
   function dbconn($server,$database,$user,$pass){
-    $db = mysql_connect($server,$user,$pass);
-    $db_select = mysql_select_db($database,$db);
+    $db = mysqli_connect($server,$user,$pass,$database);
+    $db_select = mysqli_select_db($db,$database);
     return $db;
   }
 
@@ -25,7 +25,7 @@
   $q_string  = "update psaps ";
   $q_string .= "set psap_delete = 1 ";
   $q_string .= "where psap_companyid = 41 ";
-  $result = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+  $result = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
 
 # load the data
 if (($handle = fopen($file, "r")) !== FALSE) {
@@ -55,20 +55,20 @@ if (($handle = fopen($file, "r")) !== FALSE) {
     $q_string  = "select inv_id,inv_name ";
     $q_string .= "from inventory ";
     $q_string .= "where inv_name = '" . $data[1] . "' ";
-    $q_inventory = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-    if (mysql_num_rows($q_inventory) == 0) {
+    $q_inventory = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+    if (mysqli_num_rows($q_inventory) == 0) {
 
       print "no such server as " . $data[1] . "\n";
 
     } else {
-      $a_inventory = mysql_fetch_array($q_inventory);
+      $a_inventory = mysqli_fetch_array($q_inventory);
 
 # get the matching Intrado psap information from the database
       $q_string  = "select psap_id "; 
       $q_string .= "from psaps ";
       $q_string .= "where psap_ali_id = '" . $data[0] . "' and psap_companyid = " . $a_inventory['inv_id'] . " and psap_psap_id = " . $data[3] . " ";
-      $q_psaps = mysql_query($q_string) or die($q_string . "\n: " . mysql_error() . "\n");
-      if (mysql_num_rows($q_psaps) == 0) {
+      $q_psaps = mysqli_query($db, $q_string) or die($q_string . "\n: " . mysqli_error($db) . "\n");
+      if (mysqli_num_rows($q_psaps) == 0) {
 # error, there should be an association with an existing psap id.
         print "Error: No association with an existing Intrado PSAP: Query: ALI ID = " . $data[0] . " and ALI Name = " . $a_inventory['inv_name'] . " and PSAP ID = " . $data[3] . "\n";
 
@@ -77,41 +77,41 @@ if (($handle = fopen($file, "r")) !== FALSE) {
         $q_string .= "left join inventory on inventory.inventory.inv_id = inventory.psaps.psap_companyid ";
         $q_string .= "where psap_psap_id = " . $data[3] . " and psap_ali_id like \"Q%\" ";
         $q_string .= "order by psap_ali_id,inv_name ";
-        $q_psaps = mysql_query($q_string) or die($q_string . "\n: " . mysql_error() . "\n");
-        while ($a_psaps = mysql_fetch_array($q_psaps)) {
+        $q_psaps = mysqli_query($db, $q_string) or die($q_string . "\n: " . mysqli_error($db) . "\n");
+        while ($a_psaps = mysqli_fetch_array($q_psaps)) {
           print "--Found: ALI ID = " . $a_psaps['psap_ali_id'] . " and ALI Name = " . $a_psaps['inv_name'] . " and PSAP ID = " . $data[3] . "\n";
         }
 
       } else {
-        $a_psaps = mysql_fetch_array($q_psaps);
+        $a_psaps = mysqli_fetch_array($q_psaps);
 
 # change from data[9] to data[6] per converstion with mahesh and jerry. 2/1/16
         $query  = 
           "psap_customerid  =  " . "41"                                          . "," . 
           "psap_parentid    =  " . $a_psaps['psap_id']                           . "," . 
-          "psap_ali_id      = '" . mysql_real_escape_string(clean($data[0],10))  . "'," .
+          "psap_ali_id      = '" . mysqli_real_escape_string(clean($data[0],10))  . "'," .
           "psap_companyid   =  " . $a_inventory['inv_id']                        . "," .
-          "psap_psap_id     = '" . mysql_real_escape_string(clean($data[2],255)) . "'," .
-          "psap_description = '" . mysql_real_escape_string(clean($data[8],255)) . "'," .
+          "psap_psap_id     = '" . mysqli_real_escape_string(clean($data[2],255)) . "'," .
+          "psap_description = '" . mysqli_real_escape_string(clean($data[8],255)) . "'," .
           "psap_lport       =  " . 0                                             . "," . 
-          "psap_circuit_id  = '" . mysql_real_escape_string(clean($data[5],255)) . "'," . 
-          "psap_pseudo_cid  = '" . mysql_real_escape_string(clean($data[6],255)) . "'," . 
-          "psap_lec         = '" . mysql_real_escape_string(clean($data[7],255)) . "'," .
+          "psap_circuit_id  = '" . mysqli_real_escape_string(clean($data[5],255)) . "'," . 
+          "psap_pseudo_cid  = '" . mysqli_real_escape_string(clean($data[6],255)) . "'," . 
+          "psap_lec         = '" . mysqli_real_escape_string(clean($data[7],255)) . "'," .
           "psap_delete      =  " . 0                                             . "," . 
           "psap_updated     = '" . date('Y-m-d')                                 . "'";
 
         $q_string  = "select psap_id "; 
         $q_string .= "from psaps ";
         $q_string .= "where psap_customerid = 41 and psap_ali_id = '" . $data[0] . "' and psap_companyid = " . $a_inventory['inv_id'] . " and psap_psap_id = " . $data[2] . " ";
-        $q_psaps = mysql_query($q_string) or die($query . "\n:" . $q_string . "\n: " . mysql_error());
-        if (mysql_num_rows($q_psaps) == 0) {
+        $q_psaps = mysqli_query($db, $q_string) or die($query . "\n:" . $q_string . "\n: " . mysqli_error($db));
+        if (mysqli_num_rows($q_psaps) == 0) {
           $q_string  = "insert into psaps set psap_id = null," . $query;
           if ($debug == 'no') {
 #            print "i";
             print "";
           }
         } else {
-          $a_psaps = mysql_fetch_array($q_psaps);
+          $a_psaps = mysqli_fetch_array($q_psaps);
           $q_string  = "update psaps set " . $query . " where psap_id = " . $a_psaps['psap_id'];
           if ($debug == 'no') {
 #            print "u";
@@ -120,7 +120,7 @@ if (($handle = fopen($file, "r")) !== FALSE) {
         }
 
         if ($debug == 'no') {
-          $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+          $insert = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
         } else {
           print $q_string . "\n";
         }
