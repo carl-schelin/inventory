8cf3090cb3435e8dbfc415f2bb701ea48f1d673e
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 21:10:36 -0700

    Replaced mysql with mysqli

--- rsdp/san/designed.php
+++ rsdp/san/designed.php
@@ -36,9 +36,9 @@
   $q_string  = "select os_sysname ";
   $q_string .= "from rsdp_osteam ";
   $q_string .= "where os_rsdp = " . $formVars['rsdp'];
-  $q_rsdp_osteam = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  if (mysql_num_rows($q_rsdp_osteam) > 0) {
-    $a_rsdp_osteam = mysql_fetch_array($q_rsdp_osteam);
+  $q_rsdp_osteam = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  if (mysqli_num_rows($q_rsdp_osteam) > 0) {
+    $a_rsdp_osteam = mysqli_fetch_array($q_rsdp_osteam);
   } else {
     $a_rsdp_osteam['os_sysname'] = "New Server";
   }
@@ -247,10 +247,10 @@ ready to be worked.</li>
   $q_string  = "select count(*) ";
   $q_string .= "from rsdp_comments ";
   $q_string .= "where com_rsdp = " . $formVars['rsdp'];
-  $q_rsdp_comments = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  $a_rsdp_comments = mysql_fetch_array($q_rsdp_comments);
+  $q_rsdp_comments = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  $a_rsdp_comments = mysqli_fetch_array($q_rsdp_comments);
 
-  if (mysql_num_rows($q_rsdp_comments)) {
+  if (mysqli_num_rows($q_rsdp_comments)) {
     print " (" . $a_rsdp_comments['count(*)'] . ")";
   } else {
     print " (0)";
@@ -356,8 +356,8 @@ than one port. This information is used by the Data Center folks to run cable. I
   $q_string  = "select med_id,med_text ";
   $q_string .= "from int_media ";
   $q_string .= "order by med_text";
-  $q_int_media = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  while ($a_int_media = mysql_fetch_array($q_int_media)) {
+  $q_int_media = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  while ($a_int_media = mysqli_fetch_array($q_int_media)) {
     print "<option value=\"" . $a_int_media['med_id'] . "\">" . $a_int_media['med_text'] . "</option>\n";
   }
 ?>
