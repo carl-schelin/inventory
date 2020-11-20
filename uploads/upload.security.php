68b84719ab9577cd69bb5afd47de4e6c191678b8
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 21:20:48 -0700

    Replaced mysql with mysqli

--- uploads/upload.security.php
+++ uploads/upload.security.php
@@ -9,8 +9,8 @@
   include($Sitepath . '/function.php');
 
   function dbconn($server,$database,$user,$pass){
-    $db = mysql_connect($server,$user,$pass);
-    $db_select = mysql_select_db($database,$db);
+    $db = mysqli_connect($server,$user,$pass,$database);
+    $db_select = mysqli_select_db($db,$database);
     return $db;
   }
 
@@ -34,43 +34,43 @@
       $q_string  = "select fam_id ";
       $q_string .= "from family ";
       $q_string .= "where fam_name = '" . $data[2] . "'";
-      $q_family = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      $a_family = mysql_fetch_array($q_family);
+      $q_family = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      $a_family = mysqli_fetch_array($q_family);
 
       $q_string  = "select sev_id ";
       $q_string .= "from severity ";
       $q_string .= "where sev_name = '" . $data[3] . "'";
-      $q_severity = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      $a_severity = mysql_fetch_array($q_severity);
+      $q_severity = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      $a_severity = mysqli_fetch_array($q_severity);
 
       $q_string  = "select sec_id,sec_severity ";
       $q_string .= "from security ";
       $q_string .= "where sec_id = " . $data[0];
-      $q_security = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      if (mysql_num_rows($q_security) == 0) {
+      $q_security = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      if (mysqli_num_rows($q_security) == 0) {
 
 # if sec_id is not set,
 #   add the entry
         $q_string  = "insert ";
         $q_string .= "into security set ";
         $q_string .= "sec_id       =  " . $data[0]                           . ",";
-        $q_string .= "sec_name     = '" . mysql_real_escape_string($data[1]) . "',";
+        $q_string .= "sec_name     = '" . mysqli_real_escape_string($data[1]) . "',";
         $q_string .= "sec_family   =  " . $a_family['fam_id']                . ",";
         $q_string .= "sec_severity =  " . $a_severity['sev_id']              . " ";
-        $insert = mysql_query($q_string) or die("\nQuery: " . $q_string . "\nFamily: " . $data[2] . "\nSeverity: " . $data[3] . "\n" . mysql_error() . "\n");
+        $insert = mysqli_query($db, $q_string) or die("\nQuery: " . $q_string . "\nFamily: " . $data[2] . "\nSeverity: " . $data[3] . "\n" . mysqli_error($db) . "\n");
         print "+";
       } else {
-        $a_security = mysql_fetch_array($q_security);
+        $a_security = mysqli_fetch_array($q_security);
 
 # if the severity changes, update the record
         if ($a_security['sec_severity'] != $a_severity['sev_id']) {
           $q_string  = "update ";
           $q_string .= "security ";
           $q_string .= "set ";
-          $q_string .= "sec_name     = \"" . mysql_real_escape_string($data[1]) . "\",";
+          $q_string .= "sec_name     = \"" . mysqli_real_escape_string($data[1]) . "\",";
           $q_string .= "sec_severity =   " . $a_severity['sev_id'] . " ";
           $q_string .= "where sec_id = " . $a_security['sec_id'];
-          $update = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+          $update = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
           print "^";
         } else {
           print ".";
