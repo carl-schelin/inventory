ea7d659f805578ac59657c84dd570ad587fa9d8c
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:52:02 -0700

    Replaced mysql with mysqli

--- issue/validate.hostname.php
+++ issue/validate.hostname.php
@@ -17,8 +17,8 @@
   $q_string  = "select inv_id,inv_name ";
   $q_string .= "from inventory ";
   $q_string .= "where inv_name = \"" . $formVars['server'] . "\" and inv_status = 0";
-  $q_inventory = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  $a_inventory = mysql_fetch_array($q_inventory);
+  $q_inventory = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  $a_inventory = mysqli_fetch_array($q_inventory);
 
   if ($a_inventory['inv_name'] == $formVars['server'] && $formVars['server'] != '') {
 ?>
