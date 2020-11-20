1d00209b53aa70eb00693e2a5ab287979be21013
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:31:18 -0700

    Replaced mysql with mysqli

--- admin/device.del.php
+++ admin/device.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from device ";
       $q_string .= "where dev_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+      $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
 
       print "alert('Device Type deleted.');\n";
 
