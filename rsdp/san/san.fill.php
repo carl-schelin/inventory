bc3bcca94078b954f8adee9915f3196bbc50fe26
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 21:10:44 -0700

    Replaced mysql with mysqli

--- rsdp/san/san.fill.php
+++ rsdp/san/san.fill.php
@@ -26,16 +26,16 @@
       $q_string  = "select san_id,san_sysport,san_switch,san_port,san_media,san_wwnnzone ";
       $q_string .= "from rsdp_san ";
       $q_string .= "where san_id = " . $formVars['id'];
-      $q_rsdp_san = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      $a_rsdp_san = mysql_fetch_array($q_rsdp_san);
-      mysql_free_result($q_rsdp_san);
+      $q_rsdp_san = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      $a_rsdp_san = mysqli_fetch_array($q_rsdp_san);
+      mysqli_free_result($q_rsdp_san);
 
       $sanmedia = return_Index($a_rsdp_san['san_media'], "select med_id from int_media order by med_text");
 
-      print "document.getElementById('san_sysport').innerHTML = '" . mysql_real_escape_string($a_rsdp_san['san_sysport'])  . "';\n";
-      print "document.san.san_switch.value = '"                   . mysql_real_escape_string($a_rsdp_san['san_switch'])   . "';\n";
-      print "document.san.san_port.value = '"                     . mysql_real_escape_string($a_rsdp_san['san_port'])     . "';\n";
-      print "document.san.san_wwnnzone.value = '"                 . mysql_real_escape_string($a_rsdp_san['san_wwnnzone']) . "';\n";
+      print "document.getElementById('san_sysport').innerHTML = '" . mysqli_real_escape_string($a_rsdp_san['san_sysport'])  . "';\n";
+      print "document.san.san_switch.value = '"                   . mysqli_real_escape_string($a_rsdp_san['san_switch'])   . "';\n";
+      print "document.san.san_port.value = '"                     . mysqli_real_escape_string($a_rsdp_san['san_port'])     . "';\n";
+      print "document.san.san_wwnnzone.value = '"                 . mysqli_real_escape_string($a_rsdp_san['san_wwnnzone']) . "';\n";
 
       print "document.san.san_media['" . $sanmedia . "'].selected = true;\n";
 
