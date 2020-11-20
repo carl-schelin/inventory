4012af4bfb7c45a9d9e2d9172dfbadb3a1ae621b
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:32:57 -0700

    Replaced mysql with mysqli

--- admin/service.fill.php
+++ admin/service.fill.php
@@ -25,17 +25,17 @@
       $q_string  = "select svc_name,svc_acronym,svc_availability,svc_downtime,svc_mtbf,svc_geographic,svc_mttr,svc_resource,svc_restore ";
       $q_string .= "from service ";
       $q_string .= "where svc_id = " . $formVars['id'];
-      $q_service = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_service = mysql_fetch_array($q_service);
-      mysql_free_result($q_service);
+      $q_service = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_service = mysqli_fetch_array($q_service);
+      mysqli_free_result($q_service);
 
-      print "document.service.svc_name.value = '"         . mysql_real_escape_string($a_service['svc_name'])         . "';\n";
-      print "document.service.svc_acronym.value = '"      . mysql_real_escape_string($a_service['svc_acronym'])      . "';\n";
-      print "document.service.svc_availability.value = '" . mysql_real_escape_string($a_service['svc_availability']) . "';\n";
-      print "document.service.svc_downtime.value = '"     . mysql_real_escape_string($a_service['svc_downtime'])     . "';\n";
-      print "document.service.svc_mtbf.value = '"         . mysql_real_escape_string($a_service['svc_mtbf'])         . "';\n";
-      print "document.service.svc_mttr.value = '"         . mysql_real_escape_string($a_service['svc_mttr'])         . "';\n";
-      print "document.service.svc_restore.value = '"      . mysql_real_escape_string($a_service['svc_restore'])      . "';\n";
+      print "document.service.svc_name.value = '"         . mysqli_real_escape_string($a_service['svc_name'])         . "';\n";
+      print "document.service.svc_acronym.value = '"      . mysqli_real_escape_string($a_service['svc_acronym'])      . "';\n";
+      print "document.service.svc_availability.value = '" . mysqli_real_escape_string($a_service['svc_availability']) . "';\n";
+      print "document.service.svc_downtime.value = '"     . mysqli_real_escape_string($a_service['svc_downtime'])     . "';\n";
+      print "document.service.svc_mtbf.value = '"         . mysqli_real_escape_string($a_service['svc_mtbf'])         . "';\n";
+      print "document.service.svc_mttr.value = '"         . mysqli_real_escape_string($a_service['svc_mttr'])         . "';\n";
+      print "document.service.svc_restore.value = '"      . mysqli_real_escape_string($a_service['svc_restore'])      . "';\n";
 
       if ($a_service['svc_geographic']) {
         print "document.service.svc_geographic.checked = true\n;";
