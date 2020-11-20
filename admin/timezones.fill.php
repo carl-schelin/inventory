08d496143b01751f5a4c1948627d0118f532b618
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:33:27 -0700

    Replaced mysql with mysqli

--- admin/timezones.fill.php
+++ admin/timezones.fill.php
@@ -25,13 +25,13 @@
       $q_string  = "select zone_name,zone_description,zone_offset ";
       $q_string .= "from zones ";
       $q_string .= "where zone_id = " . $formVars['id'];
-      $q_zones = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-      $a_zones = mysql_fetch_array($q_zones);
-      mysql_free_result($q_zones);
+      $q_zones = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_zones = mysqli_fetch_array($q_zones);
+      mysqli_free_result($q_zones);
 
-      print "document.zones.zone_name.value = '"         . mysql_real_escape_string($a_zones['zone_name'])        . "';\n";
-      print "document.zones.zone_description.value  = '" . mysql_real_escape_string($a_zones['zone_description']) . "';\n";
-      print "document.zones.zone_offset.value = '"       . mysql_real_escape_string($a_zones['zone_offset'])      . "';\n";
+      print "document.zones.zone_name.value = '"         . mysqli_real_escape_string($a_zones['zone_name'])        . "';\n";
+      print "document.zones.zone_description.value  = '" . mysqli_real_escape_string($a_zones['zone_description']) . "';\n";
+      print "document.zones.zone_offset.value = '"       . mysqli_real_escape_string($a_zones['zone_offset'])      . "';\n";
 
       print "document.zones.id.value = " . $formVars['id'] . ";\n";
 
