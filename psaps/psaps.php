534ce40c21c8b4e7f79f99e60e9e20f1f15c9e2a
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:41:46 -0700

    Replaced mysql with mysqli

--- psaps/psaps.php
+++ psaps/psaps.php
@@ -187,8 +187,8 @@ $(document).ready( function() {
   $q_string .= "left join products on products.prod_id = inventory.inv_product ";
   $q_string .= "where prod_name = \"ALIM\" and inv_manager = 1 and inv_appadmin != 1 and inv_status = 0 ";
   $q_string .= "order by inv_name ";
-  $q_inventory = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  while ($a_inventory = mysql_fetch_array($q_inventory)) {
+  $q_inventory = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  while ($a_inventory = mysqli_fetch_array($q_inventory)) {
     print "<option value=\"" . $a_inventory['inv_id'] . "\">" . $a_inventory['inv_name'] . "</option>\n";
   }
 
