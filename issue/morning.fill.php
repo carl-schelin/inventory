8f94ad50d63d926588ac1b506bb716b56fdbeb17
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:51:31 -0700

    Replaced mysql with mysqli

--- issue/morning.fill.php
+++ issue/morning.fill.php
@@ -26,14 +26,14 @@
       $q_string  = "select morn_text,morn_timestamp,morn_user,morn_status ";
       $q_string .= "from issue_morning ";
       $q_string .= "where morn_id = " . $formVars['id'];
-      $q_issue_morning = mysql_query($q_string) or die (mysql_error());
-      $a_issue_morning = mysql_fetch_array($q_issue_morning);
-      mysql_free_result($q_issue_morning);
+      $q_issue_morning = mysqli_query($db, $q_string) or die (mysqli_error($db));
+      $a_issue_morning = mysqli_fetch_array($q_issue_morning);
+      mysqli_free_result($q_issue_morning);
 
       $selected = return_Index($a_issue_morning['morn_user'], "select usr_id from users where usr_disabled = 0 order by usr_last,usr_first");
 
-      print "document.start.morn_text.value = '"      . mysql_real_escape_string($a_issue_morning['morn_text'])      . "';\n";
-      print "document.start.morn_timestamp.value = '" . mysql_real_escape_string($a_issue_morning['morn_timestamp']) . "';\n";
+      print "document.start.morn_text.value = '"      . mysqli_real_escape_string($a_issue_morning['morn_text'])      . "';\n";
+      print "document.start.morn_timestamp.value = '" . mysqli_real_escape_string($a_issue_morning['morn_timestamp']) . "';\n";
 
       print "document.start.morn_user['"   . $selected                       . "'].selected = true;\n";
       print "document.start.morn_status['" . $a_issue_morning['morn_status'] . "'].selected = true;\n";
