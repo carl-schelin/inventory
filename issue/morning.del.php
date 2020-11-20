3ea24f3583f2d776f9643ccb536270d1290a5251
Carl Schelin <t.carl.schelin@gmail.com>
2020-11-19 20:51:29 -0700

    Replaced mysql with mysqli

--- issue/morning.del.php
+++ issue/morning.del.php
@@ -25,7 +25,7 @@
       $q_string  = "delete ";
       $q_string .= "from issue_morning ";
       $q_string .= "where morn_id = " . $formVars['id'];
-      $insert = mysql_query($q_string) or die($q_string . ": " . mysql_error());
+      $insert = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
 
       print "alert('Status deleted.');\n";
 
