0a7cb70c58a91b591a9a25dc70c8a1ef5de5bc5c
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 04:03:38 +0000

    removed company examples

--- bin/bigfix.email.php
+++ bin/bigfix.email.php
@@ -22,7 +22,7 @@
     $debug = 'yes';
   }
 
-  $headers  = "From: Inventory Management <inventory@incojs01.scc911.com>\r\n";
+  $headers  = "From: Inventory Management <inventory@" . $hostname . ">\r\n";
   $headers .= "MIME-Version: 1.0\r\n";
   $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
