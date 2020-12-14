248d888afaa3752450a9485f303935171f797161
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 03:04:04 +0000

    closed db conn, changed from mysql to mysqli, removed company info

--- bin/lockuser.php
+++ bin/lockuser.php
@@ -2,15 +2,14 @@
 # Script: lockuser.php
 # Owner: Carl Schelin
 # Coding Standard 3.0 Applied
-# See: https://incowk01/makers/index.php/Coding_Standards
 # Description:
 
   include('settings.php');
   include($Sitepath . '/function.php');
 
   function dbconn($server,$database,$user,$pass){
-    $db = mysql_connect($server,$user,$pass);
-    $db_select = mysql_select_db($database,$db);
+    $db = mysqli_connect($server,$user,$pass,$database);
+    $db_select = mysqli_select_db($db,$database);
     return $db;
   }
 
@@ -23,16 +22,16 @@
   $q_string .= "from manageusers ";
   $q_string .= "where mu_account = 0 and mu_locked = 1 ";
   $q_string .= "order by mu_username ";
-  $q_manageusers = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  while ($a_manageusers = mysql_fetch_array($q_manageusers)) {
+  $q_manageusers = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  while ($a_manageusers = mysqli_fetch_array($q_manageusers)) {
 
 # only list if the account exists on a live server
     $q_string  = "select pwd_user ";
     $q_string .= "from syspwd ";
     $q_string .= "left join inventory on inventory.inv_id = syspwd.pwd_companyid ";
     $q_string .= "where pwd_user = \"" . $a_manageusers['mu_username'] . "\" and inv_status = 0 ";
-    $q_syspwd = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-    if (mysql_num_rows($q_syspwd) > 0) {
+    $q_syspwd = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+    if (mysqli_num_rows($q_syspwd) > 0) {
 #      if ($a_manageusers['mu_comment'] != '') {
 #        print "# " . $a_manageusers['mu_comment'] . "\n";
 #      }
@@ -42,5 +41,7 @@
       print $a_manageusers['mu_username'] . ":" . $a_manageusers['mu_email'] . "\n";
     }
   }
+
+  mysqli_free_request($db);
 
 ?>
