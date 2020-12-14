5983536b68011cf69e04992b72ea6432afdd1125
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 03:46:03 +0000

    closed db conn, changed from mysql to mysqli, removed company info

--- bin/show.inventory.userlist.php
+++ bin/show.inventory.userlist.php
@@ -3,8 +3,8 @@
   include($Sitepath . '/function.php');
 
   function dbconn($server,$database,$user,$pass){
-    $db = mysql_connect($server,$user,$pass);
-    $db_select = mysql_select_db($database,$db);
+    $db = mysqli_connect($server,$user,$pass,$database);
+    $db_select = mysqli_select_db($db,$database);
     return $db;
   }
 
@@ -14,8 +14,8 @@
   $q_string .= "from users ";
   $q_string .= "where usr_id != 1 and usr_disabled = 0 and (usr_email like '%intrado.com' or usr_email like '%west.com') ";
   $q_string .= "order by usr_last,usr_first ";
-  $q_users = mysql_query($q_string, $db) or die($q_string . ": " . mysql_error());
-  while ($a_users = mysql_fetch_array($q_users)) {
+  $q_users = mysqli_query($db, $q_string, $db) or die($q_string . ": " . mysqli_error($db));
+  while ($a_users = mysqli_fetch_array($q_users)) {
 
     print $a_users['usr_email'] . "\n";
 
@@ -27,5 +27,7 @@
       }
     }
   }
+
+  mysqli_free_request($db);
 
 ?>
