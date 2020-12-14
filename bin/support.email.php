43ef5b3d0376cf7afdade09f1d807d666ec0a387
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 04:08:46 +0000

    removed company info

--- bin/support.email.php
+++ bin/support.email.php
@@ -30,7 +30,7 @@
   $debug = 'yes';
   $debug = 'no';
 
-  $headers  = "From: Inventory Management <inventory@incojs01.scc911.com>\r\n";
+  $headers  = "From: Inventory Management <inventory@" . $hostname . ">\r\n";
   $headers .= "MIME-Version: 1.0\r\n";
   $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
