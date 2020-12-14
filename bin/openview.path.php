42bfd45cd67d2faab354666f066ececc68ae4bfe
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 03:49:21 +0000

    closed db conn, changed from mysql to mysqli, removed company info

--- bin/openview.path.php
+++ bin/openview.path.php
@@ -3,7 +3,6 @@
 # Script: openview.path.php
 # Owner: Carl Schelin
 # Coding Standard 3.0 Applied
-# See: https://incowk01/makers/index.php/Coding_Standards
 # Description: 
 # 
 
@@ -11,8 +10,8 @@
   include($Sitepath . '/function.php');
 
   function dbconn($server,$database,$user,$pass){
-    $db = mysql_connect($server,$user,$pass);
-    $db_select = mysql_select_db($database,$db);
+    $db = mysqli_connect($server,$user,$pass,$database);
+    $db_select = mysqli_select_db($db,$database);
     return $db;
   }
 
@@ -26,19 +25,21 @@
   $q_string .= "left join interface on interface.int_companyid = inventory.inv_id ";
   $q_string .= "where inv_status = 0 and inv_ssh = 1 and int_openview = 1 ";
   $q_string .= "order by inv_name ";
-  $q_inventory = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-  if (mysql_num_rows($q_inventory) > 0) {
-    while ($a_inventory = mysql_fetch_array($q_inventory)) {
+  $q_inventory = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  if (mysqli_num_rows($q_inventory) > 0) {
+    while ($a_inventory = mysqli_fetch_array($q_inventory)) {
 
       $q_string  = "select alarm_id ";
       $q_string .= "from alarms ";
       $q_string .= "where alarm_timestamp > '" . $date . "' and alarm_companyid = " . $a_inventory['inv_id'] . " ";
-      $q_alarms = mysql_query($q_string) or die($q_string . ": " . mysql_error());
-      if (mysql_num_rows($q_alarms) == 0) {
+      $q_alarms = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+      if (mysqli_num_rows($q_alarms) == 0) {
 
         print $a_inventory['inv_name'] . "\n";
       }
     }
   }
+
+  mysqli_free_request($db);
 
 ?>
