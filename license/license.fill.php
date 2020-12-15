1a4c123a15e8cb41a1e19c83b5e28e4b48e1a1e3
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 07:19:30 -0700

    fixed functions

--- license/license.fill.php
+++ license/license.fill.php
@@ -20,8 +20,8 @@
       $formVars['id'] = 0;
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from licenses");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from licenses");
 
       $q_string  = "select lic_id,lic_vendor,lic_product,lic_date,lic_vendorpo,lic_po,lic_project,lic_quantity,lic_key,lic_serial,lic_domain ";
       $q_string .= "from licenses ";
@@ -30,7 +30,7 @@
       $a_licenses = mysqli_fetch_array($q_licenses);
       mysqli_free_result($q_licenses);
 
-      $licproject = return_Index($a_licenses['lic_project'], "select prod_id from products order by prod_name");
+      $licproject = return_Index($db, $a_licenses['lic_project'], "select prod_id from products order by prod_name");
 
       print "document.license.lic_vendor.value = '"   . mysqli_real_escape_string($a_licenses['lic_vendor'])   . "';\n";
       print "document.license.lic_product.value = '"  . mysqli_real_escape_string($a_licenses['lic_product'])  . "';\n";
@@ -48,7 +48,7 @@
       print "document.license.update.disabled = false;\n\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
