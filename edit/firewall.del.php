779ba09bb8fe494b6b0c9c3108723da2bda4bd35
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:15:21 -0700

    fixed functions

--- edit/firewall.del.php
+++ edit/firewall.del.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from firewall");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from firewall");
 
       $q_string  = "delete ";
       $q_string .= "from firewall ";
@@ -29,7 +29,7 @@
 
       print "alert('Firewall Rule deleted.');\n";
     } else {
-      logaccess($_SESSION['uid'], $package, "Access denied");
+      logaccess($db, $_SESSION['uid'], $package, "Access denied");
     }
   }
 ?>
