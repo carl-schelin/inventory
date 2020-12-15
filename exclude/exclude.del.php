d2a368083afdca989991070d9e02f12eb024e217
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 07:25:41 -0700

    fixed functions

--- exclude/exclude.del.php
+++ exclude/exclude.del.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from excludes");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from excludes");
 
       $q_string  = "select ex_deleted ";
       $q_string .= "from excludes ";
@@ -47,7 +47,7 @@
 
       print "clear_fields();\n";
     } else {
-      logaccess($_SESSION['uid'], $package, "Access denied");
+      logaccess($db, $_SESSION['uid'], $package, "Access denied");
     }
   }
 ?>
