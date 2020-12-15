39cf73f6c0a940d3db128626e203c9dc679a5a75
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:18:33 -0700

    fixed functions

--- show/detail.mysql.php
+++ show/detail.mysql.php
@@ -11,7 +11,7 @@
 
   $package = "detail.mysql.php";
 
-  logaccess($formVars['uid'], $package, "Accessing the script.");
+  logaccess($db, $formVars['uid'], $package, "Accessing the script.");
 
   header('Content-Type: text/javascript');
 
@@ -57,14 +57,14 @@
   $output  = "<table class=\"ui-styled-table\">";
   $output .= "<tr>";
   $output .= "  <th class=\"" . $class_header . "\">";
-  if (check_userlevel($AL_Edit)) {
-    if (check_grouplevel($a_inventory['inv_manager'])) {
+  if (check_userlevel($db, $AL_Edit)) {
+    if (check_grouplevel($db, $a_inventory['inv_manager'])) {
       $output .= "<a href=\"" . $Editroot . "/inventory.php?server=" . $formVars['id'] . "\" target=\"_blank\"><img src=\"" . $Imgsroot . "/pencil.gif\">";
     }
   }
   $output .= "System Information";
-  if (check_userlevel($AL_Edit)) {
-    if (check_grouplevel($a_inventory['inv_manager'])) {
+  if (check_userlevel($db, $AL_Edit)) {
+    if (check_grouplevel($db, $a_inventory['inv_manager'])) {
       $output .= "</a>";
     }
   }
