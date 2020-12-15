ac314158b2e8f50b0f1b0f9eb4bda1de02f89d03
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:15:22 -0700

    fixed functions

--- edit/software.fill.php
+++ edit/software.fill.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from software");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from software");
 
       $q_string  = "select sw_id,sw_companyid,sw_vendor,sw_product,sw_software,sw_type,sw_group,sw_eol,sw_cert,";
       $q_string .= "sw_licenseid,sw_supportid,sw_department,sw_facing,sw_notification,sw_primary,sw_eolticket,sw_locked ";
@@ -30,12 +30,12 @@
       $a_software = mysqli_fetch_array($q_software);
       mysqli_free_result($q_software);
 
-      $group         = return_Index($a_software['sw_group'],       "select grp_id from groups where grp_disabled = 0 order by grp_name");
-      $product       = return_Index($a_software['sw_product'],     "select prod_id from products where prod_id != 0 order by prod_name");
-      $cert          = return_Index($a_software['sw_cert'],        "select cert_id from certs order by cert_url");
-      $support       = return_Index($a_software['sw_supportid'],   "select sup_id from support order by sup_company,sup_contract");
-      $license       = return_Index($a_software['sw_licenseid'],   "select lic_id from licenses left join products on products.prod_id = licenses.lic_project order by prod_name,lic_key,lic_id");
-      $department    = return_Index($a_software['sw_department'],  "select dep_id from department order by dep_unit,dep_name");
+      $group         = return_Index($db, $a_software['sw_group'],       "select grp_id from groups where grp_disabled = 0 order by grp_name");
+      $product       = return_Index($db, $a_software['sw_product'],     "select prod_id from products where prod_id != 0 order by prod_name");
+      $cert          = return_Index($db, $a_software['sw_cert'],        "select cert_id from certs order by cert_url");
+      $support       = return_Index($db, $a_software['sw_supportid'],   "select sup_id from support order by sup_company,sup_contract");
+      $license       = return_Index($db, $a_software['sw_licenseid'],   "select lic_id from licenses left join products on products.prod_id = licenses.lic_project order by prod_name,lic_key,lic_id");
+      $department    = return_Index($db, $a_software['sw_department'],  "select dep_id from department order by dep_unit,dep_name");
 
       print "document.edit.sw_vendor.value = '"       . mysqli_real_escape_string($a_software['sw_vendor'])       . "';\n";
       print "document.edit.sw_software.value = '"     . mysqli_real_escape_string($a_software['sw_software'])     . "';\n";
@@ -71,7 +71,7 @@
 
       print "document.edit.sw_update.disabled = false;\n";
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
