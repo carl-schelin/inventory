eb2f449b32945b451f079f24d48b4f8e4cee6dc7
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 07:22:12 -0700

    fixed functions

--- bugs/ticket.fill.php
+++ bugs/ticket.fill.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from bugs");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from bugs");
 
       $q_string  = "select bug_discovered,bug_closed,bug_subject ";
       $q_string .= "from bugs ";
@@ -46,7 +46,7 @@
       print "document.start.bugupdate.disabled = false;\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
