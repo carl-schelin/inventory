5152578dbd47917ea3f0dc753f2d667d588d5683
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 20:22:22 -0700

    fixed functions

--- security/severity.mysql.php
+++ security/severity.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel($AL_Edit)) {
+    if (check_userlevel($db, $AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']           = clean($_GET['id'],         10);
         $formVars['sev_name']     = clean($_GET['sev_name'],   60);
@@ -30,7 +30,7 @@
         }
 
         if (strlen($formVars['sev_name']) > 0) {
-          logaccess($_SESSION['uid'], $package, "Building the query.");
+          logaccess($db, $_SESSION['uid'], $package, "Building the query.");
 
           $q_string =
             "sev_name     = \"" . $formVars['sev_name']   . "\"";
@@ -44,7 +44,7 @@
             $message = "Severity updated.";
           }
 
-          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['sev_name']);
+          logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['sev_name']);
 
           mysqli_query($db, $query) or die($query . ": " . mysqli_error($db));
 
@@ -55,7 +55,7 @@
       }
 
 
-      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+      logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
       $output  = "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
@@ -87,7 +87,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel($AL_Admin)) {
+      if (check_userlevel($db, $AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -106,7 +106,7 @@
           $linkend   = "</a>";
 
           $output   .= "<tr>";
-          if (check_userlevel($AL_Admin)) {
+          if (check_userlevel($db, $AL_Admin)) {
             $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel   . 'x'                     . $linkend . "</td>";
           }
           $output   .= "  <td class=\"ui-widget-content delete\">" . $linkdel   . $a_severity['sev_id']   . $linkend . "</td>";
@@ -130,7 +130,7 @@
       print "document.severity.sev_name.focus();\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
