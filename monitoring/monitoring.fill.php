a817f44e678be8eb2e5546ea45c12958a1da0e82
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 20:31:33 -0700

    fixed functions

--- monitoring/monitoring.fill.php
+++ monitoring/monitoring.fill.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from organization");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from organization");
 
 #+-----------------+---------+------+-----+---------+----------------+
 #| Field           | Type    | Null | Key | Default | Extra          |
@@ -56,7 +56,7 @@
       print "document.monitoring.id.value = " . $formVars['id'] . ";\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
