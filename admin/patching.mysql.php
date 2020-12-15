83c6dfc8a7b47e4fa310f15602da60d7b8b4d6f0
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 18:55:57 -0700

    fixed functions

--- admin/patching.mysql.php
+++ admin/patching.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel($AL_Edit)) {
+    if (check_userlevel($db, $AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']            = clean($_GET['id'],            10);
         $formVars['patch_name']    = clean($_GET['patch_name'],    60);
@@ -33,7 +33,7 @@
         }
 
         if (strlen($formVars['patch_name']) > 0) {
-          logaccess($_SESSION['uid'], $package, "Building the query.");
+          logaccess($db, $_SESSION['uid'], $package, "Building the query.");
 
           $q_string =
             "patch_name   = \"" . $formVars['patch_name']   . "\"," .
@@ -50,7 +50,7 @@
             $message = "Patch Description updated.";
           }
 
-          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['dev_type']);
+          logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['dev_type']);
 
           mysqli_query($db, $query) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $query . "&mysql=" . mysqli_error($db)));
 
@@ -61,7 +61,7 @@
       }
 
 
-      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+      logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
       $output  = "<p></p>\n";
       $output .= "<table class=\"ui-styled-table\">\n";
@@ -132,7 +132,7 @@
       print "document.patching.patch_group[0].selecgted = true;\n";
       print "document.patching.patch_date.value = '';\n";
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
