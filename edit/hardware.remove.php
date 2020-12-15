847492cf46cb7efd569112befb1bb8f10cbf6375
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 14:20:28 -0700

    Updated to remove company info

--- edit/hardware.remove.php
+++ edit/hardware.remove.php
@@ -2,7 +2,6 @@
 # Script: hardware.remove.php
 # Owner: Carl Schelin
 # Coding Standard 3.0 Applied
-# See: https://incowk01/makers/index.php/Coding_Standards
 # Description: Marking a hardware record as deleted
 
   header('Content-Type: text/javascript');
