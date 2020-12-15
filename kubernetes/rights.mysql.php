6dfe6846442202e60af1c54704ec6ba0357e6c2c
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:20:25 -0700

    fixed functions

--- kubernetes/rights.mysql.php
+++ kubernetes/rights.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel($AL_Edit)) {
+    if (check_userlevel($db, $AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']                    = clean($_GET['id'],                    10);
         $formVars['rgt_type']              = clean($_GET['rgt_type'],              10);
@@ -86,7 +86,7 @@
         }
 
         if (strlen($formVars['rgt_apigroup']) > 0) {
-          logaccess($_SESSION['uid'], $package, "Building the query.");
+          logaccess($db, $_SESSION['uid'], $package, "Building the query.");
 
           $q_string =
             "rgt_type               =   " . $formVars['rgt_type']               . "," . 
@@ -111,7 +111,7 @@
             $message = "Rights updated.";
           }
 
-          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['rgt_id']);
+          logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['rgt_id']);
 
           mysqli_query($db, $query) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $query . "&mysql=" . mysqli_error($db)));
 
@@ -122,7 +122,7 @@
       }
 
 
-      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+      logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
       $output  = "<p></p>\n";
       $output .= "<table class=\"ui-styled-table\">\n";
@@ -148,7 +148,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel($AL_Admin)) {
+      if (check_userlevel($db, $AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\" width=\"60\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\" width=\"30\">Id</th>\n";
@@ -222,7 +222,7 @@
           }
 
           $output .= "<tr>";
-          if (check_userlevel($AL_Admin)) {
+          if (check_userlevel($db, $AL_Admin)) {
             $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel   . "</td>";
           }
           $output .= "  <td class=\"" . $class . " delete\">"   . $linkstart . $a_rights['rgt_id']          . $linkend . "</td>";
@@ -266,7 +266,7 @@
       print "document.rights.rgt_update.checked = false;\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
