986f3f699a37cee95f2d6f4c3ba81c51730f999c
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 20:53:08 -0700

    fixed functions

--- rsdp/system/san.mysql.php
+++ rsdp/system/san.mysql.php
@@ -21,7 +21,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel($AL_Edit)) {
+    if (check_userlevel($db, $AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']           = clean($_GET['id'],           10);
         $formVars['san_wwnnzone'] = clean($_GET['san_wwnnzone'], 20);
@@ -31,7 +31,7 @@
         }
 
         if (strlen($formVars['san_wwnnzone']) > 0) {
-          logaccess($_SESSION['uid'], $package, "Building the query.");
+          logaccess($db, $_SESSION['uid'], $package, "Building the query.");
 
           $q_string =
             "san_rsdp     =   " . $formVars['rsdp']         . "," . 
@@ -46,7 +46,7 @@
             $message = "SAN updated.";
           }
 
-          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['rsdp']);
+          logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['rsdp']);
 
           mysqli_query($db, $query) or die($query . ": " . mysqli_error($db));
 
@@ -57,7 +57,7 @@
       }
 
 
-      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+      logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
       $output  = "<p></p>\n";
       $output .= "<table class=\"ui-styled-table\">\n";
@@ -136,7 +136,7 @@
       print "document.san.san_wwnnzone.value = '';\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
