6b21595022531a9252fc7a180bba6790ed101eb3
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 16:25:23 -0700

    Updated to fix functions

--- datacenter/city.fill.php
+++ datacenter/city.fill.php
@@ -31,8 +31,8 @@
 
       $state = return_Index($db, $a_cities['ct_state'], "select st_id from states order by st_state");
 
-      print "document.cities.ct_city.value = '"    . mysqli_real_escape_string($a_cities['ct_city'])    . "';\n";
-      print "document.cities.ct_clli.value = '"    . mysqli_real_escape_string($a_cities['ct_clli'])    . "';\n";
+      print "document.cities.ct_city.value = '"    . mysqli_real_escape_string($db, $a_cities['ct_city'])    . "';\n";
+      print "document.cities.ct_clli.value = '"    . mysqli_real_escape_string($db, $a_cities['ct_clli'])    . "';\n";
 
       print "document.cities.ct_state['" . $state . "'].selected = true;\n";
 
