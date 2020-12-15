469e1172f3208f00e283a7697f7cb2b8e7bd3d2f
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 18:55:57 -0700

    fixed functions

--- admin/maintenance.windows.php
+++ admin/maintenance.windows.php
@@ -13,7 +13,7 @@
 
   $package = "maintenance.windows.php";
 
-  logaccess($_SESSION['uid'], $package, "Accessing script");
+  logaccess($db, $_SESSION['uid'], $package, "Accessing script");
 
 ?>
 <!DOCTYPE HTML>
@@ -34,7 +34,7 @@
 <script type="text/javascript">
 
 <?php
-  if (check_userlevel($AL_Admin)) {
+  if (check_userlevel($db, $AL_Admin)) {
 ?>
 function delete_line( p_script_url ) {
   var answer = confirm("Delete this Maintenance Window?")
