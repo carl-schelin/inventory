f97d79f5446ba92e25af85d1d613281323d1e0a3
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:29:13 -0700

    fixed functions

--- listings/icladmins.php
+++ listings/icladmins.php
@@ -13,10 +13,10 @@
 
   $package = "icladmins.php";
 
-  logaccess($_SESSION['uid'], $package, "Viewing the ICL server listing table");
+  logaccess($db, $_SESSION['uid'], $package, "Viewing the ICL server listing table");
 
 # If group or an admin, allow access
-  if (check_grouplevel($GRP_ICLAdmins)) {
+  if (check_grouplevel($db, $GRP_ICLAdmins)) {
 
 ?>
 <!DOCTYPE HTML>
