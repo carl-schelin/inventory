452662e09fb2c2993f611689f8edc38a08077e91
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:18:33 -0700

    fixed functions

--- show/logs.mysql.php
+++ show/logs.mysql.php
@@ -11,7 +11,7 @@
 
   $package = "logs.mysql.php";
 
-  logaccess($formVars['uid'], $package, "Accessing the script.");
+  logaccess($db, $formVars['uid'], $package, "Accessing the script.");
 
   header('Content-Type: text/javascript');
 
