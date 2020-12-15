9c998ab2e880b1a52371811794bfb50e8cf9089a
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:20:25 -0700

    fixed functions

--- kubernetes/rights.fill.php
+++ kubernetes/rights.fill.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from rights");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from rights");
 
       $q_string  = "select rgt_type,rgt_apigroup,rgt_resource,rgt_get,rgt_list,rgt_watch,rgt_impersonate,rgt_create,rgt_delete,rgt_deletecollection,rgt_patch,rgt_update ";
       $q_string .= "from rights ";
@@ -29,8 +29,8 @@
       $a_rights = mysqli_fetch_array($q_rights);
       mysqli_free_result($q_rights);
 
-      $apigroups = return_Index($a_rights['rgt_apigroup'], "select api_id from apigroups order by api_name");
-      $resources = return_Index($a_rights['rgt_resource'], "select res_id from resources order by res_name");
+      $apigroups = return_Index($db, $a_rights['rgt_apigroup'], "select api_id from apigroups order by api_name");
+      $resources = return_Index($db, $a_rights['rgt_resource'], "select res_id from resources order by res_name");
 
       print "document.rights.rgt_apigroup['" . $apigroups . "'].selected = true;\n";
       print "document.rights.rgt_resource['" . $resources . "'].selected = true;\n";
@@ -86,7 +86,7 @@
       print "document.rights.id.value = " . $formVars['id'] . ";\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
