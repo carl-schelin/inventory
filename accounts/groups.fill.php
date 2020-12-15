8eedac03c4c0188932a28c333de5804064a8c05c
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 18:37:54 -0700

    fixed functions

--- accounts/groups.fill.php
+++ accounts/groups.fill.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Admin)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from groups");
+    if (check_userlevel($db, $AL_Admin)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from groups");
 
       $q_string  = "select grp_disabled,grp_name,grp_snow,grp_email,grp_magic,grp_category,grp_changelog,grp_manager,grp_clfile,";
       $q_string .= "grp_clserver,grp_report,grp_organization,grp_role,grp_clscript,grp_status,grp_server,grp_import ";
@@ -30,9 +30,9 @@
       $a_groups = mysqli_fetch_array($q_groups);
       mysqli_free_result($q_groups);
 
-      $organization = return_Index($a_groups['grp_organization'], "select org_id from organizations order by org_name");
-      $role         = return_Index($a_groups['grp_role'],         "select role_id from roles order by role_name");
-      $manager      = return_Index($a_groups['grp_manager'],      "select usr_id from users where usr_disabled = 0 order by usr_last,usr_first");
+      $organization = return_Index($db, $a_groups['grp_organization'], "select org_id from organizations order by org_name");
+      $role         = return_Index($db, $a_groups['grp_role'],         "select role_id from roles order by role_name");
+      $manager      = return_Index($db, $a_groups['grp_manager'],      "select usr_id from users where usr_disabled = 0 order by usr_last,usr_first");
 
       print "document.groups.grp_name.value = '"      . mysqli_real_escape_string($a_groups['grp_name'])      . "';\n";
       print "document.groups.grp_snow.value = '"      . mysqli_real_escape_string($a_groups['grp_snow'])      . "';\n";
@@ -70,7 +70,7 @@
       print "document.groups.id.value = " . $formVars['id'] . ";\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
