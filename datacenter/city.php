d5ec093158ffdddd3a35759adac1b56348e9b363
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:30:51 -0700

    fixed functions

--- datacenter/city.php
+++ datacenter/city.php
@@ -13,7 +13,7 @@
 
   $package = "city.php";
 
-  logaccess($_SESSION['uid'], $package, "Viewing the City table");
+  logaccess($db, $_SESSION['uid'], $package, "Viewing the City table");
 
 ?>
 <!DOCTYPE HTML>
@@ -34,7 +34,7 @@
 <script type="text/javascript">
 
 <?php
-  if (check_userlevel($AL_Admin)) {
+  if (check_userlevel($db, $AL_Admin)) {
 ?>
 function delete_line( p_script_url ) {
   var answer = confirm("Delete this city?")
