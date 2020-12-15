7f40d57d23d9b9b9d5b3dd35e3d45bff9bfbad43
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:30:51 -0700

    fixed functions

--- datacenter/country.mysql.php
+++ datacenter/country.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel($AL_Edit)) {
+    if (check_userlevel($db, $AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']               = clean($_GET['id'],              10);
         $formVars['cn_acronym']       = clean($_GET['cn_acronym'],      10);
@@ -31,7 +31,7 @@
         }
     
         if (strlen($formVars['cn_country']) > 0) {
-          logaccess($_SESSION['uid'], $package, "Building the query.");
+          logaccess($db, $_SESSION['uid'], $package, "Building the query.");
 
           $q_string =
             "cn_acronym = \"" . $formVars['cn_acronym'] . "\"," . 
@@ -46,7 +46,7 @@
             $message = "Country modified.";
           }
 
-          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['cn_country']);
+          logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['cn_country']);
 
           mysqli_query($db, $query) or die($query . ": " . mysqli_error($db));
 
@@ -57,7 +57,7 @@
       }
 
 
-      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+      logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
       $output  = "<p></p>\n";
       $output .= "<table class=\"ui-styled-table\">\n";
@@ -83,7 +83,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel($AL_Admin)) {
+      if (check_userlevel($db, $AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -103,7 +103,7 @@
           $linkend   = "</a>";
 
           $output .= "<tr>";
-          if (check_userlevel($AL_Admin)) {
+          if (check_userlevel($db, $AL_Admin)) {
             $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel    . "</td>";
           }
           $output .= "  <td class=\"ui-widget-content delete\">"   . $linkstart . $a_country['cn_id']          . $linkend . "</td>";
@@ -124,7 +124,7 @@
       print "document.getElementById('table_mysql').innerHTML = '" . mysqli_real_escape_string($output) . "';\n\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
