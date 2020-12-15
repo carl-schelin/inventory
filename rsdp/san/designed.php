6b1cc27741a34d79efdbc6dc96f2f73ad8fca1a1
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 07:17:23 -0700

    fixed functions

--- rsdp/san/designed.php
+++ rsdp/san/designed.php
@@ -15,7 +15,7 @@
 
   $package = "designed.php";
 
-  logaccess($_SESSION['uid'], $package, "Accessing script");
+  logaccess($db, $_SESSION['uid'], $package, "Accessing script");
 
   if (isset($_GET['rsdp'])) {
     $formVars['rsdp'] = clean($_GET['rsdp'], 10);
