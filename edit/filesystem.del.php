a7fca4cd4aaab21bcac60dc51e18987dbf68706d
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 14:20:28 -0700

    Updated to remove company info

--- edit/filesystem.del.php
+++ edit/filesystem.del.php
@@ -2,7 +2,6 @@
 # Script: filesystem.del.php
 # Owner: Carl Schelin
 # Coding Standard 3.0 Applied
-# See: https://incowk01/makers/index.php/Coding_Standards
 # Description: Delete filesystems
 
   header('Content-Type: text/javascript');
