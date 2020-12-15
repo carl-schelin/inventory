1a246acaa52e53493da1c611779a18cb4b40a25f
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 18:55:57 -0700

    fixed functions

--- admin/support.mysql.php
+++ admin/support.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel($AL_Edit)) {
+    if (check_userlevel($db, $AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']             = clean($_GET['id'],                50);
         $formVars['sup_company']    = clean($_GET['sup_company'],       50);
@@ -37,7 +37,7 @@
         }
 
         if (strlen($formVars['sup_company']) > 0) {
-          logaccess($_SESSION['uid'], $package, "Building the query.");
+          logaccess($db, $_SESSION['uid'], $package, "Building the query.");
 
           $q_string =
             "sup_company    = \"" . $formVars['sup_company']    . "\"," .
@@ -58,7 +58,7 @@
             $message = "Support Contract updated.";
           }
 
-          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['sup_company']);
+          logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['sup_company']);
 
           mysqli_query($db, $query) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $query . "&mysql=" . mysqli_error($db)));
 
@@ -69,7 +69,7 @@
       }
 
 
-      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+      logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
       $output  = "<p></p>\n";
       $output .= "<table class=\"ui-styled-table\">\n";
@@ -95,7 +95,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel($AL_Admin)) {
+      if (check_userlevel($db, $AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -130,7 +130,7 @@
           $linkend   = "</a>";
 
           $output .= "<tr>";
-          if (check_userlevel($AL_Admin)) {
+          if (check_userlevel($db, $AL_Admin)) {
             $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel   . "</td>";
           }
           $output .= "  <td class=\"ui-widget-content delete\">"   . $linkstart . $a_support['sup_id']       . $linkend . "</td>";
@@ -164,7 +164,7 @@
       print "document.support.sup_wiki.value = '';\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
