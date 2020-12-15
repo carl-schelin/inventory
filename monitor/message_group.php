d8247fe5d74246cc8ef6b84a33bce5ad6a6c4959
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 16:38:10 -0700

    Updated to remove company info

--- monitor/message_group.php
+++ monitor/message_group.php
@@ -2,7 +2,6 @@
 # Script: message_group.php
 # Owner: Carl Schelin
 # Coding Standard 3.0 Applied
-# See: https://incowk01/makers/index.php/Coding_Standards
 # Description: 
 
   include('settings.php');
