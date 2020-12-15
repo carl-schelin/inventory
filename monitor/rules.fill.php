ab857043c9a0ec04805a7923b9dc16c98b5c231d
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 20:19:36 -0700

    fixed functions

--- monitor/rules.fill.php
+++ monitor/rules.fill.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from rules");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from rules");
 
       $q_string  = "select rule_description,rule_annotate,rule_group,rule_source,rule_application,rule_object,rule_message,rule_page,";
       $q_string .= "rule_email,rule_autoack,rule_deleted ";
@@ -30,12 +30,12 @@
       $a_rules = mysqli_fetch_array($q_rules);
       mysqli_free_result($q_rules);
 
-      $rule_parent      = return_Index($a_rules['rule_parent'],      "select rule_id from rules         where rule_deleted = 0 order by rule_description ");
-      $rule_group       = return_Index($a_rules['rule_group'],       "select key_id  from keywords      where key_deleted = 0  order by key_description  ");
-      $rule_source      = return_Index($a_rules['rule_source'],      "select src_id  from source_node   where src_deleted = 0  order by src_node         ");
-      $rule_application = return_Index($a_rules['rule_application'], "select app_id  from application   where app_deleted = 0  order by app_description  ");
-      $rule_object      = return_Index($a_rules['rule_object'],      "select obj_id  from objects       where obj_deleted = 0  order by obj_name         ");
-      $rule_message     = return_Index($a_rules['rule_message'],     "select msg_id  from message_group where msg_deleted = 0  order by msg_group        ");
+      $rule_parent      = return_Index($db, $a_rules['rule_parent'],      "select rule_id from rules         where rule_deleted = 0 order by rule_description ");
+      $rule_group       = return_Index($db, $a_rules['rule_group'],       "select key_id  from keywords      where key_deleted = 0  order by key_description  ");
+      $rule_source      = return_Index($db, $a_rules['rule_source'],      "select src_id  from source_node   where src_deleted = 0  order by src_node         ");
+      $rule_application = return_Index($db, $a_rules['rule_application'], "select app_id  from application   where app_deleted = 0  order by app_description  ");
+      $rule_object      = return_Index($db, $a_rules['rule_object'],      "select obj_id  from objects       where obj_deleted = 0  order by obj_name         ");
+      $rule_message     = return_Index($db, $a_rules['rule_message'],     "select msg_id  from message_group where msg_deleted = 0  order by msg_group        ");
 
       print "document.rules.rule_description.value = '" . mysqli_real_escape_string($a_rules['rule_description']) . "';\n";
       print "document.rules.rule_annotate.value = '"    . mysqli_real_escape_string($a_rules['rule_annotate']) . "';\n";
@@ -64,7 +64,7 @@
       print "document.rules.id.value = " . $formVars['id'] . ";\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
