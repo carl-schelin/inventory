d6ad0ed73746803c2ac1522cfef44a3fc43f47df
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 07:56:25 -0700

    fixed functions

--- rsdp/application/installed.php
+++ rsdp/application/installed.php
@@ -259,7 +259,7 @@ an email will be sent notifying the responsible team or individual that their ta
 
 </div>
 
-<?php print submit_RSDP( $formVars['rsdp'], 15, $RSDProot . "/application/installed.mysql.php", "rsdp_apppoc", "rsdp_application", 0); ?>
+<?php print submit_RSDP($db, " $formVars['rsdp'], 15, $RSDProot . "/application/installed.mysql.php", "rsdp_apppoc", "rsdp_application", 0); ?>
 
 <div id="tabs">
 
@@ -287,14 +287,14 @@ an email will be sent notifying the responsible team or individual that their ta
 
 <div id="tabs-1">
 
-<?php print request_Header($formVars['rsdp']); ?>
+<?php print request_Header($db, "$formVars['rsdp']); ?>
 
 </div>
 
 
 <div id="tabs-2">
 
-<?php print request_Server($formVars['rsdp']); ?>
+<?php print request_Server($db, "$formVars['rsdp']); ?>
 
 </div>
 
@@ -308,7 +308,7 @@ an email will be sent notifying the responsible team or individual that their ta
 <tr>
   <td id="app_inscheck"  ><input type="checkbox" id="check-1" name="app_inscheck"   onchange="validate_Form();"><label for="check-1"></label></td>
 </tr>
-<?php print return_Checklist( $formVars['rsdp'], 15); ?>
+<?php print return_Checklist($db, " $formVars['rsdp'], 15); ?>
 </table>
 
 </div>
