9f57338c7216fba86b975f2a11e28dc4380acea7
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:04:06 -0700

    fixed functions

--- reports/openview.del.php
+++ reports/openview.del.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from alarm_blocks");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from alarm_blocks");
 
       $q_string  = "select block_text ";
       $q_string .= "from alarm_blocks ";
@@ -44,7 +44,7 @@
       print "clear_fields();\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Access denied");
+      logaccess($db, $_SESSION['uid'], $package, "Access denied");
     }
   }
 ?>
