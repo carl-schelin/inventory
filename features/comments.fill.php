9f137339714d0144c2cc4c722a230b0ec608e06c
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 20:28:46 -0700

    fixed functions

--- features/comments.fill.php
+++ features/comments.fill.php
@@ -20,8 +20,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from features_detail");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from features_detail");
 
       $q_string  = "select feat_text,feat_timestamp,feat_user ";
       $q_string .= "from features_detail ";
@@ -30,7 +30,7 @@
       $a_features_detail = mysqli_fetch_array($q_features_detail);
       mysqli_free_result($q_features_detail);
 
-      $selected = return_Index($a_features_detail['feat_user'],       "select usr_id from users where usr_disabled = 0 order by usr_last,usr_first");
+      $selected = return_Index($db, $a_features_detail['feat_user'],       "select usr_id from users where usr_disabled = 0 order by usr_last,usr_first");
 
       print "document.start.feat_text.value = '"      . mysqli_real_escape_string($a_features_detail['feat_text'])      . "';\n";
       print "document.start.feat_timestamp.value = '" . mysqli_real_escape_string($a_features_detail['feat_timestamp']) . "';\n";
@@ -42,7 +42,7 @@
       print "document.start.featupdate.disabled = false;\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
