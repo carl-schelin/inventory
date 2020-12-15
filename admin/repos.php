02de637b8cd38a2385b74aa3316bdee15242bde8
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 18:55:57 -0700

    fixed functions

--- admin/repos.php
+++ admin/repos.php
@@ -13,7 +13,7 @@
 
   $package = "repos.php";
 
-  logaccess($_SESSION['uid'], $package, "Accessing script");
+  logaccess($db, $_SESSION['uid'], $package, "Accessing script");
 
 ?>
 <!DOCTYPE HTML>
