90e22ac11f2a9a6d9f6d7c604ade84346df372de
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 21:01:27 -0700

    fixed functions

--- rsdp/admin/system.fill.php
+++ rsdp/admin/system.fill.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from operatingsystem");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from operatingsystem");
 
       $q_string  = "select os_vendor,os_software,os_exception ";
       $q_string .= "from operatingsystem ";
@@ -41,7 +41,7 @@
       print "document.dialog.id.value = " . $formVars['id'] . ";\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
