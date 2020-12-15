eba00bc1ab804b2ffdacba97ded0edc946c69a49
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 18:55:57 -0700

    fixed functions

--- admin/timezones.mysql.php
+++ admin/timezones.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel($AL_Edit)) {
+    if (check_userlevel($db, $AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']               = clean($_GET['id'],               10);
         $formVars['zone_name']        = clean($_GET['zone_name'],        10);
@@ -32,7 +32,7 @@
         }
 
         if (strlen($formVars['zone_name']) > 0) {
-          logaccess($_SESSION['uid'], $package, "Building the query.");
+          logaccess($db, $_SESSION['uid'], $package, "Building the query.");
 
           $q_string =
             "zone_name        = \"" . $formVars['zone_name']        . "\"," .
@@ -48,7 +48,7 @@
             $message = "Time Zone updated.";
           }
 
-          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['zone_name']);
+          logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['zone_name']);
 
           mysqli_query($db, $query) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $query . "&mysql=" . mysqli_error($db)));
 
@@ -59,7 +59,7 @@
       }
 
 
-      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+      logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
       $output  = "<p></p>\n";
       $output .= "<table class=\"ui-styled-table\">\n";
@@ -85,7 +85,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel($AL_Admin)) {
+      if (check_userlevel($db, $AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -106,7 +106,7 @@
           $linkend   = "</a>";
 
           $output .= "<tr>";
-          if (check_userlevel($AL_Admin)) {
+          if (check_userlevel($db, $AL_Admin)) {
             $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel   . "</td>";
           }
           $output .= "  <td class=\"ui-widget-content delete\">"   . $linkstart . $a_zones['zone_id']          . $linkend . "</td>";
@@ -132,7 +132,7 @@
       print "document.zones.zone_offset.value = '';\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
