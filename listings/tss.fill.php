f8b4b9848798621cf2cfde148add7152bc8639f5
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:29:14 -0700

    fixed functions

--- listings/tss.fill.php
+++ listings/tss.fill.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from changelog");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from changelog");
 
       $q_string  = "select cl_id,cl_name ";
       $q_string .= "from changelog ";
@@ -34,7 +34,7 @@
       print "document.changelog.id.value = " . $formVars['id'] . ";\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
