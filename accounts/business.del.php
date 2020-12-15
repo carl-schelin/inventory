83a292316b12ac989a2100f55290cae7916adb61
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 18:37:54 -0700

    fixed functions

--- accounts/business.del.php
+++ accounts/business.del.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Admin)) {
-      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from business_unit");
+    if (check_userlevel($db, $AL_Admin)) {
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from business_unit");
 
       $q_string  = "delete ";
       $q_string .= "from business_unit ";
@@ -31,7 +31,7 @@
 
       print "clear_fields();\n";
     } else {
-      logaccess($_SESSION['uid'], $package, "Access denied");
+      logaccess($db, $_SESSION['uid'], $package, "Access denied");
     }
   }
 ?>
