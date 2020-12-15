c67981a9a972d266d0e96d0ad0d70a4ac98d5b3b
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:30:51 -0700

    fixed functions

--- datacenter/country.php
+++ datacenter/country.php
@@ -13,7 +13,7 @@
 
   $package = "country.php";
 
-  logaccess($_SESSION['uid'], $package, "Viewing the Country table");
+  logaccess($db, $_SESSION['uid'], $package, "Viewing the Country table");
 
 ?>
 <!DOCTYPE HTML>
@@ -34,7 +34,7 @@
 <script type="text/javascript">
 
 <?php
-  if (check_userlevel($AL_Admin)) {
+  if (check_userlevel($db, $AL_Admin)) {
 ?>
 function delete_line( p_script_url ) {
   var answer = confirm("Delete this Country?")
