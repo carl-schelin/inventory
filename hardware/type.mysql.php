60b7a85d9440ab5941c1ef16fe8e50eb97ceb46d
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:23:02 -0700

    fixed functions

--- hardware/type.mysql.php
+++ hardware/type.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel($AL_Edit)) {
+    if (check_userlevel($db, $AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']              = clean($_GET['id'],               10);
         $formVars['itp_name']        = clean($_GET['itp_name'],         30);
@@ -32,7 +32,7 @@
         }
 
         if (strlen($formVars['itp_name']) > 0) {
-          logaccess($_SESSION['uid'], $package, "Building the query.");
+          logaccess($db, $_SESSION['uid'], $package, "Building the query.");
 
           $q_string =
             "itp_name        = \"" . $formVars['itp_name']        . "\"," .
@@ -48,7 +48,7 @@
             $message = "Interface Type updated.";
           }
 
-          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['itp_name']);
+          logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['itp_name']);
 
           mysqli_query($db, $query) or die($query . ": " . mysqli_error($db));
 
@@ -59,7 +59,7 @@
       }
 
 
-      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+      logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
       $output  = "<p></p>\n";
       $output .= "<table class=\"ui-styled-table\">\n";
@@ -92,7 +92,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel($AL_Admin)) {
+      if (check_userlevel($db, $AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -113,7 +113,7 @@
           $linkend   = "</a>";
 
           $output .= "<tr>\n";
-          if (check_userlevel($AL_Admin)) {
+          if (check_userlevel($db, $AL_Admin)) {
             $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel . "x"                           . $linkend . "</td>\n";
           }
           $output .= "  <td class=\"ui-widget-content delete\">" . $linkstart . $a_inttype['itp_id']          . $linkend . "</td>\n";
@@ -138,7 +138,7 @@
       print "document.interfacetype.update.disabled = true;\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
