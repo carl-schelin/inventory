13aff274d9816cc42e641e77f267cba1b8be91c8
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 14:20:28 -0700

    Updated to remove company info

--- edit/network.del.php
+++ edit/network.del.php
@@ -2,7 +2,6 @@
 # Script: network.del.php
 # Owner: Carl Schelin
 # Coding Standard 3.0 Applied
-# See: https://incowk01/makers/index.php/Coding_Standards
 # Description: Delete a specified record from the database
 
   header('Content-Type: text/javascript');
