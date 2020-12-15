e0f27c1bbf212ff2615c5144da03e940151c9378
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:18:33 -0700

    fixed functions

--- show/routing.mysql.php
+++ show/routing.mysql.php
@@ -11,7 +11,7 @@
 
   $package = "routing.mysql.php";
 
-  logaccess($formVars['uid'], $package, "Accessing the script.");
+  logaccess($db, $formVars['uid'], $package, "Accessing the script.");
 
   header('Content-Type: text/javascript');
 
@@ -27,14 +27,14 @@
   $output .= "<table class=\"ui-styled-table\">";
   $output .= "<tr>";
   $output .= "  <th class=\"ui-state-default\">";
-  if (check_userlevel($AL_Edit)) {
-    if (check_grouplevel($a_inventory['inv_manager'])) {
+  if (check_userlevel($db, $AL_Edit)) {
+    if (check_grouplevel($db, $a_inventory['inv_manager'])) {
       $output .= "<a href=\"" . $Editroot . "/inventory.php?server=" . $formVars['id'] . "#routing\" target=\"_blank\"><img src=\"" . $Imgsroot . "/pencil.gif\">";
     }
   }
   $output .= "Routing Information";
-  if (check_userlevel($AL_Edit)) {
-    if (check_grouplevel($a_inventory['inv_manager'])) {
+  if (check_userlevel($db, $AL_Edit)) {
+    if (check_grouplevel($db, $a_inventory['inv_manager'])) {
       $output .= "</a>";
     }
   }
