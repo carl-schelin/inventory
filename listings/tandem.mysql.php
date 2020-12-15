c11be3dd31f32d82086370369d998e98176486f4
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 14:44:35 -0700

    Updated to fix functions

--- listings/tandem.mysql.php
+++ listings/tandem.mysql.php
@@ -208,7 +208,7 @@
 
         $output .= "</table>\n";
 
-        print "document.getElementById('automatic_mysql').innerHTML = '" . mysqli_real_escape_string($output) . "';\n\n";
+        print "document.getElementById('automatic_mysql').innerHTML = '" . mysqli_real_escape_string($db, $output) . "';\n\n";
       }
 
 # manual listing
@@ -281,7 +281,7 @@
 
       $output .= "</table>\n";
 
-      print "document.getElementById('manual_mysql').innerHTML = '" . mysqli_real_escape_string($output) . "';\n\n";
+      print "document.getElementById('manual_mysql').innerHTML = '" . mysqli_real_escape_string($db, $output) . "';\n\n";
 
     } else {
       logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
