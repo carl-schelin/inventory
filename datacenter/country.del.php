94201290ec08bc081ce0d97f9711e086a865cbfd
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:30:51 -0700

    fixed functions

--- datacenter/country.del.php
+++ datacenter/country.del.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Admin)) {
-      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from country");
+    if (check_userlevel($db, $AL_Admin)) {
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from country");
 
       $q_string  = "delete ";
       $q_string .= "from country ";
@@ -31,7 +31,7 @@
 
       print "clear_fields();\n";
     } else {
-      logaccess($_SESSION['uid'], $package, "Access denied");
+      logaccess($db, $_SESSION['uid'], $package, "Access denied");
     }
   }
 ?>
