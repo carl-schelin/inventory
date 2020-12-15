a6e0d0fff1cb43d170b67a9416c5371df013c05a
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 16:44:48 -0700

    Updated to fix functions

--- admin/maintenance.windows.fill.php
+++ admin/maintenance.windows.fill.php
@@ -28,7 +28,7 @@
       $a_window = mysqli_fetch_array($q_window);
       mysqli_free_result($q_window);
 
-      print "document.window.win_text.value = '" . mysqli_real_escape_string($a_window['win_text'])        . "';\n";
+      print "document.window.win_text.value = '" . mysqli_real_escape_string($db, $a_window['win_text'])        . "';\n";
 
       print "document.window.id.value = " . $formVars['id'] . ";\n";
 
