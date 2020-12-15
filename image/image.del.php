db8603989debf3e918e85b5e9bf5bd4692589be9
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 07:20:59 -0700

    fixed functions

--- image/image.del.php
+++ image/image.del.php
@@ -19,8 +19,8 @@
       $formVars['id'] = clean($_GET['id'], 10);
     }
 
-    if (check_userlevel($AL_Edit)) {
-      logaccess($_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from images");
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from images");
 
       $q_string  = "select img_file ";
       $q_string .= "from images ";
@@ -41,7 +41,7 @@
 
       print "clear_fields();\n";
     } else {
-      logaccess($_SESSION['uid'], $package, "Access denied");
+      logaccess($db, $_SESSION['uid'], $package, "Access denied");
     }
   }
 ?>
