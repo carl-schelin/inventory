9896b33c23aea87d2585292e50fe368d00006348
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:29:14 -0700

    fixed functions

--- listings/webapps.php
+++ listings/webapps.php
@@ -13,10 +13,10 @@
 
   $package = "webapps.php";
 
-  logaccess($_SESSION['uid'], $package, "Viewing the web listing table");
+  logaccess($db, $_SESSION['uid'], $package, "Viewing the web listing table");
 
 # If group or an admin, allow access
-  if (check_grouplevel($GRP_WebApps)) {
+  if (check_grouplevel($db, $GRP_WebApps)) {
 
 ?>
 <!DOCTYPE HTML>
