879b9b7a2a3d534827d47f987ce32812a50d00e5
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-14 19:23:02 -0700

    fixed functions

--- hardware/vlans.mysql.php
+++ hardware/vlans.mysql.php
@@ -20,7 +20,7 @@
       $formVars['update'] = -1;
     }
 
-    if (check_userlevel($AL_Edit)) {
+    if (check_userlevel($db, $AL_Edit)) {
       if ($formVars['update'] == 0 || $formVars['update'] == 1) {
         $formVars['id']               = clean($_GET['id'],                10);
         $formVars['vlan_vlan']        = clean($_GET['vlan_vlan'],         10);
@@ -36,7 +36,7 @@
         }
     
         if (strlen($formVars['vlan_vlan']) > 0) {
-          logaccess($_SESSION['uid'], $package, "Building the query.");
+          logaccess($db, $_SESSION['uid'], $package, "Building the query.");
 
           $q_string =
             "vlan_vlan        = \"" . $formVars['vlan_vlan']        . "\"," .
@@ -56,7 +56,7 @@
             $message = "VLan updated.";
           }
 
-          logaccess($_SESSION['uid'], $package, "Saving Changes to: " . $formVars['vlan_name']);
+          logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['vlan_name']);
 
           mysqli_query($db, $query) or die($query . ": " . mysqli_error($db));
 
@@ -67,7 +67,7 @@
       }
 
 
-      logaccess($_SESSION['uid'], $package, "Creating the table for viewing.");
+      logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
       $output  = "<p></p>\n";
       $output .= "<table class=\"ui-styled-table\">\n";
@@ -100,7 +100,7 @@
 
       $output .= "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      if (check_userlevel($AL_Admin)) {
+      if (check_userlevel($db, $AL_Admin)) {
         $output .= "  <th class=\"ui-state-default\">Del</th>\n";
       }
       $output .= "  <th class=\"ui-state-default\">Id</th>\n";
@@ -124,7 +124,7 @@
         $linkend   = "</a>";
 
         $output .= "<tr>";
-        if (check_userlevel($AL_Admin)) {
+        if (check_userlevel($db, $AL_Admin)) {
           $output .= "  <td class=\"ui-widget-content delete\">" . $linkdel . "x"                          . $linkend . "</td>";
         }
         $output .= "  <td class=\"ui-widget-content delete\">" . $linkstart . $a_vlans['vlan_id']          . $linkend . "</td>";
@@ -147,7 +147,7 @@
       print "document.vlans.update.disabled = true;\n";
 
     } else {
-      logaccess($_SESSION['uid'], $package, "Unauthorized access.");
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
   }
 ?>
