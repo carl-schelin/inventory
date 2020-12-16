ac1b83f8f8c64a111efb781cf5399c4e227c38c6
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 18:34:02 -0700

    Updated to fix functions

--- rsdp/index.mysql.php
+++ rsdp/index.mysql.php
@@ -223,7 +223,7 @@
 
       $output .= "</table>";
 
-      print "document.getElementById('table_mysql').innerHTML = '" . mysqli_real_escape_string($output) . "';\n";
+      print "document.getElementById('table_mysql').innerHTML = '" . mysqli_real_escape_string($db, $output) . "';\n";
 
     } else {
       logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
