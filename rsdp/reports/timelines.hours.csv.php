fd3af97a4f7a71a924c5ca467cf18566ed212fcb
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 19:39:45 -0700

    Updated to fix functions

--- rsdp/reports/timelines.hours.csv.php
+++ rsdp/reports/timelines.hours.csv.php
@@ -134,7 +134,7 @@
       $output .= "</body>";
       $output .= "</html>";
 
-      print  mysqli_real_escape_string($output);
+      print  mysqli_real_escape_string($db, $output);
 
     } else {
       logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
