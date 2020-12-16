18a55dce72c37afbaf20049c76166c0521359a9d
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 18:40:39 -0700

    Updated to add database connections

--- rsdp/build/build.php
+++ rsdp/build/build.php
@@ -9,7 +9,11 @@
   include($Loginpath . '/check.php');
   include($Sitepath . '/function.php');
   include($RSDPpath . '/function.php');
-  check_login('2');
+
+# connect to the database
+  $db = db_connect($DBserver, $DBname, $DBuser, $DBpassword);
+
+  check_login($db, $AL_Edit);
 
   $package = "build.php";
 
