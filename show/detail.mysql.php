b9e31d77010ba386c3e1474c66aa96bd8d1948d7
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 20:13:52 -0700

    Updated to fix functions

--- show/detail.mysql.php
+++ show/detail.mysql.php
@@ -424,5 +424,5 @@
 
 ?>
 
-document.getElementById('detail_mysql').innerHTML = '<?php print mysqli_real_escape_string($output); ?>';
+document.getElementById('detail_mysql').innerHTML = '<?php print mysqli_real_escape_string($db, $output); ?>';
 
