e64454ece58b5981a12df3f2202fe9e2241e3412
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 20:42:08 -0700

    Updated to fix functions

--- psaps/centurylink.fill.php
+++ psaps/centurylink.fill.php
@@ -29,19 +29,19 @@
       $a_psaps = mysqli_fetch_array($q_psaps);
       mysqli_free_result($q_psaps);
 
-      print "document.psaps.psap_customerid.value = '"    . mysqli_real_escape_string($a_psaps['psap_customerid'])    . "';\n";
-      print "document.psaps.psap_parentid.value = '"      . mysqli_real_escape_string($a_psaps['psap_parentid'])      . "';\n";
-      print "document.psaps.psap_ali_id.value = '"        . mysqli_real_escape_string($a_psaps['psap_ali_id'])        . "';\n";
-      print "document.psaps.psap_companyid.value = '"     . mysqli_real_escape_string($a_psaps['psap_companyid'])     . "';\n";
-      print "document.psaps.psap_psap_id.value = '"       . mysqli_real_escape_string($a_psaps['psap_psap_id'])       . "';\n";
-      print "document.psaps.psap_description.value = '"   . mysqli_real_escape_string($a_psaps['psap_description'])   . "';\n";
-      print "document.psaps.psap_lport.value = '"         . mysqli_real_escape_string($a_psaps['psap_lport'])         . "';\n";
-      print "document.psaps.psap_circuit_id.value = '"    . mysqli_real_escape_string($a_psaps['psap_circuit_id'])    . "';\n";
-      print "document.psaps.psap_pseudo_cid.value = '"    . mysqli_real_escape_string($a_psaps['psap_pseudo_cid'])    . "';\n";
-      print "document.psaps.psap_lec.value = '"           . mysqli_real_escape_string($a_psaps['psap_lec'])           . "';\n";
-      print "document.psaps.psap_texas.value = '"         . mysqli_real_escape_string($a_psaps['psap_texas'])         . "';\n";
-      print "document.psaps.psap_updated.value = '"       . mysqli_real_escape_string($a_psaps['psap_updated'])       . "';\n";
-      print "document.psaps.psap_delete.value = '"        . mysqli_real_escape_string($a_psaps['psap_delete'])        . "';\n";
+      print "document.psaps.psap_customerid.value = '"    . mysqli_real_escape_string($db, $a_psaps['psap_customerid'])    . "';\n";
+      print "document.psaps.psap_parentid.value = '"      . mysqli_real_escape_string($db, $a_psaps['psap_parentid'])      . "';\n";
+      print "document.psaps.psap_ali_id.value = '"        . mysqli_real_escape_string($db, $a_psaps['psap_ali_id'])        . "';\n";
+      print "document.psaps.psap_companyid.value = '"     . mysqli_real_escape_string($db, $a_psaps['psap_companyid'])     . "';\n";
+      print "document.psaps.psap_psap_id.value = '"       . mysqli_real_escape_string($db, $a_psaps['psap_psap_id'])       . "';\n";
+      print "document.psaps.psap_description.value = '"   . mysqli_real_escape_string($db, $a_psaps['psap_description'])   . "';\n";
+      print "document.psaps.psap_lport.value = '"         . mysqli_real_escape_string($db, $a_psaps['psap_lport'])         . "';\n";
+      print "document.psaps.psap_circuit_id.value = '"    . mysqli_real_escape_string($db, $a_psaps['psap_circuit_id'])    . "';\n";
+      print "document.psaps.psap_pseudo_cid.value = '"    . mysqli_real_escape_string($db, $a_psaps['psap_pseudo_cid'])    . "';\n";
+      print "document.psaps.psap_lec.value = '"           . mysqli_real_escape_string($db, $a_psaps['psap_lec'])           . "';\n";
+      print "document.psaps.psap_texas.value = '"         . mysqli_real_escape_string($db, $a_psaps['psap_texas'])         . "';\n";
+      print "document.psaps.psap_updated.value = '"       . mysqli_real_escape_string($db, $a_psaps['psap_updated'])       . "';\n";
+      print "document.psaps.psap_delete.value = '"        . mysqli_real_escape_string($db, $a_psaps['psap_delete'])        . "';\n";
 
       print "document.psaps.id.value = " . $formVars['id'] . ";\n";
 
