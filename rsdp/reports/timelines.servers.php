0656cd4b6f0d738d07a729bf4bf9042460688ead
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 19:39:45 -0700

    Updated to fix functions

--- rsdp/reports/timelines.servers.php
+++ rsdp/reports/timelines.servers.php
@@ -143,7 +143,7 @@
 
       $output .= "</table>\n";
 
-      print "document.getElementById('server_mysql').innerHTML = '" . mysqli_real_escape_string($output) . "';\n";
+      print "document.getElementById('server_mysql').innerHTML = '" . mysqli_real_escape_string($db, $output) . "';\n";
 
     } else {
       logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
