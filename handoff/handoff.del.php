bcd9b13a75eaf7457175dc5a06c8bbdc62ea6334
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 17:25:11 -0700

    Updated to remove company info

--- handoff/handoff.del.php
+++ handoff/handoff.del.php
@@ -2,7 +2,6 @@
 # Script: handoff.del.php
 # Owner: Carl Schelin
 # Coding Standard 3.0 Applied
-# See: https://incowk01/makers/index.php/Coding_Standards
 # Description: 
 
   header('Content-Type: text/javascript');
