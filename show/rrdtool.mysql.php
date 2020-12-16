103a01cf5c146fe739c4114b4bcd0c9869f1b17a
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 20:13:52 -0700

    Updated to fix functions

--- show/rrdtool.mysql.php
+++ show/rrdtool.mysql.php
@@ -142,5 +142,5 @@
   }
 ?>
 
-document.getElementById('performance_mysql').innerHTML = '<?php print mysqli_real_escape_string($output); ?>';
+document.getElementById('performance_mysql').innerHTML = '<?php print mysqli_real_escape_string($db, $output); ?>';
 
