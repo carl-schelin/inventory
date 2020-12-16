1175c84e14b320bdcb4c578032b2fff59375f14b
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-15 18:40:39 -0700

    Updated to add database connections

--- rsdp/build/initial.dup.php
+++ rsdp/build/initial.dup.php
@@ -11,7 +11,11 @@
   include($Loginpath . '/check.php');
   include($Sitepath . '/function.php');
   include($RSDPpath . '/function.php');
-  check_login('2');
+
+# connect to the database
+  $db = db_connect($DBserver, $DBname, $DBuser, $DBpassword);
+
+  check_login($db, $AL_Edit);
 
   $package = "initial.dup.php";
 
