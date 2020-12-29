4c992e44ecebb982d2ed6f7887ca6d3139cae2ba
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-29 15:02:42 -0700

    renamed groups

--- accounts/groups.del.php
+++ accounts/groups.del.php
@@ -19,10 +19,10 @@
     }
 
     if (check_userlevel($db, $AL_Admin)) {
-      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from groups");
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from a_groups");
 
       $q_string  = "delete ";
-      $q_string .= "from groups ";
+      $q_string .= "from a_groups ";
       $q_string .= "where grp_id = " . $formVars['id'];
       $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
 
