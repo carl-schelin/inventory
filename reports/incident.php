20cf1c51c3aed6bce3b191d0bbeb62fbc72a4623
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-29 14:56:31 -0700

    renamed group

--- reports/incident.php
+++ reports/incident.php
@@ -225,7 +225,7 @@
             . "left join locations on locations.loc_id      = inventory.inv_location "
             . "left join cities    on cities.ct_id          = locations.loc_city "
             . "left join states    on states.st_id          = locations.loc_state "
-            . "left join groups    on groups.grp_id         = inventory.inv_manager "
+            . "left join a_groups    on a_groups.grp_id         = inventory.inv_manager "
             . $where . " " 
             . $orderby;
   $q_inventory = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
