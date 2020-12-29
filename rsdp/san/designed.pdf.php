6bc925dd2ff340a43f33c7b7f424f3e881622e8b
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-29 15:42:09 -0700

    renamed groups

--- rsdp/san/designed.pdf.php
+++ rsdp/san/designed.pdf.php
@@ -33,7 +33,7 @@
   $q_string .= "left join users         on users.usr_id         = rsdp_server.rsdp_requestor ";
   $q_string .= "left join department    on department.dep_id    = users.usr_deptname ";
   $q_string .= "left join business_unit on business_unit.bus_id = department.dep_unit ";
-  $q_string .= "left join groups        on groups.grp_id        = rsdp_server.rsdp_application ";
+  $q_string .= "left join a_groups        on a_groups.grp_id        = rsdp_server.rsdp_application ";
   $q_string .= "left join locations     on locations.loc_id     = rsdp_server.rsdp_location ";
   $q_string .= "left join projects      on projects.prj_id      = rsdp_server.rsdp_project ";
   $q_string .= "where rsdp_id = " . $formVars['rsdp'];
