83b50369822fe19a6112d23a24b4c46cc198bdd7
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-29 14:53:12 -0700

    renamed groups

--- reports/compiled.class.php
+++ reports/compiled.class.php
@@ -201,7 +201,7 @@ $(document).ready( function() {
   $q_string .= "from inventory ";
   $q_string .= "left join software  on inventory.inv_id = software.sw_companyid ";
   $q_string .= "left join hardware  on inventory.inv_id = hardware.hw_companyid ";
-  $q_string .= "left join groups    on groups.grp_id    = hardware.hw_group ";
+  $q_string .= "left join a_groups    on a_groups.grp_id    = hardware.hw_group ";
   $q_string .= "left join models    on models.mod_id    = hardware.hw_vendorid ";
   $q_string .= "left join support   on support.sup_id   = hardware.hw_supportid ";
   $q_string .= "left join products  on products.prod_id = inventory.inv_product ";
@@ -281,7 +281,7 @@ $(document).ready( function() {
   $q_string .= "from inventory ";
   $q_string .= "left join software  on inventory.inv_id = software.sw_companyid ";
   $q_string .= "left join hardware  on inventory.inv_id = hardware.hw_companyid ";
-  $q_string .= "left join groups    on groups.grp_id    = hardware.hw_group ";
+  $q_string .= "left join a_groups    on a_groups.grp_id    = hardware.hw_group ";
   $q_string .= "left join models    on models.mod_id    = hardware.hw_vendorid ";
   $q_string .= "left join support   on support.sup_id   = hardware.hw_supportid ";
   $q_string .= "left join products  on products.prod_id = inventory.inv_product ";
