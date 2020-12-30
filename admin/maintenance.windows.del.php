94e26a9eec87618d79d4c83218dd907ad77c403e
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-30 07:56:39 -0700

    renamed window

--- admin/maintenance.windows.del.php
+++ admin/maintenance.windows.del.php
@@ -19,10 +19,10 @@
     }
 
     if (check_userlevel($db, $AL_Admin)) {
-      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from window");
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from maint_window");
 
       $q_string  = "delete ";
-      $q_string .= "from window ";
+      $q_string .= "from maint_window ";
       $q_string .= "where win_id = " . $formVars['id'];
       $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
 
