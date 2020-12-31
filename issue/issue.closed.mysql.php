578c030863a9d044dce327b7c152a7dc0b06e7a0
Carl Schelin <t.carl.schelin@gmail.com>
2020-12-31 12:41:15 -0700

    changed 0000-00-00 with 1971-01-01

--- issue/issue.closed.mysql.php
+++ issue/issue.closed.mysql.php
@@ -93,7 +93,7 @@
   $q_string .= "left join inventory on issue.iss_companyid = inventory.inv_id ";
   $q_string .= "left join users on users.usr_id = issue.iss_user ";
   $q_string .= $leftjoin;
-  $q_string .= "where iss_closed != '0000-00-00' " . $where;
+  $q_string .= "where iss_closed != '1971-01-01' " . $where;
   $q_string .= "order by inv_name,iss_discovered desc";
   $q_issue = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
   while ($a_issue = mysqli_fetch_array($q_issue)) {
