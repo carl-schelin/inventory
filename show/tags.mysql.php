58cfa82999b7d2805a1a3332836cf5ed5267405e
Carl Schelin <t.carl.schelin@gmail.com>
2021-03-10 17:43:45 -0700

    updated to show only server tags

--- show/tags.mysql.php
+++ show/tags.mysql.php
@@ -65,7 +65,7 @@
 
   $q_string  = "select tag_name ";
   $q_string .= "from tags ";
-  $q_string .= "where tag_companyid = " . $formVars['id'] . " and tag_view = 0 and tag_owner = " . $formVars['uid'] . " ";
+  $q_string .= "where tag_companyid = " . $formVars['id'] . " and tag_view = 0 and tag_owner = " . $formVars['uid'] . " and tag_type = 1 ";
   $q_string .= "group by tag_name ";
   $q_tags = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
   while ($a_tags = mysqli_fetch_array($q_tags)) {
@@ -87,7 +87,7 @@
 
   $q_string  = "select tag_name ";
   $q_string .= "from tags ";
-  $q_string .= "where tag_companyid = " . $formVars['id'] . " and tag_view = 1 and tag_group = " . $formVars['group'] . " ";
+  $q_string .= "where tag_companyid = " . $formVars['id'] . " and tag_view = 1 and tag_group = " . $formVars['group'] . " and tag_type = 1 ";
   $q_string .= "group by tag_name ";
   $q_tags = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
   while ($a_tags = mysqli_fetch_array($q_tags)) {
@@ -109,7 +109,7 @@
 
   $q_string  = "select tag_name ";
   $q_string .= "from tags ";
-  $q_string .= "where tag_companyid = " . $formVars['id'] . " and tag_view = 2 ";
+  $q_string .= "where tag_companyid = " . $formVars['id'] . " and tag_view = 2 and tag_type = 1 ";
   $q_string .= "group by tag_name ";
   $q_tags = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
   while ($a_tags = mysqli_fetch_array($q_tags)) {
