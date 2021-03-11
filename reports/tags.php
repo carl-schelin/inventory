ce47f740476ec88e4be1935bd44eb19a4a82536e
Carl Schelin <t.carl.schelin@gmail.com>
2021-03-10 17:45:48 -0700

    updated to show only server tags

--- reports/tags.php
+++ reports/tags.php
@@ -88,7 +88,7 @@
   $q_string .= "left join inventory on inventory.inv_id = tags.tag_companyid ";
   $q_string .= "left join users     on users.usr_id     = tags.tag_owner ";
   $q_string .= "left join a_groups    on a_groups.grp_id    = tags.tag_group ";
-  $q_string .= "where inv_status = 0 ";
+  $q_string .= "where inv_status = 0 and tag_type = 1 ";
   $q_string .= $orderby;
   $q_tags = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
   while ($a_tags = mysqli_fetch_array($q_tags)) {
