8a0c91df6cbf46a2c8205bc2ef321465ba82c422
Carl Schelin <t.carl.schelin@gmail.com>
2021-04-10 15:08:14 -0600

    removed changelog

--- reports/lastlogin.php
+++ reports/lastlogin.php
@@ -54,8 +54,8 @@
 
 <div class="main-help ui-widget-content">
 
-<p>This provides a list of all installed products. Clicking on the link brings up a document with three 
-tabs displaying all the hardware associated with the product, the installed software, and all changelog entries.</p>
+<p>This provides a list of all installed products. Clicking on the link brings up a document with two 
+tabs displaying all the hardware associated with the product and the software.</p>
 
 </div>
 
