1d3a2e84aad52e650ccbfc4bf6c150d0fdfddd26
Carl Schelin <t.carl.schelin@gmail.com>
2021-06-01 08:22:45 -0600

    renamed zones to timezones

--- reports/eolreport.php
+++ reports/eolreport.php
@@ -295,7 +295,7 @@
   $q_string .= "left join hardware  on hardware.hw_companyid = inventory.inv_id ";
   $q_string .= "left join locations on locations.loc_id      = inventory.inv_location ";
   $q_string .= "left join cities    on cities.ct_id          = locations.loc_city ";
-  $q_string .= "left join zones     on zones.zone_id         = inventory.inv_zone ";
+  $q_string .= "left join timezones on timezones.zone_id     = inventory.inv_zone ";
   $q_string .= "left join models    on models.mod_id         = hardware.hw_vendorid ";
   $q_string .= "left join a_groups    on a_groups.grp_id         = inventory.inv_manager ";
   $q_string .= $group . $product . $inwork . $location . $type . " ";
