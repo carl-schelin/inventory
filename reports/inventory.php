15d53213ecc9131e143db9bdb4fcb2399c6e03c0
Carl Schelin <t.carl.schelin@gmail.com>
2021-06-01 08:23:20 -0600

    renamed zones to timezones

--- reports/inventory.php
+++ reports/inventory.php
@@ -427,9 +427,9 @@ $(document).ready( function () {
   $q_string .= "left join hardware  on hardware.hw_companyid = inventory.inv_id ";
   $q_string .= "left join locations on locations.loc_id      = inventory.inv_location ";
   $q_string .= "left join cities    on cities.ct_id          = locations.loc_city ";
-  $q_string .= "left join zones     on zones.zone_id         = inventory.inv_zone ";
+  $q_string .= "left join timezones on timezones.zone_id     = inventory.inv_zone ";
   $q_string .= "left join models    on models.mod_id         = hardware.hw_vendorid ";
-  $q_string .= "left join a_groups    on a_groups.grp_id         = inventory.inv_manager ";
+  $q_string .= "left join a_groups  on a_groups.grp_id       = inventory.inv_manager ";
   $q_string .= $product . $inwork . $location . $type . " ";
   $q_string .= $orderby;
   $q_inventory = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
