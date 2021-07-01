577266cd4636125fa8983dee9821d60f2a6bd31c
Carl Schelin <t.carl.schelin@gmail.com>
2021-06-30 18:39:23 -0600

    updated to only have app, mgt, backup, or external vip IPs listed in the report

--- reports/inventory.php
+++ reports/inventory.php
@@ -454,7 +454,7 @@ $(document).ready( function () {
       $q_string .= "from interface ";
       $q_string .= "left join int_types on int_types.itp_id = interface.int_type ";
       $q_string .= "where int_companyid = \"" . $a_inventory['inv_id'] . "\" and int_type != 7 and int_addr != '' and int_ip6 = 0 ";
-      $q_string .= "order by int_face";
+      $q_string .= "order by int_acronym,int_face";
       $q_interface = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
       while ($a_interface = mysqli_fetch_array($q_interface)) {
 
@@ -471,7 +471,10 @@ $(document).ready( function () {
             $console .= $a_interface['int_face'] . "=" . $a_interface['int_addr'] . $primary . " ";
           }
         } else {
-          $interface .= $a_interface['itp_acronym'] . "=" . $a_interface['int_addr'] . $primary . " ";
+# only want to see App, Mgt, EVIP, or Bkp  interfaces.
+          if ($a_interface['int_type == 1 || $a_interface['int_type'] == 2 || $a_interface['int_type'] == 12 || $a_interface['int_type'] == 16) {
+            $interface .= $a_interface['itp_acronym'] . "=" . $a_interface['int_addr'] . $primary . " ";
+          }
         }
       }
 
