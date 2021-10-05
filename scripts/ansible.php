8a67e8717db4382b1d1d5963cd651929f81cf365
Carl Schelin <t.carl.schelin@gmail.com>
2021-10-04 19:22:04 -0600

    Removed unnecessary tags and moved to tags as saved by the type of product.

--- scripts/ansible.php
+++ scripts/ansible.php
@@ -26,74 +26,102 @@
     $manager = $argv[1];
   }
 
-# by products
-  $q_string  = "select prod_id,prod_name ";
-  $q_string .= "from products ";
-  $q_string .= "order by prod_name ";
-  $q_product = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
-  while ($a_product = mysqli_fetch_array($q_product)) {
-
-    $q_string  = "select int_server ";
-    $q_string .= "from inventory ";
-    $q_string .= "left join interface on interface.int_companyid = inventory.inv_id ";
-    $q_string .= "where inv_manager = " . $manager . " and inv_status = 0 and inv_ssh = 1 and inv_product = " . $a_product['prod_id'] . " and inv_ansible = 1 and int_management = 1 ";
-    $q_string .= "order by inv_name ";
-    $q_inventory = mysqli_query($db, $q_string) or die(mysqli_error($db));
-    if (mysqli_num_rows($q_inventory) > 0) {
-      print "[" . str_replace(" ", "_", str_replace("/", "_", $a_product['prod_name'])) . "]\n";
-
-      while ($a_inventory = mysqli_fetch_array($q_inventory)) {
-        print $a_inventory['int_server'] . "\n";
-      }
-      print "\n";
-    }
-  }
-
-# find tags with 0 which apply to all servers
-# ignore any server with a '-[tag]'
-# 
-
-
 # by tags
   $q_string  = "select tag_name ";
   $q_string .= "from tags ";
-  $q_string .= "where tag_group = " . $manager . " ";
+  $q_string .= "where tag_group = " . $manager . " and tag_type = 1 ";
   $q_string .= "group by tag_name ";
+  $q_string .= "order by tag_name ";
   $q_tags = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
-  while ($a_tags = mysqli_fetch_array($q_tags)) {
+  if (mysqli_num_rows($q_tags) > 0) {
+    while ($a_tags = mysqli_fetch_array($q_tags)) {
 
-    $q_string  = "select int_server ";
-    $q_string .= "from inventory ";
-    $q_string .= "left join tags on tags.tag_companyid = inventory.inv_id ";
-    $q_string .= "left join interface on interface.int_companyid = inventory.inv_id ";
-    $q_string .= "where inv_status = 0 and inv_ssh = 1 and tag_name = \"" . $a_tags['tag_name'] . "\" and inv_ansible = 1 and int_management = 1 ";
-    $q_string .= "order by inv_name ";
-    $q_inventory = mysqli_query($db, $q_string) or die(mysqli_error($db));
-    if (mysqli_num_rows($q_inventory) > 0) {
-      print "[" . str_replace(" ", "_", str_replace("/", "_", $a_tags['tag_name'])) . "]\n";
+      $q_string  = "select int_server ";
+      $q_string .= "from inventory ";
+      $q_string .= "left join tags on tags.tag_companyid = inventory.inv_id ";
+      $q_string .= "left join interface on interface.int_companyid = inventory.inv_id ";
+      $q_string .= "where inv_status = 0 and inv_ssh = 1 and tag_name = \"" . $a_tags['tag_name'] . "\" and inv_ansible = 1 and int_management = 1 ";
+      $q_string .= "order by inv_name ";
+      $q_inventory = mysqli_query($db, $q_string) or die(mysqli_error($db));
+      if (mysqli_num_rows($q_inventory) > 0) {
+        print "[" . str_replace(" ", "_", str_replace("/", "_", $a_tags['tag_name'])) . "]\n";
 
-      while ($a_inventory = mysqli_fetch_array($q_inventory)) {
-        print $a_inventory['int_server'] . "\n";
+        while ($a_inventory = mysqli_fetch_array($q_inventory)) {
+          print $a_inventory['int_server'] . "\n";
+        }
+        print "\n";
       }
-      print "\n";
     }
   }
 
-# nagios
-  $q_string  = "select int_server ";
-  $q_string .= "from inventory ";
-  $q_string .= "left join interface on interface.int_companyid = inventory.inv_id ";
-  $q_string .= "where inv_status = 0 and inv_ssh = 1 and int_nagios = 1 and inv_ansible = 1 and int_management = 1 ";
-  $q_string .= "group by int_server ";
-  $q_inventory = mysqli_query($db, $q_string) or die(mysqli_error($db));
-  if (mysqli_num_rows($q_inventory) > 0) {
-    print "[nagios]\n";
 
-    while ($a_inventory = mysqli_fetch_array($q_inventory)) {
-      print $a_inventory['int_server'] . "\n";
+# by location tags. type == 2
+# loop through tags where type == 2, get the loc_id and list any server that is at that location.
+
+  $q_string  = "select tag_name,tag_companyid ";
+  $q_string .= "from tags ";
+  $q_string .= "where tag_group = " . $manager . " and tag_type = 2 ";
+  $q_string .= "group by tag_name ";
+  $q_string .= "order by tag_name ";
+  $q_tags = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  if (mysqli_num_rows($q_tags) > 0) {
+    while ($a_tags = mysqli_fetch_array($q_tags)) {
+
+      $q_string  = "select int_server ";
+      $q_string .= "from inventory ";
+      $q_string .= "left join interface on interface.int_companyid = inventory.inv_id ";
+      $q_string .= "where inv_status = 0 and inv_ssh = 1 and inv_location = " . $a_tags['tag_companyid'] . " and inv_ansible = 1 and int_management = 1 ";
+      $q_string .= "order by inv_name ";
+      $q_inventory = mysqli_query($db, $q_string) or die(mysqli_error($db));
+      if (mysqli_num_rows($q_inventory) > 0) {
+        print "[" . str_replace(" ", "_", str_replace("/", "_", $a_tags['tag_name'])) . "]\n";
+        while ($a_inventory = mysqli_fetch_array($q_inventory)) {
+          print $a_inventory['int_server'] . "\n";
+        }
+        print "\n";
+      }
+
     }
-    print "\n";
   }
+
+
+# by product tags, type == 3
+
+  $q_string  = "select tag_name,tag_companyid ";
+  $q_string .= "from tags ";
+  $q_string .= "where tag_group = " . $manager . " and tag_type = 3 ";
+  $q_string .= "group by tag_name ";
+  $q_string .= "order by tag_name ";
+  $q_tags = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+  if (mysqli_num_rows($q_tags) > 0) {
+    while ($a_tags = mysqli_fetch_array($q_tags)) {
+
+      $q_string  = "select int_server ";
+      $q_string .= "from inventory ";
+      $q_string .= "left join interface on interface.int_companyid = inventory.inv_id ";
+      $q_string .= "where inv_status = 0 and inv_ssh = 1 and inv_product = " . $a_tags['tag_companyid'] . " and inv_ansible = 1 and int_management = 1 ";
+      $q_string .= "order by inv_name ";
+      $q_inventory = mysqli_query($db, $q_string) or die(mysqli_error($db));
+      if (mysqli_num_rows($q_inventory) > 0) {
+        print "[" . str_replace(" ", "_", str_replace("/", "_", $a_tags['tag_name'])) . "]\n";
+        while ($a_inventory = mysqli_fetch_array($q_inventory)) {
+          print $a_inventory['int_server'] . "\n";
+        }
+        print "\n";
+      }
+
+    }
+  }
+
+
+# by software tags
+
+
+# by hardare tags
+
+
+
+
 
   mysqli_close($db);
 
