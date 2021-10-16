b4b009bb592333769cbd9462c4da7551a4518d23
Carl Schelin <t.carl.schelin@gmail.com>
2021-10-16 06:20:48 -0600

    updated to rename everything to just Business

--- accounts/business.php
+++ accounts/business.php
@@ -30,7 +30,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Business Units Editor</title>
+<title>Business Editor</title>
 
 <style type="text/css" title="currentStyle" media="screen">
 <?php include($Sitepath . "/mobile.php"); ?>
@@ -46,7 +46,7 @@
   if (check_userlevel($db, $AL_Admin)) {
 ?>
 function delete_line( p_script_url ) {
-  var answer = confirm("Delete this Business Unit?")
+  var answer = confirm("Delete this Business?")
 
   if (answer) {
     script = document.createElement('script');
@@ -119,7 +119,7 @@ $(document).ready( function() {
         }
       },
       {
-        text: "Add Business Unit",
+        text: "Add Business",
         click: function() {
           attach_file('business.mysql.php', 0);
           $( this ).dialog( "close" );
@@ -149,14 +149,14 @@ $(document).ready( function() {
         }
       },
       {
-        text: "Update Business Unit",
+        text: "Update Business",
         click: function() {
           update_file('business.mysql.php', 1);
           $( this ).dialog( "close" );
         }
       },
       {
-        text: "Add Business Unit",
+        text: "Add Business",
         click: function() {
           update_file('business.mysql.php', 0);
           $( this ).dialog( "close" );
@@ -178,16 +178,16 @@ $(document).ready( function() {
 
 <table class="ui-styled-table">
 <tr>
-  <th class="ui-state-default">Business Unit Editor</th>
-  <th class="ui-state-default" width="20"><a href="javascript:;" onmousedown="toggleDiv('business-unit-help');">Help</a></th>
+  <th class="ui-state-default">Business Editor</th>
+  <th class="ui-state-default" width="20"><a href="javascript:;" onmousedown="toggleDiv('business-help');">Help</a></th>
 </tr>
 </table>
 
-<div id="business-unit-help" style="<?php print $display; ?>">
+<div id="business-help" style="<?php print $display; ?>">
 
 <div class="main-help ui-widget-content">
 
-<p>A <strong>business unit</strong> is an entity in an organization that has a specialized function. It develops its own 
+<p>A <strong>Business</strong> is an entity in an Organization that has a specialized function. It develops its own 
 strategy within the organization that aligns with company objectives.</p>
 
 </div>
@@ -196,7 +196,7 @@ strategy within the organization that aligns with company objectives.</p>
 
 <table class="ui-styled-table">
 <tr>
-  <td class="ui-widget-content button"><input type="button" id="clickCreate" value="Add Business Unit"></td>
+  <td class="ui-widget-content button"><input type="button" id="clickCreate" value="Add Business"></td>
 </tr>
 </table>
 
@@ -204,25 +204,25 @@ strategy within the organization that aligns with company objectives.</p>
 
 <table class="ui-styled-table">
 <tr>
-  <th class="ui-state-default">Business Unit Listing</th>
-  <th class="ui-state-default" width="20"><a href="javascript:;" onmousedown="toggleDiv('business-unit-listing-help');">Help</a></th>
+  <th class="ui-state-default">Business Listing</th>
+  <th class="ui-state-default" width="20"><a href="javascript:;" onmousedown="toggleDiv('business-listing-help');">Help</a></th>
 </tr>
 </table>
 
-<div id="business-unit-listing-help" style="<?php print $display; ?>">
+<div id="business-listing-help" style="<?php print $display; ?>">
 
 <div class="main-help ui-widget-content">
 
 
-<p><strong>Business Unit Listing</strong></p>
+<p><strong>Business Listing</strong></p>
 
-<p>This page lists all the currently defined <strong>Business Units</strong> within this organization.</p>
+<p>This page lists all the currently defined <strong>Business</strong> within this organization.</p>
 
-<p>To add a new Business Unit, click the <strong>Add Business Unit</strong> button. This will bring up a dialog box which you can then 
-use to create a new business unit.</p>
+<p>To add a new Business, click the <strong>Add Business</strong> button. This will bring up a dialog box which you can then 
+use to create a new Business.</p>
 
-<p>To editing an existing business unit, click on an entry in the listing. A dialog box will be displayed where you can edit the current 
-business unit, or if there's a small difference, you can make changes and add a new business unit.</p>
+<p>To editing an existing Business, click on an entry in the listing. A dialog box will be displayed where you can edit the current 
+Business, or if there's a small difference, you can make changes and add a new Business.</p>
 
 </div>
 
@@ -234,16 +234,16 @@ business unit, or if there's a small difference, you can make changes and add a 
 </div>
 
 
-<div id="dialogCreate" title="Add Business Unit">
+<div id="dialogCreate" title="Add Business">
 
 <form name="formCreate">
 
 <table class="ui-styled-table">
 <tr>
-  <td class="ui-widget-content">Business Unit Name: <input type="text" name="bus_name" size="40"></td>
+  <td class="ui-widget-content">Business Name: <input type="text" name="bus_name" size="40"></td>
 </tr>
 <tr>
-  <td class="ui-widget-content">Business Unit ID: <input type="number" name="bus_unit" size="10"></td>
+  <td class="ui-widget-content">Business ID: <input type="number" name="bus_unit" size="10"></td>
 </tr>
 <tr>
   <td class="ui-widget-content">Organization: <select name="bus_org">
@@ -267,7 +267,7 @@ business unit, or if there's a small difference, you can make changes and add a 
 </div>
 
 
-<div id="dialogUpdate" title="Edit Business Unit">
+<div id="dialogUpdate" title="Edit Business">
 
 <form name="formUpdate">
 
@@ -275,10 +275,10 @@ business unit, or if there's a small difference, you can make changes and add a 
 
 <table class="ui-styled-table">
 <tr>
-  <td class="ui-widget-content">Business Unit Name: <input type="text" name="bus_name" size="40"></td>
+  <td class="ui-widget-content">Business Name: <input type="text" name="bus_name" size="40"></td>
 </tr>
 <tr>
-  <td class="ui-widget-content">Business Unit ID: <input type="number" name="bus_unit" size="10"></td>
+  <td class="ui-widget-content">Business ID: <input type="number" name="bus_unit" size="10"></td>
 </tr>
 <tr>
   <td class="ui-widget-content">Organization: <select name="bus_org">
