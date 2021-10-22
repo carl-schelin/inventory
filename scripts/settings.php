eee7b1146b738ef74186ee55b2a89c5ea3f58592
Carl Schelin <t.carl.schelin@gmail.com>
2021-10-21 18:13:26 -0600

    updated to change permissions to 10644

--- scripts/settings.php
+++ scripts/settings.php
@@ -1,38 +1,102 @@
 <?php
 
 # add a space at the end as the company will be inserted into strings.
-$Sitecompany		= 'Company ';
+$Sitecompany		= 'Hobgoblin Consulting Services, LLC ';
+$Companyemail           = 'internal.pri';
+
 # by default, enable debugging in case we missed a server entry. ALL means full on screen debugging
 $Sitedebug		= 'ALL';
 
 # Set the environment here so other places in the code can be tested without changing code.
 $hostname = php_uname('n');
 
+# inventory is the new home but it's a VirtualHost so hostname is lnmt1cuomtool10. This "fixes" this.
+#if ($hostname == 'lnmt1cuomtool11.internal.pri') {
+#  $hostname = "inventory.internal.pri";
+#}
 
 #############################################################
-# Server Descriptions
+# Development servers
 #############################################################
 
-if ($hostname == "[hostname]") {
-  $Siteenv		= "DEV";
-  $Sitedebug		= "NO";
-  $Sitedebug		= "YES";
+if ($hostname == "bldr0cuomdev1.internal.pri") {
+  $Siteenv              = "DEV";
+  $Sitedebug		= "NO"; # no error logging
+  $Sitedebug		= "ALL"; # log errors to file _and_ to the screen
+  $Sitedebug		= "YES"; # log errors just to a file
 
 # Set site specific variables
-  $Sitehttp		= "[hostname]";
-  $Siteurl		= "http://" . $Sitehttp;
-  $Statushttp		= "status";
+  $Sitehttp             = "bldr0cuomdev1.internal.pri";
+  $Siteurl              = "http://" . $Sitehttp;
+  $Statushttp		= "bldr0cuomdev1.internal.pri";
   $Statusurl		= "http://" . $Statushttp;
-  $Nagioshttp		= "status";
+  $Nagioshttp		= "lnmt1cuomjs1.internal.pri";
   $Nagiosurl		= "http://" . $Nagioshttp;
-  $Wikihttp		= "wiki";
+  $Wikihttp		= "lnmt1cuomwk1.internal.pri";
   $Wikiurl		= "http://" . $Wikihttp;
+
+# Changelog location (home directories)
+  $Changehome           = "/home";
+
+# Header graphic
+  $Siteheader           = "devtitlegraphic.gif";
+
+# Path details
+  $Sitedir              = "/var/www/html";
+  $Siteinstall          = "/inventory";
+  $Statusdir		= "/var/www/html";
+  $Statusinstall	= "/status";
+  $Nagiosdir		= "/usr/local/httpd/htsecure";
+  $Nagiosinstall	= "/nagios";
+  $Wikidir		= "/usr/local/httpd/htsecure";
+  $Wikiinstall		= "/makers";
+
+# Who to contact
+  $Siteadmins           = ",cschelin@internal.pri";
+  $Sitedev              = "cschelin@internal.pri";
+  $EmergencyContact     = "cschelin@internal.pri";
+
+# MySQL specific settings
+  $DBtype               = "mysql";
+  $DBserver             = "localhost";
+  $DBname               = "inventory";
+  $DBuser               = "invuser";
+  $DBpassword           = "this4now!!";
+  $DBprefix             = "";
+}
+
+#############################################################
+
+#############################################################
+# QA servers
+#############################################################
+
+#############################################################
+# Production servers
+#############################################################
+
+#############################################################
+
+if ($hostname == 'inventory.internal.pri') {
+  $Siteenv		= "PROD";
+  $Sitedebug		= "YES";
+  $Sitedebug		= "NO";
+
+# Set site specific variables
+  $Sitehttp		= "inventory.internal.pri";
+  $Siteurl		= "https://" . $Sitehttp;
+  $Statushttp		= "status.internal.pri";
+  $Statusurl		= "https://" . $Statushttp;
+  $Nagioshttp		= "status.internal.pri";
+  $Nagiosurl		= "http://" . $Nagioshttp;
+  $Wikihttp		= "lnmt1cuomwiki1.internal.pri";
+  $Wikiurl		= "https://" . $Wikihttp;
 
 # Changelog location (home directories)
   $Changehome		= "/home";
 
 # Header graphic
-  $Siteheader		= "devtitlegraphic.gif";
+  $Siteheader		= "intrado-logo.png";
 
 # Path details
   $Sitedir		= "/var/www/html";
@@ -45,16 +109,63 @@ if ($hostname == "[hostname]") {
   $Wikiinstall		= "/makers";
 
 # Who to contact
-  $Siteadmins		= ",[your email]";
-  $Sitedev		= "[your email]";
-  $EmergencyContact	= "[your email]";
+  $Siteadmins		= ",cschelin@internal.pri";
+  $Sitedev		= "cschelin@internal.pri";
+  $EmergencyContact	= "cschelin@internal.pri";
 
 # MySQL specific settings
   $DBtype		= "mysql";
   $DBserver		= "localhost";
   $DBname		= "inventory";
-  $DBuser		= "[inventory username]";
-  $DBpassword		= "[password]";
+  $DBuser		= "invuser";
+  $DBpassword		= "this4now!!";
+  $DBprefix		= "";
+}
+
+#############################################################
+
+if ($hostname == 'lnmt1cuomtool11.internal.pri') {
+  $Siteenv		= "PROD";
+  $Sitedebug		= "YES";
+  $Sitedebug		= "NO";
+
+# Set site specific variables
+  $Sitehttp		= "lnmt1cuomtool11.internal.pri";
+  $Siteurl		= "https://" . $Sitehttp;
+  $Statushttp		= "status.internal.pri";
+  $Statusurl		= "https://" . $Statushttp;
+  $Nagioshttp		= "status.internal.pri";
+  $Nagiosurl		= "http://" . $Nagioshttp;
+  $Wikihttp		= "lnmt1cuomwiki1.internal.pri";
+  $Wikiurl		= "https://" . $Wikihttp;
+
+# Changelog location (home directories)
+  $Changehome		= "/home";
+
+# Header graphic
+  $Siteheader		= "intrado-logo.png";
+
+# Path details
+  $Sitedir		= "/var/www/html";
+  $Siteinstall		= "/inventory";
+  $Statusdir		= "/var/www/html";
+  $Statusinstall	= "/status";
+  $Nagiosdir		= "/usr/local/httpd/htsecure";
+  $Nagiosinstall	= "/nagios";
+  $Wikidir		= "/usr/local/httpd/htsecure";
+  $Wikiinstall		= "/makers";
+
+# Who to contact
+  $Siteadmins		= ",cschelin@internal.pri";
+  $Sitedev		= "cschelin@internal.pri";
+  $EmergencyContact	= "cschelin@internal.pri";
+
+# MySQL specific settings
+  $DBtype		= "mysql";
+  $DBserver		= "inventory.internal.pri";
+  $DBname		= "inventory";
+  $DBuser		= "invuser";
+  $DBpassword		= "this4now!!";
   $DBprefix		= "";
 }
 
@@ -96,12 +207,48 @@ $Nagiosroot		= $Nagiosurl . $Nagiosinstall;
 $Wikipath		= $Wikidir . $Wikiinstall;
 $Wikiroot		= $Wikiurl . $Wikiinstall;
 
+
+#######
+## Service Class Definitions Location
+#######
+
+$Serviceclass		= "http://intradonet/sites/database/Shared%20Documents/Service_Class/Service_Class_Definition.doc";
+
+#######
+## Email address for specific functions
+#######
+
+# for the Magic ticketing system
+$Magicdev 		= "svc_MagicAdminDev@intrado.com";
+$Magicprod 		= "svc_magicprodemail@intrado.com";
+
+$Magicdevemail		= "svc_MagicAdminDev@intrado.com"; # Testing to Dev
+$Magicprodemail		= "svc_magicprodemail@intrado.com"; # Production
+
+# for the Remedy ticketing system
+$Remedydev8		= "remedy.helpdesk.dev@intrado.com";
+$Remedydevsvr8		= "LMV08-REMAPPQA.corp.intrado.pri";
+$Remedydev9		= "remedy.helpdesk.dev.safetyservices@regmail.west.com";
+$Remedydevsvr9		= "LNMT0CWASRMAP00";
+
+$Remedyprod8		= "remedy.helpdesk@intrado.com";
+$Remedyprodsvr8		= "LMV08-REMAR01.corp.intrado.pri";
+$Remedyprod9		= "Remedy91HelpdeskProd@intrado.com";
+$Remedyprodsvr9		= "LNMT1CWASRMAP01.corp.intrado.pri";
+
+$Remedyqa9		= "Remedy91HelpdeskQA@intrado.com";
+$Remedyqasvr9		= "lnmt0cwasrmap10.corp.intrado.pri";
+
 #######
 ##  Application and Utility specific locations
 ##  Sitepath is the prefix for OS level files such as include() or fopen()
 ##  Siteroot is the prefix for URL based files
 #######
 
+
+## Account Management path
+$Userspath		= $Sitepath . "/accounts";
+$Usersroot		= $Siteroot . "/accounts";
 
 ## Admin scripts (db modifiers)
 $Adminpath		= $Sitepath . "/admin";
@@ -110,10 +257,6 @@ $Adminroot		= $Siteroot . "/admin";
 ## Articles
 $Articlepath		= $Sitepath . "/articles";
 $Articleroot		= $Siteroot . "/articles";
-
-## Bulk Uploads
-$Assetpath		= $Sitepath . "/assets";
-$Assetroot		= $Siteroot . "/assets";
 
 ## Bug Tracking Manager scripts
 $Bugpath		= $Sitepath . "/bugs";
@@ -139,10 +282,6 @@ $FAQroot		= $Siteroot . "/faq";
 $Featurepath		= $Sitepath . "/features";
 $Featureroot		= $Siteroot . "/features";
 
-## Handoff scripts
-$Handoffpath		= $Sitepath . "/handoff";
-$Handoffroot		= $Siteroot . "/handoff";
-
 ## Hardware scripts
 $Hardwarepath		= $Sitepath . "/hardware";
 $Hardwareroot		= $Siteroot . "/hardware";
@@ -155,6 +294,14 @@ $Imgsroot		= $Siteroot . "/imgs";
 $Imagepath		= $Sitepath . "/image";
 $Imageroot		= $Siteroot . "/image";
 
+## Tabbed Inventory scripts
+$Invpath		= $Sitepath . "/inventory";
+$Invroot		= $Siteroot . "/inventory";
+
+## IPAM
+$IPAMpath		= $Sitepath . "/ipam";
+$IPAMroot		= $Siteroot . "/ipam";
+
 ## Issue Tracker scripts
 $Issuepath		= $Sitepath . "/issue";
 $Issueroot		= $Siteroot . "/issue";
@@ -163,61 +310,29 @@ $Issueroot		= $Siteroot . "/issue";
 $Licensepath		= $Sitepath . "/license";
 $Licenseroot		= $Siteroot . "/license";
 
-## Server Listing Manager scripts
-$Listingpath		= $Sitepath . "/listings";
-$Listingroot		= $Siteroot . "/listings";
-
 ## Login
 $Loginpath		= $Sitepath . "/login";
 $Loginroot		= $Siteroot . "/login";
 
-## Monitoring
-$Monitorpath		= $Sitepath . "/monitor";
-$Monitorroot		= $Siteroot . "/monitor";
+## Manage server errors
+$Managepath		= $Sitepath . "/manage";
+$Manageroot		= $Siteroot . "/manage";
 
-## Morning Report
-$Morningpath		= $Sitepath . "/morning";
-$Morningroot		= $Siteroot . "/morning";
+## Server Monitoring errors
+$Monitorpath		= $Sitepath . "/monitoring";
+$Monitorroot		= $Siteroot . "/monitoring";
 
 ## Pictures
 $Picturepath		= $Sitepath . "/pictures";
 $Pictureroot		= $Siteroot . "/pictures";
 
-## PSAPs
-$PSAPpath		= $Sitepath . "/psaps";
-$PSAProot		= $Siteroot . "/psaps";
-
-## Request path
-$Requestpath		= $Sitepath . "/requests";
-$Requestroot		= $Siteroot . "/requests";
-
 ## Report path
 $Reportpath		= $Sitepath . "/reports";
 $Reportroot		= $Siteroot . "/reports";
 
-## RSDP path
-$RSDPpath		= $Sitepath . "/rsdp";
-$RSDProot		= $Siteroot . "/rsdp";
-
-## Sanity Report path
-$Sanitypath		= $Sitepath . "/sanity";
-$Sanityroot		= $Siteroot . "/sanity";
-
-## Security path
-$Securitypath		= $Sitepath . "/security";
-$Securityroot		= $Siteroot . "/security";
-
 ## Show path
 $Showpath		= $Sitepath . "/show";
 $Showroot		= $Siteroot . "/show";
-
-## Upload directory
-$Uploadpath		= $Sitepath . "/uploads";
-$Uploadroot		= $Siteroot . "/uploads";
-
-## Account Management path
-$Userspath		= $Sitepath . "/accounts";
-$Usersroot		= $Siteroot . "/accounts";
 
 
 # disable access to the site and print a maintenance message
@@ -229,18 +344,6 @@ $called			= 'no';
 
 # Group settings. Hate to hard code numbers
 $GRP_Unix		= 1;
-$GRP_VoiceNetwork	= 2;
-$GRP_Virtualization	= 4;
-$GRP_Windows		= 5;
-$GRP_TSS		= 7;
-$GRP_DBAdmins		= 8;
-$GRP_SAN		= 9;
-$GRP_Backups		= 9;
-$GRP_Monitoring		= 10;
-$GRP_Networking		= 12;
-$GRP_TechOps		= 14;
-$GRP_Tandem		= 15;
-$GRP_Mobility		= 19;
 $GRP_WebApps		= 25;
 $GRP_ICLAdmins		= 26;
 $GRP_SCM		= 27;
@@ -250,8 +353,6 @@ $GRP_Shipping		= 40;
 $GRP_DataCenter		= 44;
 $GRP_ALIMAdmin		= 55;
 $GRP_IENV		= 87;
-$GRP_OpsEng		= 96;
-$GRP_i3			= 99;
 
 # Access levels
 $AL_Admin		= 1;
