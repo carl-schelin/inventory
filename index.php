a076fe5fa19cf3ccdec4910c623da55d7d160306
Carl Schelin <t.carl.schelin@gmail.com>
2021-10-29 13:05:53 -0600

    updated to fix tag cloud display

--- index.php
+++ index.php
@@ -665,19 +665,17 @@ selection of commonly selected Data Centers in the Data Center menu. By default 
   if (mysqli_num_rows($q_tags) > 0) {
     while ($a_tags = mysqli_fetch_array($q_tags)) {
 
-      if ($a_tags['tag_name'] != $tag_name) {
-        if ($tag_name != '') {
-          $q_string  = "select tag_id ";
-          $q_string .= "from tags ";
-          $q_string .= "where tag_type = 1 and tag_name = \"" . $a_tags['tag_name'] . "\" ";
-          $q_count = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
-          $tag_count = mysqli_num_rows($q_count);
+      if ($a_tags['tag_name'] != $tag_name || $tag_name == '') {
+        $q_string  = "select tag_id ";
+        $q_string .= "from tags ";
+        $q_string .= "where tag_type = 1 and tag_name = \"" . $a_tags['tag_name'] . "\" ";
+        $q_count = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+        $tag_count = mysqli_num_rows($q_count);
 
-          $linkstart = "<a href=\"javascript:;\" onClick=\"javascript:attach_tag('" . $Reportroot . "/tag.view.php?tag=" . $a_tags['tag_name'] . "&type=2');\">";
-          $linkend   = "</a>";
+        $linkstart = "<a href=\"javascript:;\" onClick=\"javascript:attach_tag('" . $Reportroot . "/tag.view.php?tag=" . $a_tags['tag_name'] . "&type=2');\">";
+        $linkend   = "</a>";
 
-          print $linkstart . $a_tags['tag_name'] . " (" . $tag_count . ")" . $linkend . "&nbsp;&nbsp;";
-        }
+        print $linkstart . $a_tags['tag_name'] . " (" . $tag_count . ")" . $linkend . "&nbsp;&nbsp;";
         $tag_name = $a_tags['tag_name'];
       }
     }
