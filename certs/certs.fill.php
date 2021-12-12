31c1a204f1e711081ad54c61f9cab765ce1f932f
Carl Schelin <t.carl.schelin@gmail.com>
2021-12-12 10:15:50 -0700

    updated to standards

--- certs/certs.fill.php
+++ certs/certs.fill.php
@@ -31,28 +31,28 @@
       $group = return_Index($db, $a_certs['cert_group'], "select grp_id from a_groups where grp_disabled = 0 order by grp_name");
       $cert  = return_Index($db, $a_certs['cert_ca'],    "select cert_id from certs where cert_isca = 1 order by cert_desc");
 
-      print "document.dialog.cert_desc.value = '"      . mysqli_real_escape_string($db, $a_certs['cert_desc'])      . "';\n";
-      print "document.dialog.cert_url.value = '"       . mysqli_real_escape_string($db, $a_certs['cert_url'])       . "';\n";
-      print "document.dialog.cert_expire.value = '"    . mysqli_real_escape_string($db, $a_certs['cert_expire'])    . "';\n";
-      print "document.dialog.cert_authority.value = '" . mysqli_real_escape_string($db, $a_certs['cert_authority']) . "';\n";
-      print "document.dialog.cert_memo.value = '"      . mysqli_real_escape_string($db, $a_certs['cert_memo'])      . "';\n";
+      print "document.formUpdate.cert_desc.value = '"      . mysqli_real_escape_string($db, $a_certs['cert_desc'])      . "';\n";
+      print "document.formUpdate.cert_url.value = '"       . mysqli_real_escape_string($db, $a_certs['cert_url'])       . "';\n";
+      print "document.formUpdate.cert_expire.value = '"    . mysqli_real_escape_string($db, $a_certs['cert_expire'])    . "';\n";
+      print "document.formUpdate.cert_authority.value = '" . mysqli_real_escape_string($db, $a_certs['cert_authority']) . "';\n";
+      print "document.formUpdate.cert_memo.value = '"      . mysqli_real_escape_string($db, $a_certs['cert_memo'])      . "';\n";
 
       if ($a_certs['cert_isca']) {
-        print "document.dialog.cert_isca.checked = true;\n";
+        print "document.formUpdate.cert_isca.checked = true;\n";
       } else {
-        print "document.dialog.cert_isca.checked = false;\n";
+        print "document.formUpdate.cert_isca.checked = false;\n";
       }
 
 # if your group matches the cert group for the item or if you're in webapps (group 25) or if the user is an admin
       if (check_grouplevel($db, $GRP_WebApps)) {
-        print "document.dialog.cert_group[" . ($group - 1) . "].selected = true;\n";
+        print "document.formUpdate.cert_group[" . ($group - 1) . "].selected = true;\n";
       } else {
-        print "document.dialog.cert_group.value = " . ($group - 1) . ";\n";
+        print "document.formUpdate.cert_group.value = " . ($group - 1) . ";\n";
       }
 
-      print "document.dialog.cert_ca[" . $cert . "].selected = true;\n";
+      print "document.formUpdate.cert_ca[" . $cert . "].selected = true;\n";
 
-      print "document.dialog.id.value = '" . $formVars['id'] . "'\n";
+      print "document.formUpdate.id.value = '" . $formVars['id'] . "'\n";
 
     } else {
       logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
