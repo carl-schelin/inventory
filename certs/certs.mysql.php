2b05fa53d6367d4b26a8f50ba0d40836405477a3
Carl Schelin <t.carl.schelin@gmail.com>
2021-12-12 10:15:59 -0700

    updated to standards

--- certs/certs.mysql.php
+++ certs/certs.mysql.php
@@ -55,18 +55,15 @@
 
           if ($formVars['update'] == 0) {
             $query = "insert into certs set cert_id = NULL, " . $q_string;
-            $message = "Certificate added.";
           }
           if ($formVars['update'] == 1) {
             $query = "update certs set " . $q_string . " where cert_id = " . $formVars['id'];
-            $message = "Certificate updated.";
           }
 
           logaccess($db, $_SESSION['uid'], $package, "Saving Changes to: " . $formVars['cert_desc']);
 
           mysqli_query($db, $query) or die($query . ": " . mysqli_error($db));
 
-          print "alert('" . $message . "');\n";
         } else {
           print "alert('You must input data before saving changes.');\n";
         }
@@ -75,62 +72,19 @@
 
       logaccess($db, $_SESSION['uid'], $package, "Creating the table for viewing.");
 
-      $q_string  = "select usr_notify ";
-      $q_string .= "from users ";
-      $q_string .= "where usr_id = " . $_SESSION['uid'];
-      $q_users = mysqli_query($db, $q_string) or die($q_string . ": " . $mysqli_error($db));
-      $a_users = mysqli_fetch_array($q_users);
-
-      if ($a_users['usr_notify'] == 0) {
-        $a_users['usr_notify'] = 90;
-      }
-
       $date = time();
 #      $date = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
       $warningdate = mktime(0, 0, 0, date('m'), date('d') + $a_users['usr_notify'], date('Y'));
 
-      $output  = "<p></p>";
-      $output .= "<table class=\"ui-styled-table\">";
-      $output .= "<tr>";
-      $output .= "  <th class=\"ui-state-default\">Certificate Listing</th>";
-      $output .= "  <th class=\"ui-state-default\" width=\"20\"><a href=\"javascript:;\" onmousedown=\"toggleDiv('cert-listing-help');\">Help</a></th>\n";
+      $output  = "<table class=\"ui-styled-table\">\n";
+      $output .= "<tr>\n";
+      $output .= "  <th class=\"ui-state-default\" width=\"160\">Delete Certificate</th>\n";
+      $output .= "  <th class=\"ui-state-default\">Description</th>\n";
+      $output .= "  <th class=\"ui-state-default\">CA?</th>\n";
+      $output .= "  <th class=\"ui-state-default\">Expiration</th>\n";
+      $output .= "  <th class=\"ui-state-default\">Authority</th>\n";
+      $output .= "  <th class=\"ui-state-default\">Managed By</th>\n";
       $output .= "</tr>\n";
-      $output .= "</table>\n";
-
-      $output .= "<div id=\"cert-listing-help\" style=\"display: none\">\n";
-
-      $output .= "<div class=\"main-help ui-widget-content\">\n";
-
-      $output .= "<ul>\n";
-      $output .= "  <li><strong>Certificate Listing</strong>\n";
-      $output .= "  <ul>\n";
-      $output .= "    <li><strong>Delete (x)</strong> - Click here to delete this Website Certificate.</li>\n";
-      $output .= "    <li><strong>Editing</strong> - Click on a Certificate to toggle the form and edit the certificate.</li>\n";
-      $output .= "    <li><strong>Highlight</strong> - If a certificate is <span class=\"ui-state-error\">highlighted</span>, then the certificate has expired.</li>\n";
-      $output .= "    <li><strong>Highlight</strong> - If a certificate is <span class=\"ui-state-highlight\">highlighted</span>, then the certificate will expire within the notification period you specified in your profile (default: 90 days, your setting: " . $a_users['usr_notify'] . " days).</li>\n";
-      $output .= "  </ul></li>\n";
-      $output .= "</ul>\n";
-
-      $output .= "<ul>\n";
-      $output .= "  <li><strong>Notes</strong>\n";
-      $output .= "  <ul>\n";
-      $output .= "    <li>Click the <strong>Certificate Management</strong> title bar to toggle the <strong>Certificate Form</strong>.</li>\n";
-      $output .= "  </ul></li>\n";
-      $output .= "</ul>\n";
-
-      $output .= "</div>\n";
-
-      $output .= "</div>\n";
-
-      $output .= "<table class=\"ui-styled-table\">";
-      $output .= "<tr>";
-      $output .= "  <th class=\"ui-state-default\">Del</th>";
-      $output .= "  <th class=\"ui-state-default\">Description</th>";
-      $output .= "  <th class=\"ui-state-default\">CA?</th>";
-      $output .= "  <th class=\"ui-state-default\">Expiration</th>";
-      $output .= "  <th class=\"ui-state-default\">Authority</th>";
-      $output .= "  <th class=\"ui-state-default\">Managed By</th>";
-      $output .= "</tr>";
 
       $count = 0;
       $q_string  = "select cert_desc,cert_id,cert_url,cert_expire,cert_authority,cert_group,grp_name,cert_isca ";
@@ -138,60 +92,57 @@
       $q_string .= "left join a_groups on a_groups.grp_id = certs.cert_group ";
       $q_string .= "order by cert_desc,cert_expire";
       $q_certs = mysqli_query($db, $q_string) or die ($q_string . ": " . mysqli_error($db));
-      while ($a_certs = mysqli_fetch_array($q_certs)) {
+      if (mysqli_num_rows($q_certs) > 0) {
+        while ($a_certs = mysqli_fetch_array($q_certs)) {
 
-        $certtime = strtotime($a_certs['cert_expire']);
+          $certtime = strtotime($a_certs['cert_expire']);
 
-        $class = " class=\"ui-widget-content";
-        if ($certtime < $date) {
-          $class = " class=\"ui-state-error";
-        } else {
-          if ($certtime < $warningdate) {
-            $class = " class=\"ui-state-highlight";
+          $class = " class=\"ui-widget-content";
+          if ($certtime < $date) {
+            $class = " class=\"ui-state-error";
+          } else {
+            if ($certtime < $warningdate) {
+              $class = " class=\"ui-state-highlight";
+            }
           }
-        }
 
-        if ($a_certs['cert_isca']) {
-          $isca = "Yes";
-        } else {
-          $isca = "No";
-        }
+          if ($a_certs['cert_isca']) {
+            $isca = "Yes";
+          } else {
+            $isca = "No";
+          }
 
-
-        $linkstart = "<a href=\"#\" onclick=\"show_file('certs.fill.php?id=" . $a_certs['cert_id'] . "');jQuery('#dialogCertificate').dialog('open');return false;\">";
-        $linkdel   = "<input type=\"button\" value=\"Remove\" onclick=\"delete_line('certs.del.php?id=" . $a_certs['cert_id'] . "');\">";
-        $linkend   = "</a>";
+          $linkstart = "<a href=\"#\" onclick=\"show_file('certs.fill.php?id=" . $a_certs['cert_id'] . "');jQuery('#dialogUpdate').dialog('open');return false;\">";
+          $linkdel   = "<input type=\"button\" value=\"Remove\" onclick=\"delete_line('certs.del.php?id=" . $a_certs['cert_id'] . "');\">";
+          $linkend   = "</a>";
 
 # if a member of the webapps team or an admin, permit editing.
-        $output .= "<tr>";
-        if (check_grouplevel($db, $GRP_WebApps)) {
-          $output .= "  <td" . $class . " delete\">"                                 . $linkdel                                           . "</td>";
-        } else {
-          $output .= "  <td" . $class . "\">--</td>";
+          $output .= "<tr>";
+          if (check_grouplevel($db, $GRP_WebApps)) {
+            $output .= "  <td" . $class . " delete\">"                                 . $linkdel                                           . "</td>\n";
+          } else {
+            $output .= "  <td" . $class . "\">--</td>\n";
+          }
+          $output .= "  <td" . $class . "\" title=\"" . $a_certs['cert_url'] . "\">" . $linkstart . $a_certs['cert_desc']      . $linkend . "</td>\n";
+          $output .= "  <td" . $class . "\">"                                        . $linkstart . $isca                      . $linkend . "</td>\n";
+          $output .= "  <td" . $class . "\">"                                        . $linkstart . $a_certs['cert_expire']    . $linkend . "</td>\n";
+          $output .= "  <td" . $class . "\">"                                        . $linkstart . $a_certs['cert_authority'] . $linkend . "</td>\n";
+          $output .= "  <td" . $class . "\">"                                        . $linkstart . $a_certs['grp_name']       . $linkend . "</td>\n";
+          $output .= "</tr>\n";
+          $count++;
         }
-        $output .= "  <td" . $class . "\" title=\"" . $a_certs['cert_url'] . "\">" . $linkstart . $a_certs['cert_desc']      . $linkend . "</td>";
-        $output .= "  <td" . $class . "\">"                                        . $linkstart . $isca                      . $linkend . "</td>";
-        $output .= "  <td" . $class . "\">"                                        . $linkstart . $a_certs['cert_expire']    . $linkend . "</td>";
-        $output .= "  <td" . $class . "\">"                                        . $linkstart . $a_certs['cert_authority'] . $linkend . "</td>";
-        $output .= "  <td" . $class . "\">"                                        . $linkstart . $a_certs['grp_name']       . $linkend . "</td>";
-        $output .= "</tr>";
-        $count++;
+
+      } else {
+        $output .= "<tr>\n";
+        $output .= "  <td class=\"ui-widget-content\" colspan=\"6\">No Certificates Defined</td>\n";
+        $output .= "</tr>\n";
       }
 
-      $output .= "</table>";
-      $output .= "</div>";
-      $output .= "</table>";
+      $output .= "</table>\n";
+      $output .= "</div>\n";
+      $output .= "</table>\n";
 
       print "document.getElementById('table_mysql').innerHTML = '" . mysqli_real_escape_string($db, $output) . "';\n\n";
-
-      print "document.dialog.cert_desc.value = '';\n";
-      print "document.dialog.cert_url.value = '';\n";
-      print "document.dialog.cert_expire.value = '';\n";
-      print "document.dialog.cert_authority.value = '';\n";
-      print "document.dialog.cert_group.value = '0';\n";
-      print "document.dialog.cert_ca.value = '0';\n";
-      print "document.dialog.cert_memo.value = '';\n";
-      print "document.dialog.cert_isca.checked = false;\n";
 
     } else {
       logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
