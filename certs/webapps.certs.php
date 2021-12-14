f20f2860fcf3c17e24514306c68ca45e70a95293
Carl Schelin <t.carl.schelin@gmail.com>
2021-12-14 05:45:41 -0700

    updated to switch the software tables to svr_software

--- certs/webapps.certs.php
+++ certs/webapps.certs.php
@@ -57,44 +57,53 @@
   $q_string .= "from certs ";
   $q_string .= "left join a_groups on a_groups.grp_id = certs.cert_group ";
   $q_string .= "order by cert_url,cert_expire";
-  $q_certs = mysqli_query($db, $q_string) or die (mysqli_error($db));
-  while ($a_certs = mysqli_fetch_array($q_certs)) {
+  $q_certs = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+  if (mysqli_num_rows($q_certs) > 0) {
+    while ($a_certs = mysqli_fetch_array($q_certs)) {
 
-    $certtime = strtotime($a_certs['cert_expire']);
+      $certtime = strtotime($a_certs['cert_expire']);
 
-    $class = " class=\"ui-widget-content\"";
-    if ($certtime < $date) {
-      $class = " class=\"ui-state-error\"";
-    } else {
-      if ($certtime < $warningdate) {
-        $class = " class=\"ui-state-highlight\"";
+      $class = " class=\"ui-widget-content\"";
+      if ($certtime < $date) {
+        $class = " class=\"ui-state-error\"";
+      } else {
+        if ($certtime < $warningdate) {
+          $class = " class=\"ui-state-highlight\"";
+        }
+      }
+
+      print "<tr>";
+      print "  <td" . $class . " title=\"" . $a_certs['cert_url'] . "\">" . $a_certs['cert_desc']      . "</td>";
+      print "  <td" . $class . ">"                                        . $a_certs['cert_expire']    . "</td>";
+      print "  <td" . $class . ">"                                        . $a_certs['cert_authority'] . "</td>";
+      print "  <td" . $class . ">"                                        . $a_certs['grp_name']       . "</td>";
+      print "</tr>";
+
+      $q_string  = "select svr_id,inv_name,sw_software,prod_name,grp_name ";
+      $q_string .= "from svr_software ";
+      $q_string .= "left join software  on software.sw_id     = svr_software.svr_softwareid ";
+      $q_string .= "left join inventory on inventory.inv_id   = svr_software.svr_companyid ";
+      $q_string .= "left join products  on products.prod_id   = software.sw_product ";
+      $q_string .= "left join a_groups  on a_groups.grp_id    = svr_software.svr_groupid ";
+      $q_string .= "where svr_certid = " . $a_certs['cert_id'];
+      $q_svr_software = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      if (mysqli_num_rows($q_svr_software) > 0) {
+        while ($a_svr_software = mysqli_fetch_array($q_svr_software)) {
+
+          print "<tr>\n";
+          print "  <td class=\"ui-widget-content\">--" . $a_svr_software['inv_name']    . "</td>\n";
+          print "  <td class=\"ui-widget-content\">"   . $a_svr_software['sw_software'] . "</td>\n";
+          print "  <td class=\"ui-widget-content\">"   . $a_svr_software['prod_name']   . "</td>\n";
+          print "  <td class=\"ui-widget-content\">"   . $a_svr_software['grp_name']    . "</td>\n";
+          print "</tr>\n";
+
+        }
       }
     }
-
-    print "<tr>";
-    print "  <td" . $class . " title=\"" . $a_certs['cert_url'] . "\">" . $a_certs['cert_desc']      . "</td>";
-    print "  <td" . $class . ">"                                        . $a_certs['cert_expire']    . "</td>";
-    print "  <td" . $class . ">"                                        . $a_certs['cert_authority'] . "</td>";
-    print "  <td" . $class . ">"                                        . $a_certs['grp_name']       . "</td>";
-    print "</tr>";
-
-    $q_string  = "select sw_id,inv_name,sw_software,prod_name,grp_name ";
-    $q_string .= "from software ";
-    $q_string .= "left join inventory on inventory.inv_id = software.sw_companyid ";
-    $q_string .= "left join products on products.prod_id = software.sw_product ";
-    $q_string .= "left join a_groups on a_groups.grp_id = software.sw_group ";
-    $q_string .= "where sw_cert = " . $a_certs['cert_id'];
-    $q_software = mysqli_query($db, $q_string) or die(mysqli_error($db));
-    while ($a_software = mysqli_fetch_array($q_software)) {
-
-      print "<tr>\n";
-      print "  <td class=\"ui-widget-content\">--" . $a_software['inv_name']    . "</a></td>";
-      print "  <td class=\"ui-widget-content\">"   . $a_software['sw_software'] . "</td>\n";
-      print "  <td class=\"ui-widget-content\">"   . $a_software['prod_name']   . "</a></td>";
-      print "  <td class=\"ui-widget-content\">"   . $a_software['grp_name']    . "</a></td>";
-      print "</tr>\n";
-
-    }
+  } else {
+      print "<tr>";
+      print "  <td class=\"ui-widget-content\" colspan=\"4\">No Certificates Defined</td>\n";
+      print "</tr>";
   }
 
   mysqli_free_result($q_certs);
