356d79aa9ceb75beed914fe86969359185912666
Carl Schelin <t.carl.schelin@gmail.com>
2021-12-14 05:44:09 -0700

    added new software editor

---
+++ software/software.del.php
@@ -0,0 +1,34 @@
+<?php
+# Script: software.del.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# Description: 
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "software.del.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($db, $AL_Admin)) {
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from software");
+
+      $q_string  = "delete ";
+      $q_string .= "from software ";
+      $q_string .= "where sw_id = " . $formVars['id'];
+      $insert = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
+
+      print "clear_fields();\n";
+    } else {
+      logaccess($db, $_SESSION['uid'], $package, "Access denied");
+    }
+  }
+?>
