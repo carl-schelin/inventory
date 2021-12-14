fc141caa5beaf264a0fe9eefe52a210c6c2ee84c
Carl Schelin <t.carl.schelin@gmail.com>
2021-12-14 05:49:17 -0700

    updated to standardize the software tab and to convert to the selectable software from the main table

--- edit/software.fill.php
+++ edit/software.fill.php
@@ -21,66 +21,45 @@
     if (check_userlevel($db, $AL_Edit)) {
       logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from software");
 
-      $q_string  = "select sw_id,sw_companyid,sw_vendor,sw_product,sw_software,sw_type,sw_group,sw_eol,sw_cert,";
-      $q_string .= "sw_licenseid,sw_supportid,sw_department,sw_facing,sw_notification,sw_primary,sw_eolticket,sw_locked ";
-      $q_string .= "from software ";
-      $q_string .= "where sw_id = " . $formVars['id'];
-      $q_software = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
-      $a_software = mysqli_fetch_array($q_software);
-      mysqli_free_result($q_software);
+      $q_string  = "select svr_id,svr_companyid,svr_softwareid,svr_groupid,svr_certid,";
+      $q_string .= "svr_facing,svr_primary,svr_locked ";
+      $q_string .= "from svr_software ";
+      $q_string .= "where svr_id = " . $formVars['id'];
+      $q_svr_software = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_svr_software = mysqli_fetch_array($q_svr_software);
+      mysqli_free_result($q_svr_software);
 
-      $group         = return_Index($db, $a_software['sw_group'],       "select grp_id from a_groups where grp_disabled = 0 order by grp_name");
-      $product       = return_Index($db, $a_software['sw_product'],     "select prod_id from products where prod_id != 0 order by prod_name");
-      $cert          = return_Index($db, $a_software['sw_cert'],        "select cert_id from certs order by cert_url");
-      $support       = return_Index($db, $a_software['sw_supportid'],   "select sup_id from support order by sup_company,sup_contract");
-      $license       = return_Index($db, $a_software['sw_licenseid'],   "select lic_id from licenses left join products on products.prod_id = licenses.lic_project order by prod_name,lic_key,lic_id");
-      $department    = return_Index($db, $a_software['sw_department'],  "select dep_id from department order by dep_business,dep_name");
+      $softwareid      = return_Index($db, $a_svr_software['svr_softwareid'], "select sw_id from software left join products on products.prod_id = software.sw_product order by sw_software,prod_name");
+      $groupid         = return_Index($db, $a_svr_software['svr_groupid'],    "select grp_id from a_groups where grp_disabled = 0 order by grp_name");
+      $certid          = return_Index($db, $a_svr_software['svr_certid'],     "select cert_id from certs order by cert_url");
 
-      print "document.edit.sw_vendor.value = '"       . mysqli_real_escape_string($db, $a_software['sw_vendor'])       . "';\n";
-      print "document.edit.sw_software.value = '"     . mysqli_real_escape_string($db, $a_software['sw_software'])     . "';\n";
-      print "document.edit.sw_type.value = '"         . mysqli_real_escape_string($db, $a_software['sw_type'])         . "';\n";
-      print "document.edit.sw_notification.value = '" . mysqli_real_escape_string($db, $a_software['sw_notification']) . "';\n";
-      print "document.edit.sw_eol.value = '"          . mysqli_real_escape_string($db, $a_software['sw_eol'])          . "';\n";
-      print "document.edit.sw_eolticket.value = '"    . mysqli_real_escape_string($db, $a_software['sw_eolticket'])    . "';\n";
-
-      if ($group > 0) {
-        print "document.edit.sw_group['"      . $group      . "'].selected = true;\n";
+      if ($softwareid > 0) {
+        print "document.formSoftwareUpdate.svr_softwareid['"   . $softwareid . "'].selected = true;\n";
       }
-      if ($product > 0) {
-        print "document.edit.sw_product['"    . $product    . "'].selected = true;\n";
+      if ($groupid > 0) {
+        print "document.formSoftwareUpdate.svr_groupid['"      . $groupid    . "'].selected = true;\n";
       }
-      if ($cert > 0) {
-        print "document.edit.sw_cert['"       . $cert       . "'].selected = true;\n";
-      }
-      if ($support > 0) {
-        print "document.edit.sw_supportid['"  . $support    . "'].selected = true;\n";
-      }
-      if ($license > 0) {
-        print "document.edit.sw_licenseid['"  . $license    . "'].selected = true;\n";
-      }
-      if ($department > 0) {
-        print "document.edit.sw_department['" . $department . "'].selected = true;\n";
+      if ($certid > 0) {
+        print "document.formSoftwareUpdate.svr_certid['"       . $certid     . "'].selected = true;\n";
       }
 
-      if ($a_software['sw_facing']) {
-        print "document.edit.sw_facing.checked = true;\n";
+      if ($a_svr_software['svr_facing']) {
+        print "document.formSoftwareUpdate.svr_facing.checked = true;\n";
       } else {
-        print "document.edit.sw_facing.checked = false;\n";
+        print "document.formSoftwareUpdate.svr_facing.checked = false;\n";
       }
-      if ($a_software['sw_primary']) {
-        print "document.edit.sw_primary.checked = true;\n";
+      if ($a_svr_software['svr_primary']) {
+        print "document.formSoftwareUpdate.svr_primary.checked = true;\n";
       } else {
-        print "document.edit.sw_primary.checked = false;\n";
+        print "document.formSoftwareUpdate.svr_primary.checked = false;\n";
       }
-      if ($a_software['sw_locked']) {
-        print "document.edit.sw_locked.checked = true;\n";
+      if ($a_svr_software['svr_locked']) {
+        print "document.formSoftwareUpdate.svr_locked.checked = true;\n";
       } else {
-        print "document.edit.sw_locked.checked = false;\n";
+        print "document.formSoftwareUpdate.svr_locked.checked = false;\n";
       }
 
-      print "document.edit.sw_id.value = " . $formVars['id'] . ";\n";
-
-      print "document.edit.sw_update.disabled = false;\n";
+      print "document.formSoftwareUpdate.svr_id.value = " . $formVars['id'] . ";\n";
     } else {
       logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
     }
