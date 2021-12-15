712556c8e5e06f62ac80e9bb292076d6c8229e48
Carl Schelin <t.carl.schelin@gmail.com>
2021-12-14 20:03:48 -0700

    updated to fix software to svr_software

--- scripts/endoflife.php
+++ scripts/endoflife.php
@@ -25,12 +25,13 @@
   }
 
   $product = '';
-  $q_string  = "select inv_id,inv_name,inv_function,prod_name,hw_group,mod_vendor,mod_name,mod_virtual,mod_eol,";
+  $q_string  = "select inv_id,inv_name,inv_function,prod_name,hw_group,ven_name,mod_name,mod_virtual,mod_eol,";
   $q_string .= "hw_serial,hw_purchased,grp_name,inv_appadmin,sup_company,sup_contract,hw_eolticket ";
   $q_string .= "from inventory ";
   $q_string .= "left join hardware on inventory.inv_id = hardware.hw_companyid ";
   $q_string .= "left join a_groups   on a_groups.grp_id    = hardware.hw_group ";
   $q_string .= "left join models   on models.mod_id    = hardware.hw_vendorid ";
+  $q_string .= "left join vendors   on vendors.ven_id    = models.mod_vendor ";
   $q_string .= "left join support  on support.sup_id   = hardware.hw_supportid ";
   $q_string .= "left join products on products.prod_id = inventory.inv_product ";
   $q_string .= "where hw_primary = 1 and inv_status = 0 " . $where;
@@ -56,9 +57,11 @@
       $product = $a_inventory['prod_name'];
     }
 
-    $q_string  = "select sw_software,sw_eol,sw_eolticket ";
+    $q_string  = "select sw_software,sw_eol ";
     $q_string .= "from software ";
-    $q_string .= "where sw_companyid = " . $a_inventory['inv_id'] . " and sw_type = 'OS' ";
+    $q_string .= "left join svr_software on svr_software.svr_softwareid = software.sw_id ";
+    $q_string .= "left join sw_types on sw_types.typ_id = software.sw_type ";
+    $q_string .= "where svr_companyid = " . $a_inventory['inv_id'] . " and typ_name = 'OS' ";
     $q_software = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
     $a_software = mysqli_fetch_array($q_software);
 
@@ -68,7 +71,7 @@
     $q_groups = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
     $a_groups = mysqli_fetch_array($q_groups);
 
-    if ($a_inventory['mod_vendor'] == 'Dell') {
+    if ($a_inventory['ven_name'] == 'Dell') {
       # For Dell, the end of support is 5 years after the purchase date
       $date = explode("-", $a_inventory['hw_purchased']);
       $support = mktime(0,0,0,$date[1],$date[2],$date[0] + 5);
@@ -107,8 +110,7 @@
     print "\"" . $a_inventory['inv_function'] . "\",";
     print "\"" . $a_software['sw_software'] . "\",";
     print "\"" . $moddate . "\",";
-    print "\"" . $a_software['sw_eolticket'] . "\",";
-    print "\"" . $a_inventory['mod_vendor'] . " " . $a_inventory['mod_name'] . "\",";
+    print "\"" . $a_inventory['ven_name'] . " " . $a_inventory['mod_name'] . "\",";
     print "\"" . $newdate . "\",";
     print "\"" . $a_inventory['hw_eolticket'] . "\"\n";
   }
