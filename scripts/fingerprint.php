0f70140dea43e12a195df1d0631e4dd26495345f
Carl Schelin <t.carl.schelin@gmail.com>
2021-12-14 20:41:54 -0700

    updated to change software to svr_software

--- scripts/fingerprint.php
+++ scripts/fingerprint.php
@@ -112,10 +112,12 @@
   $software = "Unset";
   $q_string  = "select mod_eol,sw_eol,hw_supportid ";
   $q_string .= "from hardware ";
-  $q_string .= "left join inventory on inventory.inv_id = hardware.hw_companyid ";
-  $q_string .= "left join models on models.mod_id = hardware.hw_vendorid ";
-  $q_string .= "left join software on software.sw_companyid = inventory.inv_id  ";
-  $q_string .= "where hw_companyid = " . $a_inventory['inv_id'] . " and hw_deleted = 0 and hw_primary = 1 and sw_type = \"OS\" ";
+  $q_string .= "left join inventory    on inventory.inv_id           = hardware.hw_companyid ";
+  $q_string .= "left join models       on models.mod_id              = hardware.hw_vendorid ";
+  $q_string .= "left join svr_software on svr_software.svr_companyid = inventory.inv_id  ";
+  $q_string .= "left join software     on software.sw_id             = svr_software.svr_softwareid  ";
+  $q_string .= "left join sw_types     on sw_types.typ_id            = software.sw_type  ";
+  $q_string .= "where hw_companyid = " . $a_inventory['inv_id'] . " and hw_deleted = 0 and hw_primary = 1 and typ_name = \"OS\" ";
   $q_hardware = mysqli_query($db, $q_string) or die($q_string . ": " . mysqli_error($db));
   if (mysqli_num_rows($q_hardware) > 0) {
     $a_hardware = mysqli_fetch_array($q_hardware);
