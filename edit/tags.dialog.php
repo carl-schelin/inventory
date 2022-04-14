2160380070bb1bf29091070f27f013dfa51886eb
Carl Schelin <t.carl.schelin@gmail.com>
2022-04-14 08:24:16 -0600

    extracted from inventory.php to make it easier to manage the scripts

---
+++ edit/tags.dialog.php
@@ -0,0 +1,17 @@
+
+<div id="dialogTagCreate" title="Add Server Tag">
+
+<form name="formTagCreate">
+
+<input type="hidden" name="tag_companyid" value="<?php print $formVars['tag_companyid']; ?>">
+
+<table class="ui-styled-table">
+<tr>
+  <td class="ui-widget-content">Tag Name <input type="text" name="tag_name" size="40"></td>
+</tr>
+</table>
+
+</form>
+
+</div>
+
