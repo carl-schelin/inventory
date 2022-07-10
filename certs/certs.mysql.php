78990f54c33775383d9b5ec5fc3109e5f2138a67
Carl Schelin <t.carl.schelin@gmail.com>
2022-07-09 22:15:32 -0600

    added a child and grandchild setting to display such certs added a members column and countdown. Removed the unnecessary links

--- certs/certs.mysql.php
+++ certs/certs.mysql.php
@@ -78,18 +78,22 @@
 
       $output  = "<table class=\"ui-styled-table\">\n";
       $output .= "<tr>\n";
-      $output .= "  <th class=\"ui-state-default\" width=\"160\">Delete Certificate</th>\n";
+      if (check_grouplevel($db, $GRP_WebApps)) {
+        $output .= "  <th class=\"ui-state-default\" width=\"160\">Delete Certificate</th>\n";
+      }
       $output .= "  <th class=\"ui-state-default\">Description</th>\n";
       $output .= "  <th class=\"ui-state-default\">CA?</th>\n";
       $output .= "  <th class=\"ui-state-default\">Expiration</th>\n";
       $output .= "  <th class=\"ui-state-default\">Authority</th>\n";
       $output .= "  <th class=\"ui-state-default\">Managed By</th>\n";
+      $output .= "  <th class=\"ui-state-default\">Members</th>\n";
       $output .= "</tr>\n";
 
       $count = 0;
       $q_string  = "select cert_desc,cert_id,cert_url,cert_expire,cert_authority,cert_group,grp_name,cert_isca ";
       $q_string .= "from certs ";
       $q_string .= "left join a_groups on a_groups.grp_id = certs.cert_group ";
+      $q_string .= "where cert_ca = 0 ";
       $q_string .= "order by cert_desc,cert_expire";
       $q_certs = mysqli_query($db, $q_string) or die ($q_string . ": " . mysqli_error($db));
       if (mysqli_num_rows($q_certs) > 0) {
@@ -116,20 +120,157 @@
           $linkdel   = "<input type=\"button\" value=\"Remove\" onclick=\"delete_line('certs.del.php?id=" . $a_certs['cert_id'] . "');\">";
           $linkend   = "</a>";
 
+          $total = 0;
+          $q_string  = "select svr_certid ";
+          $q_string .= "from svr_software ";
+          $q_string .= "where svr_certid = " . $a_certs['cert_id'] . " ";
+          $q_svr_software = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+          if (mysqli_num_rows($q_svr_software) > 0) {
+            while ($a_svr_software = mysqli_fetch_array($q_svr_software)) {
+              $total++;
+            }
+          }
+
 # if a member of the webapps team or an admin, permit editing.
           $output .= "<tr>";
           if (check_grouplevel($db, $GRP_WebApps)) {
-            $output .= "  <td" . $class . " delete\">"                                 . $linkdel                                           . "</td>\n";
-          } else {
-            $output .= "  <td" . $class . "\">--</td>\n";
+            if ($total == 0) {
+              $output .= "  <td" . $class . " delete\">" . $linkdel . "</td>\n";
+            } else {
+              $output .= "  <td class=\"ui-widget-content delete\">Members &gt; 0</td>";
+            }
           }
           $output .= "  <td" . $class . "\" title=\"" . $a_certs['cert_url'] . "\">" . $linkstart . $a_certs['cert_desc']      . $linkend . "</td>\n";
-          $output .= "  <td" . $class . "\">"                                        . $linkstart . $isca                      . $linkend . "</td>\n";
-          $output .= "  <td" . $class . "\">"                                        . $linkstart . $a_certs['cert_expire']    . $linkend . "</td>\n";
-          $output .= "  <td" . $class . "\">"                                        . $linkstart . $a_certs['cert_authority'] . $linkend . "</td>\n";
-          $output .= "  <td" . $class . "\">"                                        . $linkstart . $a_certs['grp_name']       . $linkend . "</td>\n";
+          $output .= "  <td" . $class . " delete\">"                                              . $isca                                 . "</td>\n";
+          $output .= "  <td" . $class . " delete\">"                                              . $a_certs['cert_expire']               . "</td>\n";
+          $output .= "  <td" . $class . "\">"                                                     . $a_certs['cert_authority']            . "</td>\n";
+          $output .= "  <td" . $class . "\">"                                                     . $a_certs['grp_name']                  . "</td>\n";
+          $output .= "  <td" . $class . " delete\">"                                 . $linkstart . $total                     . $linkend . "</td>\n";
           $output .= "</tr>\n";
           $count++;
+
+
+          $q_string  = "select cert_desc,cert_id,cert_url,cert_expire,cert_authority,cert_group,grp_name,cert_isca ";
+          $q_string .= "from certs ";
+          $q_string .= "left join a_groups on a_groups.grp_id = certs.cert_group ";
+          $q_string .= "where cert_ca = " . $a_certs['cert_id'] . " ";
+          $q_string .= "order by cert_desc,cert_expire";
+          $q_child = mysqli_query($db, $q_string) or die ($q_string . ": " . mysqli_error($db));
+          if (mysqli_num_rows($q_child) > 0) {
+            while ($a_child = mysqli_fetch_array($q_child)) {
+
+              $certtime = strtotime($a_child['cert_expire']);
+
+              $class = " class=\"ui-widget-content";
+              if ($certtime < $date) {
+                $class = " class=\"ui-state-error";
+              } else {
+                if ($certtime < $warningdate) {
+                  $class = " class=\"ui-state-highlight";
+                }
+              }
+
+              if ($a_child['cert_isca']) {
+                $isca = "Yes";
+              } else {
+                $isca = "No";
+              }
+
+              $linkstart = "<a href=\"#\" onclick=\"show_file('certs.fill.php?id=" . $a_child['cert_id'] . "');jQuery('#dialogUpdate').dialog('open');return false;\">";
+              $linkdel   = "<input type=\"button\" value=\"Remove\" onclick=\"delete_line('certs.del.php?id=" . $a_child['cert_id'] . "');\">";
+              $linkend   = "</a>";
+
+              $total = 0;
+              $q_string  = "select svr_certid ";
+              $q_string .= "from svr_software ";
+              $q_string .= "where svr_certid = " . $a_child['cert_id'] . " ";
+              $q_svr_software = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+              if (mysqli_num_rows($q_svr_software) > 0) {
+                while ($a_svr_software = mysqli_fetch_array($q_svr_software)) {
+                  $total++;
+                }
+              }
+
+# if a member of the webapps team or an admin, permit editing.
+              $output .= "<tr>";
+              if (check_grouplevel($db, $GRP_WebApps)) {
+                if ($total == 0) {
+                  $output .= "  <td" . $class . " delete\">" . $linkdel . "</td>\n";
+                } else {
+                  $output .= "  <td class=\"ui-widget-content delete\">Members &gt; 0</td>";
+                }
+              }
+              $output .= "  <td" . $class . "\" title=\"" . $a_child['cert_url'] . "\">&gt; " . $linkstart . $a_child['cert_desc']      . $linkend . "</td>\n";
+              $output .= "  <td" . $class . " delete\">"                                                   . $isca                                 . "</td>\n";
+              $output .= "  <td" . $class . " delete\">"                                                   . $a_child['cert_expire']               . "</td>\n";
+              $output .= "  <td" . $class . "\">"                                                          . $a_child['cert_authority']            . "</td>\n";
+              $output .= "  <td" . $class . "\">"                                                          . $a_child['grp_name']                  . "</td>\n";
+              $output .= "  <td" . $class . " delete\">"                                      . $linkstart . $total                     . $linkend . "</td>\n";
+              $output .= "</tr>\n";
+
+
+
+              $q_string  = "select cert_desc,cert_id,cert_url,cert_expire,cert_authority,cert_group,grp_name,cert_isca ";
+              $q_string .= "from certs ";
+              $q_string .= "left join a_groups on a_groups.grp_id = certs.cert_group ";
+              $q_string .= "where cert_ca = " . $a_child['cert_id'] . " ";
+              $q_string .= "order by cert_desc,cert_expire";
+              $q_grandchild = mysqli_query($db, $q_string) or die ($q_string . ": " . mysqli_error($db));
+              if (mysqli_num_rows($q_grandchild) > 0) {
+                while ($a_grandchild = mysqli_fetch_array($q_grandchild)) {
+
+                  $certtime = strtotime($a_grandchild['cert_expire']);
+
+                  $class = " class=\"ui-widget-content";
+                  if ($certtime < $date) {
+                    $class = " class=\"ui-state-error";
+                  } else {
+                    if ($certtime < $warningdate) {
+                      $class = " class=\"ui-state-highlight";
+                    }
+                  }
+
+                  if ($a_grandchild['cert_isca']) {
+                    $isca = "Yes";
+                  } else {
+                    $isca = "No";
+                  }
+
+                  $linkstart = "<a href=\"#\" onclick=\"show_file('certs.fill.php?id=" . $a_grandchild['cert_id'] . "');jQuery('#dialogUpdate').dialog('open');return false;\">";
+                  $linkdel   = "<input type=\"button\" value=\"Remove\" onclick=\"delete_line('certs.del.php?id=" . $a_grandchild['cert_id'] . "');\">";
+                  $linkend   = "</a>";
+
+                  $total = 0;
+                  $q_string  = "select svr_certid ";
+                  $q_string .= "from svr_software ";
+                  $q_string .= "where svr_certid = " . $a_grandchild['cert_id'] . " ";
+                  $q_svr_software = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+                  if (mysqli_num_rows($q_svr_software) > 0) {
+                    while ($a_svr_software = mysqli_fetch_array($q_svr_software)) {
+                      $total++;
+                    }
+                  }
+
+# if a member of the webapps team or an admin, permit editing.
+                  $output .= "<tr>";
+                  if (check_grouplevel($db, $GRP_WebApps)) {
+                    if ($total == 0) {
+                      $output .= "  <td" . $class . " delete\">" . $linkdel . "</td>\n";
+                    } else {
+                      $output .= "  <td class=\"ui-widget-content delete\">Members &gt; 0</td>";
+                    }
+                  }
+                  $output .= "  <td" . $class . "\" title=\"" . $a_grandchild['cert_url'] . "\">&gt;&gt; " . $linkstart . $a_grandchild['cert_desc']      . $linkend . "</td>\n";
+                  $output .= "  <td" . $class . " delete\">"                                                            . $isca                                      . "</td>\n";
+                  $output .= "  <td" . $class . " delete\">"                                                            . $a_grandchild['cert_expire']               . "</td>\n";
+                  $output .= "  <td" . $class . "\">"                                                                   . $a_grandchild['cert_authority']            . "</td>\n";
+                  $output .= "  <td" . $class . "\">"                                                                   . $a_cgrandchild['grp_name']                 . "</td>\n";
+                  $output .= "  <td" . $class . " delete\">"                                               . $linkstart . $total                          . $linkend . "</td>\n";
+                  $output .= "</tr>\n";
+                }
+              }
+            }
+          }
         }
 
       } else {
