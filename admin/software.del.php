49d22bd946f4a1f7855a2fb38a6c9f6536a59df7
Carl Schelin <t.carl.schelin@gmail.com>
2022-12-15 12:55:19 -0700

    IM_76
    * Renamed the sw_support table to inv_sw_support

--- admin/software.del.php
+++ admin/software.del.php
@@ -19,10 +19,10 @@
     }
 
     if (check_userlevel($db, $AL_Admin)) {
-      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from sw_software");
+      logaccess($db, $_SESSION['uid'], $package, "Deleting " . $formVars['id'] . " from inv_sw_support");
 
       $q_string  = "delete ";
-      $q_string .= "from sw_support ";
+      $q_string .= "from inv_sw_support ";
       $q_string .= "where sw_id = " . $formVars['id'];
       $insert = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
 
