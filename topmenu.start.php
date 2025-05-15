2b9ffc2e3d8bf90ba3c05bce184c6f8ffb09e924
Carl Schelin <t.carl.schelin@gmail.com>
2025-05-15 15:47:43 -0600

    moved items around, removed items from the admin menu

--- topmenu.start.php
+++ topmenu.start.php
@@ -14,36 +14,23 @@
       <li><a href="<?php print $Editroot; ?>/inventory.php">Add Server or Device</a></li>
     </ul>
   </li>
-  <li id="tm_tools"><a href="<?php print $Siteroot; ?>/index.tools.php">Tools</a>
+
+  <li id="tm_siteadmin"><a href="<?php print $Siteroot; ?>/index.organization.php">Organization</a>
     <ul>
-      <li><a href="<?php print $Issueroot;   ?>/issue.php">Issue Tracker</a></li>
-      <li><a href="<?php print $Certsroot;   ?>/certs.php">Web Certificate Management</a></li>
-      <li><a href="<?php print $Certsroot;   ?>/webapps.certs.php">-View Certificates</a></li>
-      <li><a href="<?php print $Reportroot;  ?>/hostname.php">Hostname Encode/Decode</a></li>
-      <li><a href="<?php print $Excluderoot; ?>/exclude.php">Exclusion Manager</a></li>
-      <li><a href="<?php print $Imageroot;   ?>/image.php">Image Manager</a></li>
-      <li><a href="javascript:;" onClick="javascript:attach_group('<?php print $Adminroot; ?>/tags.php');">Tag Manager</a></li>
-      <li><a href="<?php print $Statusroot;  ?>/home.php" target="_blank">Status Management</a></li>
-<?php
-    if (check_userlevel($db, $AL_Admin)) {
-?>
-      <li><a href="<?php print $Assetroot;  ?>/assets.php" target="_blank">Bulk Upload</a></li>
-      <li><a href="<?php print $Manageroot; ?>/users.php" target="_blank">User Manager</a></li>
-<?php
-  }
-?>
-      <li><a href="<?php print $Nagiosroot; ?>" target="_blank">Lab Monitoring (Nagios)</a></li>
+      <li><a href="<?php print $Usersroot; ?>/organization.php">Organizations</a></li>
+      <li><a href="<?php print $Usersroot; ?>/business.php">Business Names</a></li>
+      <li><a href="<?php print $Usersroot; ?>/department.php">Department Names</a></li>
+      <li><a href="<?php print $Usersroot; ?>/groups.php">Group Management</a></li>
+      <li><a href="<?php print $Usersroot; ?>/users.php">User Management</a></li>
     </ul>
   </li>
 
-  <li id="tm_ipam"><a href="<?php print $Siteroot; ?>/index.ipam.php">IPAM</a>
+  <li id="tm_location"><a href="<?php print $Siteroot; ?>/index.datacenter.php">Data Centers</a>
     <ul>
-      <li><a href="<?php print $IPAMroot; ?>/ipam.php">IP Address Manager</a></li>
-      <li><a href="<?php print $IPAMroot; ?>/subzones.php">IP Address Zones</a></li>
-      <li><a href="<?php print $IPAMroot; ?>/types.php">IP Address Types</a></li>
-      <li><a href="<?php print $IPAMroot; ?>/description.php">Interface Descriptions</a></li>
-      <li><a href="<?php print $IPAMroot; ?>/network.php">Network Manager</a></li>
-      <li><a href="<?php print $IPAMroot; ?>/zones.php">Network Zones</a></li>
+      <li><a href="<?php print $DCroot;      ?>/datacenter.php">Data Center Manager</a></li>
+      <li><a href="<?php print $DCroot;      ?>/city.php">City/County Manager</a></li>
+      <li><a href="<?php print $DCroot;      ?>/state.php">State Manager</a></li>
+      <li><a href="<?php print $DCroot;      ?>/country.php">Country Manager</a></li>
     </ul>
   </li>
 
@@ -75,34 +62,37 @@
     </ul>
   </li>
 
-  <li id="tm_location"><a href="<?php print $Siteroot; ?>/index.datacenter.php">Data Centers</a>
+  <li id="tm_ipam"><a href="<?php print $Siteroot; ?>/index.ipam.php">IPAM</a>
     <ul>
-      <li><a href="<?php print $DCroot;      ?>/datacenter.php">Data Center Manager</a></li>
-      <li><a href="<?php print $DCroot;      ?>/city.php">City/County Manager</a></li>
-      <li><a href="<?php print $DCroot;      ?>/state.php">State Manager</a></li>
-      <li><a href="<?php print $DCroot;      ?>/country.php">Country Manager</a></li>
+      <li><a href="<?php print $IPAMroot; ?>/ipam.php">IP Address Manager</a></li>
+      <li><a href="<?php print $IPAMroot; ?>/subzones.php">IP Address Zones</a></li>
+      <li><a href="<?php print $IPAMroot; ?>/types.php">IP Address Types</a></li>
+      <li><a href="<?php print $IPAMroot; ?>/description.php">Interface Descriptions</a></li>
+      <li><a href="<?php print $IPAMroot; ?>/network.php">Network Manager</a></li>
+      <li><a href="<?php print $IPAMroot; ?>/zones.php">Network Zones</a></li>
     </ul>
   </li>
 
-  <li id="tm_siteadmin"><a href="<?php print $Siteroot; ?>/index.siteadmin.php">Site Administration</a>
+  <li id="tm_tools"><a href="<?php print $Siteroot; ?>/index.tools.php">Tools</a>
     <ul>
-      <li><a href="<?php print $Usersroot; ?>/organization.php">Organizations</a></li>
-      <li><a href="<?php print $Usersroot; ?>/business.php">Business Names</a></li>
-      <li><a href="<?php print $Usersroot; ?>/department.php">Department Names</a></li>
-      <li><a href="<?php print $Usersroot; ?>/groups.php">Group Management</a></li>
-      <li><a href="<?php print $Usersroot; ?>/users.php">User Management</a></li>
+      <li><a href="<?php print $Issueroot;   ?>/issue.php">Issue Tracker</a></li>
+      <li><a href="<?php print $Certsroot;   ?>/certs.php">Web Certificate Management</a></li>
+      <li><a href="<?php print $Certsroot;   ?>/webapps.certs.php">-View Certificates</a></li>
+      <li><a href="<?php print $Reportroot;  ?>/hostname.php">Hostname Encode/Decode</a></li>
+      <li><a href="<?php print $Excluderoot; ?>/exclude.php">Exclusion Manager</a></li>
+      <li><a href="<?php print $Imageroot;   ?>/image.php">Image Manager</a></li>
+      <li><a href="javascript:;" onClick="javascript:attach_group('<?php print $Adminroot; ?>/tags.php');">Tag Manager</a></li>
+      <li><a href="<?php print $Statusroot;  ?>/home.php" target="_blank">Status Management</a></li>
+<?php
+    if (check_userlevel($db, $AL_Admin)) {
+?>
+      <li><a href="<?php print $Assetroot;  ?>/assets.php" target="_blank">Bulk Upload</a></li>
+      <li><a href="<?php print $Manageroot; ?>/users.php" target="_blank">User Manager</a></li>
+<?php
+  }
+?>
+      <li><a href="<?php print $Nagiosroot; ?>" target="_blank">Lab Monitoring (Nagios)</a></li>
     </ul>
   </li>
 
-  <li id="tm_database"><a href="<?php print $Siteroot; ?>/index.manage.php">Database</a>
-    <ul>
-      <li><a href="<?php print $Usersroot; ?>/roles.php">Role Names</a></li>
-      <li><a href="<?php print $Adminroot; ?>/maintenance.windows.php">Maintenance Windows</a></li>
-      <li><a href="<?php print $Productroot; ?>/product.php">Products and Services</a></li>
-      <li><a href="<?php print $Projectroot; ?>/project.php">Projects</a></li>
-      <li><a href="<?php print $Adminroot; ?>/service.php">Service Class</a></li>
-      <li><a href="<?php print $Adminroot; ?>/software.php">Software Support</a></li>
-      <li><a href="<?php print $Adminroot; ?>/support.php">Support Contracts</a></li>
-      <li><a href="<?php print $Adminroot; ?>/timezones.php">Time Zones</a></li>
-    </ul>
-  </li>
+  <li id="tm_database"><a href="<?php print $Siteroot; ?>/index.manage.php">Inventory Administration</a></li>
