d1352f9bede7a23baf3d1f6b691d8f8aaf8ae4bf
Carl Schelin <t.carl.schelin@gmail.com>
2025-05-28 14:30:21 -0600

    added table management of the vendors table

---
+++ hardware/vendors.fill.php
@@ -0,0 +1,39 @@
+<?php
+# Script: vendors.fill.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# Description: 
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "vendors.fill.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from inv_vendors");
+
+      $q_string  = "select ven_name ";
+      $q_string .= "from inv_vendors ";
+      $q_string .= "where ven_id = " . $formVars['id'];
+      $q_inv_vendors = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_inv_vendors = mysqli_fetch_array($q_inv_vendors);
+      mysqli_free_result($q_inv_vendors);
+
+      print "document.formUpdate.ven_name.value = '"   . mysqli_real_escape_string($db, $a_inv_vendors['ven_name'])   . "';\n";
+
+      print "document.formUpdate.id.value = " . $formVars['id'] . ";\n";
+
+    } else {
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
+    }
+  }
+?>
