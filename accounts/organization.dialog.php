5de69326a103f52ee1f31192744887a5d3e75598
Carl Schelin <t.carl.schelin@gmail.com>
2025-06-24 13:02:04 -0600

    moved the dialog box outside the main file

---
+++ accounts/organization.dialog.php
@@ -0,0 +1,19 @@
+<table class="ui-styled-table">
+<tr>
+  <td class="ui-widget-content">Organization Name: <input type="text" name="org_name" size="50"></td>
+</tr>
+<tr>
+  <td class="ui-widget-content">Manager: <select name="org_manager">
+<?php
+  $q_string  = "select usr_id,usr_last,usr_first ";
+  $q_string .= "from inv_users ";
+  $q_string .= "where usr_disabled = 0 ";
+  $q_string .= "order by usr_last,usr_first ";
+  $q_inv_users = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+  while ($a_inv_users = mysqli_fetch_array($q_inv_users)) {
+    print "<option value=\"" . $a_inv_users['usr_id'] . "\">" . $a_inv_users['usr_last'] . ", " . $a_inv_users['usr_first'] . "</option>\n";
+  }
+?>
+</select></td>
+</tr>
+</table>
--- accounts/organization.php
+++ accounts/organization.php
@@ -237,25 +237,7 @@ you can add the necessary information and then save the new Organization.</p>
 
 <form name="formCreate">
 
-<table class="ui-styled-table">
-<tr>
-  <td class="ui-widget-content">Organization Name: <input type="text" name="org_name" size="60"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Manager: <select name="org_manager">
-<?php
-  $q_string  = "select usr_id,usr_last,usr_first ";
-  $q_string .= "from inv_users ";
-  $q_string .= "where usr_disabled = 0 ";
-  $q_string .= "order by usr_last,usr_first ";
-  $q_inv_users = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
-  while ($a_inv_users = mysqli_fetch_array($q_inv_users)) {
-    print "<option value=\"" . $a_inv_users['usr_id'] . "\">" . $a_inv_users['usr_last'] . ", " . $a_inv_users['usr_first'] . "</option>\n";
-  }
-?>
-</select></td>
-</tr>
-</table>
+<?php include('organization.dialog.php'); ?>
 
 </form>
 
@@ -267,25 +249,7 @@ you can add the necessary information and then save the new Organization.</p>
 
 <input type="hidden" name="id" value="0">
 
-<table class="ui-styled-table">
-<tr>
-  <td class="ui-widget-content">Organization Name: <input type="text" name="org_name" size="60"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Manager: <select name="org_manager">
-<?php
-  $q_string  = "select usr_id,usr_last,usr_first ";
-  $q_string .= "from inv_users ";
-  $q_string .= "where usr_disabled = 0 ";
-  $q_string .= "order by usr_last,usr_first ";
-  $q_inv_users = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
-  while ($a_inv_users = mysqli_fetch_array($q_inv_users)) {
-    print "<option value=\"" . $a_inv_users['usr_id'] . "\">" . $a_inv_users['usr_last'] . ", " . $a_inv_users['usr_first'] . "</option>\n";
-  }
-?>
-</select></td>
-</tr>
-</table>
+<?php include('organization.dialog.php'); ?>
 
 </form>
 
