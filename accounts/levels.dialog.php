15c912dadaada6dd06b8c7679403e88d471886da
Carl Schelin <t.carl.schelin@gmail.com>
2025-06-24 13:12:48 -0600

    moved the dialog box to a file

---
+++ accounts/levels.dialog.php
@@ -0,0 +1,14 @@
+<table class="ui-styled-table">
+<tr>
+  <td class="ui-widget-content">Level Name: <input type="text" name="lvl_name" size="40"></td>
+</tr>
+<tr>
+  <td class="ui-widget-content">Access Level: <input type="number" name="lvl_level" size="10"></td>
+</tr>
+<tr>
+  <td class="ui-widget-content">Status <select name="lvl_disabled">
+<option value="0">Enabled</option>
+<option value="1">Disabled</option>
+</select></td>
+</tr>
+</table>
