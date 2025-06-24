ec8be0cbc2edce14321c0d64c2f2d06c41ef339a
Carl Schelin <t.carl.schelin@gmail.com>
2025-06-24 14:47:58 -0600

    moved dialog into a file

--- admin/titles.php
+++ admin/titles.php
@@ -227,17 +227,7 @@ $(document).ready( function() {
 
 <form name="formCreate">
 
-<table class="ui-styled-table">
-<tr>
-  <td class="ui-widget-content">Title: <input type="text" name="tit_name" size="60"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Level: <input type="text" name="tit_level" size="10"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Order: <input type="text" name="tit_order" size="10"></td>
-</tr>
-</table>
+<?php include('titles.dialog.php'); ?>
 
 </form>
 
@@ -250,17 +240,7 @@ $(document).ready( function() {
 
 <input type="hidden" name="id" value="0">
 
-<table class="ui-styled-table">
-<tr>
-  <td class="ui-widget-content">Title: <input type="text" name="tit_name" size="60"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Level: <input type="text" name="tit_level" size="10"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Order: <input type="text" name="tit_order" size="10"></td>
-</tr>
-</table>
+<?php include('titles.dialog.php'); ?>
 
 </form>
 
