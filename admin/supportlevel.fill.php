2819b31e89b93f2c72218e79911f64c6cd93d2f7
Carl Schelin <t.carl.schelin@gmail.com>
2025-06-24 10:12:34 -0600

    added scripts to manage hardware and software support levels

---
+++ admin/supportlevel.fill.php
@@ -0,0 +1,42 @@
+<?php
+# Script: supportlevel.fill.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# Description:
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "supportlevel.fill.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from inv_supportlevel");
+
+      $q_string  = "select slv_value,slv_translate ";
+      $q_string .= "from inv_supportlevel ";
+      $q_string .= "where slv_id = " . $formVars['id'];
+      $q_inv_supportlevel = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_inv_supportlevel = mysqli_fetch_array($q_inv_supportlevel);
+      mysqli_free_result($q_inv_supportlevel);
+
+      print "document.formUpdate.slv_value.value = '"        . $a_inv_supportlevel['slv_value']        . "';\n";
+      print "document.formUpdate.slv_translate.value = '"    . $a_inv_supportlevel['slv_translate']    . "';\n";
+
+      print "document.formUpdate.id.value = " . $formVars['id'] . ";\n";
+
+      print "clear_fields();\n";
+
+    } else {
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
+    }
+  }
+?>
