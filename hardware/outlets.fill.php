2742e72f6b4848e0848d6e3ea5afcc5cc6b00bbf
Carl Schelin <t.carl.schelin@gmail.com>
2025-08-26 15:09:26 -0600

    added the ability to manage power outlets such as on UPSs

---
+++ hardware/outlets.fill.php
@@ -0,0 +1,57 @@
+<?php
+# Script: outlets.fill.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# Description: 
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "outlets.fill.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel($db, $AL_Edit)) {
+      logaccess($db, $_SESSION['uid'], $package, "Requesting record " . $formVars['id'] . " from inv_outlets");
+
+      $q_string  = "select out_deviceid,out_name,out_type,out_active,out_desc,out_facing ";
+      $q_string .= "from inv_outlets ";
+      $q_string .= "where out_id = " . $formVars['id'];
+      $q_inv_outlets = mysqli_query($db, $q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysqli_error($db)));
+      $a_inv_outlets = mysqli_fetch_array($q_inv_outlets);
+      mysqli_free_result($q_inv_outlets);
+
+      $outdeviceid = return_Index($db, $a_inv_outlets['out_deviceid'], "select ast_id from inv_assets where ast_name != \"\" order by ast_name ");
+      $outtype     = return_Index($db, $a_inv_outlets['out_type'],     "select plug_id from inv_int_plugtype order by plug_text");
+
+      print "document.formUpdate.out_name.value = '"    . mysqli_real_escape_string($db, $a_inv_outlets['out_name'])    . "';\n";
+      print "document.formUpdate.out_desc.value = '"    . mysqli_real_escape_string($db, $a_inv_outlets['out_desc'])    . "';\n";
+
+      print "document.formUpdate.out_deviceid['" . $outdeviceid . "'].selected = true;\n";
+      print "document.formUpdate.out_type['"     . $outtype     . "'].selected = true;\n";
+
+      if ($a_inv_outlets['out_active']) {
+        print "document.formUpdate.out_active.checked = true;\n";
+      } else {
+        print "document.formUpdate.out_active.checked = false;\n";
+      }
+      if ($a_inv_outlets['out_facing']) {
+        print "document.formUpdate.out_facing.checked = true;\n";
+      } else {
+        print "document.formUpdate.out_facing.checked = false;\n";
+      }
+
+      print "document.formUpdate.id.value = " . $formVars['id'] . ";\n";
+
+    } else {
+      logaccess($db, $_SESSION['uid'], $package, "Unauthorized access.");
+    }
+  }
+?>
---
+++ hardware/outlets.php
@@ -0,0 +1,282 @@
+<?php
+# Script: outlets.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# Description: 
+
+  include('settings.php');
+  $called = 'no';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+# connect to the database
+  $db = db_connect($DBserver, $DBname, $DBuser, $DBpassword);
+
+  check_login($db, $AL_Edit);
+
+  $package = "outlets.php";
+
+  logaccess($db, $_SESSION['uid'], $package, "Accessing script");
+
+  $formVars['sort'] = '';
+  if (isset($_GET['sort'])) {
+    $formVars['sort'] = clean($_GET['sort'], 30);
+  } 
+
+# if help has not been seen yet,
+  if (show_Help($db, $Sitepath . "/" . $package)) {
+    $display = "display: block";
+  } else {
+    $display = "display: none";
+  }
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Manage Power Outlets</title>
+
+<style type='text/css' title='currentStyle' media='screen'>
+<?php include($Sitepath . "/mobile.php"); ?>
+</style>
+
+<script type="text/javascript" language="javascript" src="<?php print $Siteroot; ?>/css/jquery.js"></script>
+<script type="text/javascript" language="javascript" src="<?php print $Siteroot; ?>/css/jquery-ui/jquery-ui.js"></script>
+<link   rel="stylesheet" type="text/css"            href="<?php print $Siteroot; ?>/css/jquery-ui-themes/themes/<?php print $_SESSION['theme']; ?>/jquery-ui.css">
+<script type="text/javascript" language="javascript" src="<?php print $Siteroot; ?>/functions/jquery.inventory.js"></script>
+
+<script type="text/javascript">
+
+<?php
+  if (check_userlevel($db, $AL_Admin)) {
+?>
+function delete_line( p_script_url ) {
+  var answer = confirm("Delete this Power Outlet?")
+
+  if (answer) {
+    script = document.createElement('script');
+    script.src = p_script_url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
+}
+<?php
+  }
+?>
+
+function attach_file( p_script_url, update ) {
+  var af_form = document.formCreate;
+  var af_url;
+
+  af_url  = '?update='   + update;
+
+  af_url += "&out_deviceid="     + af_form.out_deviceid.value;
+  af_url += "&out_name="         + encode_URI(af_form.out_name.value);
+  af_url += "&out_type="         + af_form.out_type.value;
+  af_url += "&out_active="       + af_form.out_active.checked;
+  af_url += "&out_desc="         + encode_URI(af_form.out_desc.value);
+  af_url += "&out_facing="       + af_form.out_facing.checked;
+
+  script = document.createElement('script');
+  script.src = p_script_url + af_url;
+  document.getElementsByTagName('head')[0].appendChild(script);
+  clear_fields();
+}
+
+function update_file( p_script_url, update ) {
+  var uf_form = document.formUpdate;
+  var uf_url;
+
+  uf_url  = '?update='   + update;
+  uf_url += '&id='       + uf_form.id.value;
+
+  uf_url += "&out_deviceid="     + uf_form.out_deviceid.value;
+  uf_url += "&out_name="         + encode_URI(uf_form.out_name.value);
+  uf_url += "&out_type="         + uf_form.out_type.value;
+  uf_url += "&out_active="       + uf_form.out_active.checked;
+  uf_url += "&out_desc="         + encode_URI(uf_form.out_desc.value);
+  uf_url += "&out_facing="       + uf_form.out_facing.checked;
+
+  script = document.createElement('script');
+  script.src = p_script_url + uf_url;
+  document.getElementsByTagName('head')[0].appendChild(script);
+  clear_fields();
+}
+
+function clear_fields() {
+  show_file('outlets.mysql.php?update=-1&sort=<?php print $formVars['sort']; ?>');
+}
+
+$(document).ready( function() {
+  $( '#clickCreate' ).click(function() {
+    $( "#dialogCreate" ).dialog('open');
+  });
+
+  $( "#dialogCreate" ).dialog({
+    autoOpen: false,
+    modal: true,
+    height: 275,
+    width:  600,
+    show: 'slide',
+    hide: 'slide',
+    closeOnEscape: true,
+    dialogClass: 'dialogWithDropShadow',
+    close: function(event, ui) {
+      $( "#dialogCreate" ).hide();
+    },
+    buttons: [
+      {
+        text: "Cancel",
+        click: function() {
+          attach_file('outlets.mysql.php', -1);
+          $( this ).dialog( "close" );
+        }
+      },
+      {
+        text: "Add Outlet",
+        click: function() {
+          attach_file('outlets.mysql.php', 0);
+          $( this ).dialog( "close" );
+        }
+      }
+    ]
+  });
+
+  $( "#dialogUpdate" ).dialog({
+    autoOpen: false,
+    modal: true,
+    height: 275,
+    width:  600,
+    show: 'slide',
+    hide: 'slide',
+    closeOnEscape: true,
+    dialogClass: 'dialogWithDropShadow',
+    close: function(event, ui) {
+      $( "#dialogUpdate" ).hide();
+    },
+    buttons: [
+      {
+        text: "Cancel",
+        click: function() {
+          update_file('outlets.mysql.php', -1);
+          $( this ).dialog( "close" );
+        }
+      },
+      {
+        text: "Update Outlet",
+        click: function() {
+          update_file('outlets.mysql.php', 1);
+          $( this ).dialog( "close" );
+        }
+      },
+      {
+        text: "Add Outlet",
+        click: function() {
+          update_file('outlets.mysql.php', 0);
+          $( this ).dialog( "close" );
+        }
+      }
+    ]
+  });
+});
+
+</script>
+
+</head>
+<body onLoad="clear_fields();" class="ui-widget-content">
+
+<?php include($Sitepath . '/topmenu.start.php'); ?>
+<?php include($Sitepath . '/topmenu.end.php'); ?>
+
+<div id="main">
+
+<table class="ui-styled-table">
+<tr>
+  <th class="ui-state-default">Power Outlet Editor</th>
+  <th class="ui-state-default" width="20"><a href="javascript:;" onmousedown="toggleDiv('outlet-help');">Help</a></th>
+</tr>
+</table>
+
+<div id="outlet-help" style="<?php print $display; ?>">
+
+<div class="main-help ui-widget-content">
+
+<p><u><strong>Power Port Listing</strong></u></p>
+
+<p>The purpose behind the Power Port Listing is to identify the connection between 
+devices and ensure the racks are sufficiently powered.</p>
+
+<p>We'll select the device that has one or more ports, identify the correct port, 
+and save the information.</p>
+
+</div>
+
+</div>
+
+
+<table class="ui-styled-table">
+<tr>
+  <td class="ui-widget-content button"><input type="button" id="clickCreate" value="Add Power Outlet"></td>
+</tr>
+</table>
+
+<p></p>
+
+<table class="ui-styled-table">
+<tr>
+  <th class="ui-state-default">Power Outlet Listing</th>
+  <th class="ui-state-default" width="20"><a href="javascript:;" onmousedown="toggleDiv('outlet-listing-help');">Help</a></th>
+</tr>
+</table>
+
+<div id="outlet-listing-help" style="<?php print $display; ?>">
+
+<div class="main-help ui-widget-content">
+
+<p><u><strong>Power Port Listing</strong></u></p>
+
+<p>This is a list of the Power Ports for all Assets in the Inventory. to be 
+perfectly clear, ports only need to be defined for systems where we want to 
+track connections such as computer rooms. Assigning a port to a device that 
+isn't normally in a computer room is unnecessary.</p>
+
+<img src="<?php print $Imgsroot . "/powerports.webp"; ?>">
+
+</div>
+
+</div>
+
+
+<span id="mysql_table"><?php print wait_Process('Waiting...')?></span>
+
+</div>
+
+
+<div id="dialogCreate" title="Add Power Outlet Form">
+
+<form name="formCreate">
+
+<?php include('outlets.dialog.php'); ?>
+
+</form>
+
+</div>
+
+
+<div id="dialogUpdate" title="Edit Power Outlet Form">
+
+<form name="formUpdate">
+
+<input type="hidden" name="id" value="0">
+
+<?php include('outlets.dialog.php'); ?>
+
+</form>
+
+</div>
+
+
+<?php include($Sitepath . '/footer.php'); ?>
+
+</body>
+</html>
